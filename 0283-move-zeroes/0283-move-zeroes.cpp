e5bcf8873a87ea435197435fe925bcb0f2945a6f
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-01-21 13:44:03 +0530

    Time: 24 ms (79.33%), Space: 19.3 MB (16.78%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int i=0, j=nums.size();
-       while(i<j){
-           if(nums[i]==0){
-               nums.erase(nums.begin()+i);
-               nums.push_back(0);
-               j--;
-           }
-           else{
-               i++;
-           }
-       }
+       // int i=0, j=nums.size();
+       // while(i<j){
+       //     if(nums[i]==0){
+       //         nums.erase(nums.begin()+i);
+       //         nums.push_back(0);
+       //         j--;
+       //     }
+       //     else{
+       //         i++;
+       //     }
+       // }
+        
+        int i=0;
+        for(int j=0; j<nums.size(); j++){
+            if(nums[j]!=0){
+                swap(nums[i], nums[j]);
+                i++;
+            }
+        }
     }
 };
