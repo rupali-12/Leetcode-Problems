5254d327e41a4e316f62745346b0cdc45e4637e1
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-02-14 19:52:27 +0530

    Added solution - LeetHub

--- Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.cpp
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.cpp
@@ -84,39 +84,66 @@ class Solution
   }
        
     }
+    
+    
     struct Node* add(struct Node* first, struct Node* second){
         int carry=0;
         Node* ansHead =NULL;
         Node* ansTail =NULL;
-        while(first!=NULL && second!=NULL){
-       int sum = carry + first->data + second->data;
-            int digit= sum%10;
-            insertAtTail(ansHead, ansTail, digit);
-            carry= sum/10;
-            first= first->next;
-            second= second->next;
-        }
-          while(first!=NULL){
-            int sum= carry+ first->data;
-            int digit= sum%10;
-          insertAtTail(ansHead, ansTail, digit);
-           carry = sum/10;
-           first= first->next;
-          }
-          while(second!=NULL){
-            int sum= carry+ second->data;
-            int digit= sum%10;
-          insertAtTail(ansHead, ansTail, digit);
-           carry = sum/10;
-          second= second->next;
-          }
-          while(carry!=0){
-            int sum= carry;
-            int digit= sum%10;
-          insertAtTail(ansHead, ansTail, digit);
-           carry = sum/10;
-          }
         
+    // ******************************************************************************
+        
+    //     while(first!=NULL && second!=NULL){
+    //   int sum = carry + first->data + second->data;
+    //         int digit= sum%10;
+    //         insertAtTail(ansHead, ansTail, digit);
+    //         carry= sum/10;
+    //         first= first->next;
+    //         second= second->next;
+    //     }
+    //       while(first!=NULL){
+    //         int sum= carry+ first->data;
+    //         int digit= sum%10;
+    //       insertAtTail(ansHead, ansTail, digit);
+    //       carry = sum/10;
+    //       first= first->next;
+    //       }
+    //       while(second!=NULL){
+    //         int sum= carry+ second->data;
+    //         int digit= sum%10;
+    //       insertAtTail(ansHead, ansTail, digit);
+    //       carry = sum/10;
+    //       second= second->next;
+    //       }
+    //       while(carry!=0){
+    //         int sum= carry;
+    //         int digit= sum%10;
+    //       insertAtTail(ansHead, ansTail, digit);
+    //       carry = sum/10;
+    //       }
+    
+    
+    // ******************************************************************************
+        // General>>>
+        while(first!=NULL || second!=NULL || carry!=0){
+            int val1= 0;
+            if(first!=NULL)
+               val1= first->data;
+               
+            int val2=0;
+            if(second!=NULL)
+               val2= second->data;
+               
+            int sum = carry +val1+val2;
+            int digit =sum%10;
+            insertAtTail(ansHead, ansTail, digit);
+            carry =sum/10;
+            
+            if(first!=NULL)
+               first= first->next;
+            if(second!=NULL)
+               second= second->next;
+        }
         return ansHead;
     }
     public:
