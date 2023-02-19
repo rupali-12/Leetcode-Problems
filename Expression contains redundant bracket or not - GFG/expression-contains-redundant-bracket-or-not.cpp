e928f79ebdbf926b94bfac8be9ec02b9f90ab62b
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-02-19 18:54:18 +0530

    Added solution - LeetHub

---
+++ Expression contains redundant bracket or not - GFG/expression-contains-redundant-bracket-or-not.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int checkRedundancy(string s) {
+        // code here
+        stack<char>st;
+        for(int i=0; i<s.length(); i++){
+            char ch =s[i];
+            
+            if(ch=='(' || ch=='+' || ch=='-' || ch=='*' || ch=='/'){
+                st.push(ch);
+            }
+            else{
+                if(ch==')'){
+                     bool isRedundant = true;
+                     
+                     while(st.top()!='('){
+                         char top = st.top();
+                        //  if any operator in between means bracket are not redundant
+                         if(top=='+' || top=='-' || top=='*' || top=='/'){
+                             isRedundant= false;
+                         }
+                         st.pop();
+                     }
+                    //  it comes out from while loop when it is encountered with opening bracket 
+                     st.pop();
+                     if(isRedundant==true){
+                         return 1;
+                     }
+                }
+            }
+        }
+        return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        
+    
+        string s; 
+        cin>>s;
+        
+        Solution obj;
+        int res = obj.checkRedundancy(s);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
