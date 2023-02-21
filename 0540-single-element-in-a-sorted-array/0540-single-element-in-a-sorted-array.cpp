9e04cef54d8ad81f1dddeb5c1103791a5e182e9c
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-02-21 14:26:39 +0530

    Time: 34 ms (32.30%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,10 +6,6 @@ public:
          int s =0, e= nums.size()-1;
           int mid =s+(e-s)/2;
  while(s<e){
-             
-        //      if(s==e){
-        //     return nums[s];
-        // }
         if(mid%2==0){
             if(nums[mid] == nums[mid+1]){
                 s =mid+2;
@@ -23,7 +19,7 @@ public:
                 s = mid + 1;;
             }
             else{
-                e =mid;
+                e =mid-2;
             }
         }
     mid =s+(e-s)/2;
