d3c1eec39fd590d174ce23bdcd7618873c2189ab
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-02-25 10:17:41 +0530

    Time: 29 ms (59.47%), Space: 16.9 MB (62.11%) - LeetHub

---
+++ 0641-design-circular-deque/0641-design-circular-deque.cpp
@@ -0,0 +1,131 @@
+class MyCircularDeque {
+public:
+    int *arr;
+    int size;
+    int front;
+    int rear;
+    MyCircularDeque(int k) {
+        size= k;
+        arr= new int[size];
+        front=-1;
+        rear=-1;
+    }
+    
+    bool insertFront(int value) {
+        if(isFull()){
+            return false;
+        }
+        else if(isEmpty()){
+            front=rear=0;
+        }
+        else if(front==0 && rear!=size-1){
+            front=size-1;
+        }
+        else{
+            front--;
+        }
+         arr[front]=value;
+        return true;
+    }
+    
+    bool insertLast(int value) {
+        if(isFull()){
+            return false;
+        }
+        else if(front==-1){
+            front=0;
+            rear=0;
+        }
+        else if(front!=0 && rear==size-1){
+            rear=0;
+        }
+        else{
+            rear++;
+        }
+        arr[rear]=value;
+        return true;
+    }
+    
+    bool deleteFront() {
+        if(isEmpty()){
+            return false;
+        }
+        arr[front]=-1;
+        if(front==rear){
+            front=rear=-1;
+        }
+        else if(front==size-1){
+            front=0;
+        }
+        else{
+            front++;
+        }
+        return true;
+    }
+    
+    bool deleteLast() {
+        if(isEmpty()){
+            return false;
+        }
+        arr[rear]=-1;
+        if(front==rear){
+            front=rear=-1;
+        }
+        else if(rear==0){
+            rear= size-1;
+        }
+        else{
+            rear--;
+        }
+        return true;
+    }
+    
+    int getFront() {
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+            return arr[front];
+        }
+    }
+    
+    int getRear() {
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+            return arr[rear];
+        }
+    }
+    
+    bool isEmpty() {
+        if(front==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    bool isFull() {
+        if((front==0 && rear==size-1) || (front!=0 && rear==(front-1)%(size-1))){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
