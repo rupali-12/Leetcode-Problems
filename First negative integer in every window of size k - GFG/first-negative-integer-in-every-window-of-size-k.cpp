370264026d8828f483e2d8ad2fe9ef1e64a1e453
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-02-26 17:22:54 +0530

    Added solution - LeetHub

--- First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -31,41 +31,86 @@ int main() {
 
 vector<long long> printFirstNegativeInteger(long long int A[],
                                              long long int N, long long int K) {
-         deque<int>dq;
-         vector<long long>ans;
-        for(int i=0; i<K; i++){
-            if(A[i]<0){
-                dq.push_back(i);
-            }
-        }
+    // //   Approach-1> .
+    //     deque<int>dq;
+    //      vector<long long>ans;
+    //     for(int i=0; i<K; i++){
+    //         if(A[i]<0){
+    //             dq.push_back(i);
+    //         }
+    //     }
         
-        // store ans for first window
-        if(dq.size()>0){
-           ans.push_back(A[dq.front()]);
-        }
-        else{
-          ans.push_back(0);    
-        }
+    //     // store ans for first window
+    //     if(dq.size()>0){
+    //       ans.push_back(A[dq.front()]);
+    //     }
+    //     else{
+    //       ans.push_back(0);    
+    //     }
         
-        // Process further windows 
-        for(int i=K; i<N; i++){
-            if(!dq.empty() && (i-dq.front())>=K){
-               dq.pop_front();
-            }
+    //     // Process further windows 
+    //     for(int i=K; i<N; i++){
+    //         if(!dq.empty() && (i-dq.front())>=K){
+    //           dq.pop_front();
+    //         }
             
-            // addition of next index
-            if(A[i]<0){
-                dq.push_back(i);
-            }
+    //         // addition of next index
+    //         if(A[i]<0){
+    //             dq.push_back(i);
+    //         }
             
-            // store ans>>>>
-             if(dq.size()>0){
-           ans.push_back(A[dq.front()]);
-        }
-        else{
-          ans.push_back(0);    
-        }
+    //         // store ans>>>>
+    //          if(dq.size()>0){
+    //       ans.push_back(A[dq.front()]);
+    //     }
+    //     else{
+    //       ans.push_back(0);    
+    //     }
             
-        }
-        return ans;
+    //     }
+    //     return ans;
+    
+    
+//     // >>Approach-2>>>>  only 10055 test passed
+//   vector<long long int>ans;
+//   for(int i=0; i<N-K+1; i++){
+//       int j=0, l=i, flag=0;
+//       while(j<K){
+//           if(A[l]<0){
+//               flag=1;
+//               break;
+//           }
+//           l++; j++;
+//       }   
+//       if(flag==1){
+//           ans.push_back(A[l]);
+//       }
+//       else{
+//           ans.push_back(0);
+//       }
+//   }
+//     return ans;
+    
+    
+    // Approach-3> optimized 
+    int firstNegIndex=0;
+    int firstNegElement;
+    vector<long long int>ans;
+   for(int i=K-1; i<N; i++){
+       while((firstNegIndex<i) && (firstNegIndex<=i-K || A[firstNegIndex]>=0)){
+           firstNegIndex++;
+       }
+       if(A[firstNegIndex]<0){
+           firstNegElement= A[firstNegIndex];
+       }
+       else{
+           firstNegElement=0;
+       }
+       ans.push_back(firstNegElement);
+   }
+   return ans;
  }
+
+
+
+ 
