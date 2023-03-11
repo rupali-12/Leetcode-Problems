8410c20e421d2e4048b9d2b016e979365f032fbf
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-03-11 17:17:49 +0530

    Added solution - LeetHub

--- Sum of the Longest Bloodline of a Tree (Sum of nodes on the longest path from root to leaf node) - GFG/sum-of-the-longest-bloodline-of-a-tree-sum-of-nodes-on-the-longest-path-from-root-to-leaf-node.cpp
+++ Sum of the Longest Bloodline of a Tree (Sum of nodes on the longest path from root to leaf node) - GFG/sum-of-the-longest-bloodline-of-a-tree-sum-of-nodes-on-the-longest-path-from-root-to-leaf-node.cpp
@@ -118,21 +118,40 @@ class Solution
 {
 public:
     void solve(Node* root, int sum, int &maxSum, int len, int &maxLen){
+        //         if(root==NULL){
+        //     return;
+        // }
+        //  sum+= root->data;
+        // len++;
+        // if(len>maxLen){
+        //     maxLen= len;
+        //     maxSum =sum;
+        // }
+        // else if(maxLen==len){
+        //     maxSum = max(sum, maxSum);
+        // }
+       
+        // solve(root->left, sum, maxSum, len, maxLen);
+        // solve(root->right, sum, maxSum, len, maxLen);
+        
+        
+        
+        
+        
         if(root==NULL){
-            return;
-        }
-         sum+= root->data;
-        len++;
-        if(len>maxLen){
+            if(len>maxLen){
             maxLen= len;
             maxSum =sum;
         }
         else if(maxLen==len){
             maxSum = max(sum, maxSum);
         }
+            return;
+        }
+         sum+= root->data;
        
-        solve(root->left, sum, maxSum, len, maxLen);
-        solve(root->right, sum, maxSum, len, maxLen);
+        solve(root->left, sum, maxSum, len+1, maxLen);
+        solve(root->right, sum, maxSum, len+1, maxLen);
     }
     int sumOfLongRootToLeafPath(Node *root)
     {
