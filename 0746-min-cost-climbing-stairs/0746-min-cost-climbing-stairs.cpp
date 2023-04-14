aded17122042fe999f5a77930c5fbf8f24870ccf
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-04-14 11:16:48 +0530

    Time: 4 ms (75.89%), Space: 13.6 MB (97.41%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -17,14 +17,17 @@ public:
         dp[n]= cost[n]+min(solve1(cost, n-1, dp), solve1(cost,n-2, dp));
         return dp[n];
     }
-     int solve2(vector<int>&cost, int n){
-          vector<int>dp(n+1);
-         dp[0]=cost[0];
-         dp[1]=cost[1];
+    
+    // prev1 prev2 curr 
+     int solve3(vector<int>&cost, int n){
+        int prev1=cost[0];
+        int prev2=cost[1];
        for(int i=2; i<n; i++){
-           dp[i]= cost[i]+ min(dp[i-1], dp[i-2]);
+          int curr = cost[i]+ min(prev1, prev2);
+           prev1 = prev2;
+           prev2 = curr;
        }
-         return min(dp[n-1], dp[n-2]);
+         return min(prev1, prev2);
     }
     int minCostClimbingStairs(vector<int>& cost) {
         // // Approach-1>> Without memoization 
@@ -38,8 +41,12 @@ public:
         // int ans =min(solve1(cost, n-1, dp), solve1(cost,n-2, dp));
         // return ans;
         
-         // Approach-3>> Tabulation
+        //  // Approach-3>> Tabulation
+        // int n=cost.size();
+        // return solve2(cost, n);
+        
+           // Approach-3>> using two variables
         int n=cost.size();
-        return solve2(cost, n);
+        return solve3(cost, n);
     }
 };
