475101c590a59f31e546091bc63495e459e8c40d
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-04-20 18:19:35 +0530

    Added solution - LeetHub

---
+++ Get Minimum Squares - GFG/get-minimum-squares.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+	public:
+	int solveRec(int n){
+	    if(n==0){
+	        return 0;
+	    }
+	    int ans =n;
+	    for(int i=1; i*i<=n; i++){
+	        ans = min(ans, 1+solveRec(n- i*i));
+	    }
+	    return ans;
+	}
+	int solveMem(int n, vector<int>&dp){
+	    if(n==0){
+	        return 0;
+	    }
+	    if(dp[n]!=-1){
+	    return dp[n];
+	    }
+	    int ans =n;
+	    for(int i=1; i*i<=n; i++){
+	      ans = min(ans, 1+solveMem(n- i*i, dp));
+	    }
+	    dp[n]=ans;
+	    return dp[n];
+	}
+	int MinSquares(int n)
+	{
+	    // Code here
+	   //// Approach-1> Using Recursion 
+	   //return solveRec(n);
+	   
+	     // Approach-2> Using Recursion + memoization
+	     vector<int>dp(n+1, -1);
+	   return solveMem(n, dp);
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans = ob.MinSquares(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
