b7b549a6f54f7034c857414959a27e00f3539864
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-04-23 11:07:25 +0530

    Time: 4 ms (93.36%), Space: 8.7 MB (16.13%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -23,19 +23,38 @@ public:
     for(int k=i+1; k<=j-1; k++){
    int sum =(values[i]*values[k]*values[j]) + solveMem(values, i, k, dp) + solveMem(values, k, j, dp);
            ans =min(ans, sum);
-        dp[i][j]=ans;
         }
-         // return ans;
+          dp[i][j]=ans;
         return dp[i][j];
+    }
+    int solveTab(vector<int>values){
+        int n= values.size();
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+        // int ans=INT_MAX;
+       for(int i=n-1; i>=0; i--){
+           for(int j=i+2; j<n; j++){
+                int ans=INT_MAX;
+               for(int k=i+1; k<=j-1; k++){
+                int sum =(values[i]*values[k]*values[j]) + dp[i][k] +dp[k][j];
+                   ans =min(ans, sum);
+               }
+                dp[i][j]=ans;
+           }
+       }
+        return dp[0][n-1];
     }
     int minScoreTriangulation(vector<int>& values) {
         
         // // Approach->1 using Recursion 
         // return solveRec(values, 0, values.size()-1);
         
-         // Approach->2 using Recursion + Memoization
+        //  // Approach->2 using Recursion + Memoization
+        // int n= values.size();
+        // vector<vector<int>>dp(n, vector<int>(n, -1));
+        // return solveMem(values, 0, n-1, dp);
+        
+         // Approach->3 using Tabulation
         int n= values.size();
-        vector<vector<int>>dp(n, vector<int>(n, -1));
-        return solveMem(values, 0, n-1, dp);
+        return solveTab(values);
     }
 };
