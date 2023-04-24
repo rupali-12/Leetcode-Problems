e2635a66f76182553298110660e1fb237f8e79be
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-04-24 14:03:41 +0530

    Time: 360 ms (56.67%), Space: 230 MB (73.73%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -48,13 +48,43 @@ public:
         }
         return ans;
     }
+    int solveTab(vector<int>& obstacles){
+        int n=obstacles.size()-1;
+          vector<vector<int>>dp(4, vector<int>(obstacles.size(), 1e9));
+        // we know that
+         dp[0][n]=0;
+         dp[1][n]=0;
+         dp[2][n]=0;
+         dp[3][n]=0;
+        
+        for(int currpos= n-1; currpos>=0; currpos--){
+            for(int currlane=1; currlane<=3; currlane++){
+                if(obstacles[currpos+1]!=currlane){
+                 dp[currlane][currpos] = dp[currlane][currpos+1];
+                }
+                else{
+                    int ans= 1e9;
+                    for(int i=1; i<=3; i++){
+                        if(currlane!=i && obstacles[currpos]!=i){
+                            ans =min(ans, 1+dp[i][currpos+1]);
+                        }
+                    }
+                dp[currlane][currpos]=ans;
+                }
+            }
+        }
+        return min(dp[2][0], min(1+dp[1][0], 1+dp[3][0]));
+    }
     int minSideJumps(vector<int>& obstacles) {
         // // Approach-1>. Using Recursion 
         // return solveRec(obstacles, 2, 0);
         
-        // // Approach-2>. Using Recursion +memoization
-        vector<vector<int>>dp(4, vector<int>(obstacles.size(), -1));
-        // dp[lane][position ]
-        return solveMem(obstacles, 2, 0, dp);
+        // // // Approach-2>. Using Recursion +memoization
+        // vector<vector<int>>dp(4, vector<int>(obstacles.size(), -1));
+        // // dp[lane][position ]
+        // return solveMem(obstacles, 2, 0, dp);
+        
+         // // Approach-3>. Using Tabulation
+        return solveTab(obstacles);
     }
 };
