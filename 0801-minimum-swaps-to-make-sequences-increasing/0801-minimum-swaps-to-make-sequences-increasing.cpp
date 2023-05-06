76aeb7df7eab58856ae9cc841a9ca306cd30f659
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-05-06 11:41:55 +0530

    Time: 340 ms (9.15%), Space: 131.2 MB (28.23%) - LeetHub

---
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int solveRec(vector<int>& nums1, vector<int>& nums2, int index, bool swapped){
+        if(index==nums1.size()){
+            return 0;
+        }
+        int ans =INT_MAX;
+        int prev1=nums1[index-1];
+        int prev2 =nums2[index-1];
+        // catch id swapped is 1 then we have to swap prev1 and prev2 
+        if(swapped){
+            swap(prev1, prev2);
+        }
+        
+        // case of no swap
+       if(nums1[index]>prev1 && nums2[index]>prev2){
+            ans = solveRec(nums1, nums2, index+1, 0);
+        }
+        
+        // case of swap 
+        if(nums1[index]>prev2 && nums2[index]>prev1){
+            ans = min(ans, 1+ solveRec(nums1, nums2, index+1, 1));
+        }
+        return ans;
+    }
+    int solveMem(vector<int>& nums1, vector<int>& nums2, int index, bool swapped, vector<vector<int>>&dp){
+        if(index==nums1.size()){
+            return 0;
+        }
+        if(dp[index][swapped]!=-1){
+            return dp[index][swapped];
+        }
+        int ans =INT_MAX;
+        int prev1= nums1[index-1];
+        int prev2 = nums2[index-1];
+        if(swapped){
+            swap(prev1, prev2);
+        }
+        
+        if(nums1[index]>prev1 && nums2[index]>prev2){
+            ans = solveMem(nums1, nums2, index+1, 0, dp);
+        }
+        
+    if(nums1[index]>prev2 && nums2[index]>prev1){
+        ans = min(ans, 1+ solveMem(nums1, nums2, index+1, 1, dp));
+    }
+     return dp[index][swapped]=ans;
+    }
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        
+        // // Approach-1> Using Recursion 
+        // nums1.insert(nums1.begin(), -1);
+        // nums2.insert(nums2.begin(), -1);
+        // bool swapped=0;
+        // return solveRec(nums1, nums2, 1, swapped);
+        
+         // Approach-1> Using Recursion 
+        nums1.insert(nums1.begin(), -1);
+        nums2.insert(nums2.begin(), -1);
+        int n = nums1.size();
+        vector<vector<int>>dp(n+1, vector<int>(2, -1));
+        bool swapped=0;
+        return solveMem(nums1, nums2, 1, swapped,dp);
+    }
+};
