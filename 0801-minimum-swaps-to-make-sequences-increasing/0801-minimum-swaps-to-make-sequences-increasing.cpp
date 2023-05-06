9b8d7455f3d603601c69b45cdf564b7e3d7c270f
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-05-06 12:00:18 +0530

    Time: 320 ms (24.09%), Space: 120.4 MB (51.93%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -46,20 +46,45 @@ public:
     }
      return dp[index][swapped]=ans;
     }
+    int solveTab(vector<int>& nums1, vector<int>& nums2){
+        int n = nums1.size();
+        vector<vector<int>>dp(n+1, vector<int>(2, 0));
+        for(int index=n-1; index>=1; index--){
+            for(int swapped=1; swapped>=0; swapped--){
+                int ans =INT_MAX;
+                int prev1=nums1[index-1];
+                int prev2 =nums2[index-1];
+                if(swapped){
+                    swap(prev1, prev2);
+                 }
+                
+                if(nums1[index]>prev1 && nums2[index]>prev2){
+                    ans = dp[index+1][0];
+                }
+                
+                if(nums1[index]>prev2 && nums2[index]>prev1){
+                    ans = min(ans, 1 + dp[index+1][1]);
+                }
+                
+                dp[index][swapped]=ans;
+            }
+        }
+        return dp[1][0];
+    }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
-        
+         nums1.insert(nums1.begin(), -1);
+        nums2.insert(nums2.begin(), -1);
+         // bool swapped=0;
         // // Approach-1> Using Recursion 
-        // nums1.insert(nums1.begin(), -1);
-        // nums2.insert(nums2.begin(), -1);
-        // bool swapped=0;
         // return solveRec(nums1, nums2, 1, swapped);
         
-         // Approach-1> Using Recursion 
-        nums1.insert(nums1.begin(), -1);
-        nums2.insert(nums2.begin(), -1);
+        //  // Approach-2> Using Recursion + Memoization 
+        // int n = nums1.size();
+        // vector<vector<int>>dp(n+1, vector<int>(2, -1));
+        // return solveMem(nums1, nums2, 1, swapped,dp);
+        
+          // Approach-2> Using Recursion + Memoization 
         int n = nums1.size();
-        vector<vector<int>>dp(n+1, vector<int>(2, -1));
-        bool swapped=0;
-        return solveMem(nums1, nums2, 1, swapped,dp);
+        return solveTab(nums1, nums2);
     }
 };
