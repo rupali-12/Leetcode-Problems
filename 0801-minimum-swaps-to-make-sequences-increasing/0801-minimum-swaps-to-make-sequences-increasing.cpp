99d8d969364ae9370ac3a9a5095437c768f145b7
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-05-07 11:11:44 +0530

    Time: 181 ms (93.46%), Space: 90.1 MB (98.46%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -71,6 +71,41 @@ public:
         }
         return dp[1][0];
     }
+    int solveSpOpt(vector<int>& nums1, vector<int>& nums2){
+        int n=nums1.size();
+        // f s 
+        // f s
+        // f s
+       int swapp=0, noswap=0, currswap=0, currnoswap=0;
+        for(int index=n-1; index>=1; index--){
+            for(int swapped=1; swapped>=0; swapped--){
+                int ans =INT_MAX;
+                int prev1= nums1[index-1];
+                int prev2 =nums2[index-1];
+                if(swapped){
+                    swap(prev1, prev2);
+                }
+                
+                if(nums1[index]>prev1 && nums2[index]>prev2){
+                    ans = noswap;
+                }
+                  
+                if(nums1[index]>prev2 && nums2[index]>prev1){
+                    ans =min(ans, 1+swapp);
+                }
+                
+                if(swapped){
+                    currswap=ans;
+                }
+                else{
+                    currnoswap =ans;
+                }
+            }
+            swapp= currswap;
+            noswap= currnoswap;
+        }
+        return min(swapp, noswap);
+    }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
          nums1.insert(nums1.begin(), -1);
         nums2.insert(nums2.begin(), -1);
@@ -83,8 +118,12 @@ public:
         // vector<vector<int>>dp(n+1, vector<int>(2, -1));
         // return solveMem(nums1, nums2, 1, swapped,dp);
         
-          // Approach-2> Using Recursion + Memoization 
+        //   // Approach-3> Using Tabulation
+        // int n = nums1.size();
+        // return solveTab(nums1, nums2);
+        
+         // Approach-4> Using Space Optimization
         int n = nums1.size();
-        return solveTab(nums1, nums2);
+        return solveSpOpt(nums1, nums2);
     }
 };
