9317ce6494c4167c3e81e71fb441a998bb69220b
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-06-25 16:55:49 +0530

    Time: 845 ms (22.07%), Space: 217.9 MB (16.19%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -102,16 +102,58 @@ public:
     
     
     // *******************************************************************************
-    
-    int maxProfit(vector<int>& prices) {
-    // Approach-1: 
-        int min_price1=INT_MAX, min_price2= INT_MAX, maxProfit1=0, maxProfit2=0;
-        for(int i=0; i<prices.size(); i++){
-            min_price1= min(min_price1, prices[i]);
-            maxProfit1 = max(maxProfit1, prices[i]-min_price1);
-             min_price2= min(min_price2, prices[i]-maxProfit1);
-            maxProfit2 = max(maxProfit2, prices[i]-min_price2);
+    int solveRec(vector<int>& prices, int index, int buy, int limit){
+        if(index==prices.size()){
+            return 0;
         }
-        return maxProfit2;
+        if(limit==0) return 0;
+        int profit=0;
+        if(buy){
+            profit= max((-prices[index]+solveRec(prices, index+1, 0, limit)), (0+ solveRec(prices, index+1, 1, limit)));
+        }
+        else{
+             profit= max((prices[index]+solveRec(prices, index+1, 1, limit-1)), (0+ solveRec(prices, index+1, 0, limit)));
+        }
+        return profit;
+    }
+    
+    int solveMem(vector<int>& prices, int index, int buy, int limit, vector<vector<vector<int>>>&dp){
+        if(index==prices.size()){
+            return 0;
+        }
+        if(limit==0) return 0;
+        if(dp[index][buy][limit]!=-1){
+            return dp[index][buy][limit];
+        }
+        int profit=0;
+        if(buy){
+            profit = max((-prices[index]+ solveMem(prices, index+1, 0, limit, dp)), (0 +solveMem(prices, index+1, 1, limit, dp)));
+        }
+        else{
+           profit = max((prices[index]+ solveMem(prices, index+1, 1, limit-1, dp)), (0 +solveMem(prices, index+1, 0, limit, dp))); 
+        }
+     return dp[index][buy][limit]=profit;                   
+    }
+    int maxProfit(vector<int>& prices) {
+    // // Approach-1: 
+    //     int min_price1=INT_MAX, min_price2= INT_MAX, maxProfit1=0, maxProfit2=0;
+    //     for(int i=0; i<prices.size(); i++){
+    //         min_price1= min(min_price1, prices[i]);
+    //         maxProfit1 = max(maxProfit1, prices[i]-min_price1);
+    //          min_price2= min(min_price2, prices[i]-maxProfit1);
+    //         maxProfit2 = max(maxProfit2, prices[i]-min_price2);
+    //     }
+    //     return maxProfit2;
+        
+        // Approach-2: Using DP 
+                int n=prices.size();
+        // // method-1: Using Recursion
+        // // (index, buy, limit)
+        // return solveRec(prices, 0, 1, 2);
+        
+        // method-2: Using Recursion +Memoization 
+        // // (index, buy, limit)
+       vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        return solveMem(prices, 0, 1, 2, dp);
     }
 };
