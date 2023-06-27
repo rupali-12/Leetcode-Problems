aaa4b936287615f6186258416e6fd629b9991fb8
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-06-27 19:29:12 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (31.45%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int>ans;
+        int carry=0;
+        for(int i=digits.size()-1; i>=0; i--){
+            if(digits[i]<9){
+                if(carry){
+                  digits[i]+=carry;
+                    // carry=0;
+                }
+                 if(i==digits.size()-1){
+                      digits[i]+=1;
+                 }
+                return digits;
+            }
+            else{
+                carry=1;
+                digits[i]=0;
+            }
+        }
+        if(carry){
+            ans.push_back(carry);
+        }
+        for(int i=0; i<digits.size(); i++){
+            ans.push_back(digits[i]);
+        }
+        return ans;
+    }
+};
