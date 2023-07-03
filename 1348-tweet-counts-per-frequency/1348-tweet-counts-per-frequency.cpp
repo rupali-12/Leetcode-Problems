94d34f6f5e015542a149a22f2dc6b9ce2e18625b
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-03 16:14:31 +0530

    Time: 99 ms (71.53%), Space: 38.2 MB (73.72%) - LeetHub

---
+++ 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.cpp
@@ -0,0 +1,48 @@
+class TweetCounts {
+public:
+    unordered_map<string, vector<int>>mp;
+    TweetCounts() {
+        // mp.clear();
+    }
+    
+    void recordTweet(string tweetName, int time) {
+        mp[tweetName].push_back(time);
+    }
+    
+    vector<int> getTweetCountsPerFrequency(string freq, string tweetName, int st, int et) {
+       if(freq=="minute"){
+           vector<int>ans((et-st)/60 +1, 0);
+           for(auto a: mp[tweetName]){
+               if(a>=st && a<=et){
+                   ans[(a-st)/60]++;
+               }
+           }
+           return ans;
+       }  
+        else if(freq=="hour"){
+             vector<int>ans((et-st)/3600 +1, 0);
+           for(auto a: mp[tweetName]){
+               if(a>=st && a<=et){
+                   ans[(a-st)/3600]++;
+               }
+           }
+           return ans;
+        }
+        else{
+             vector<int>ans((et-st)/86400 +1, 0);
+           for(auto a: mp[tweetName]){
+               if(a>=st && a<=et){
+                   ans[(a-st)/86400]++;
+               }
+           }
+           return ans;
+        }
+    } 
+};
+
+/**
+ * Your TweetCounts object will be instantiated and called as such:
+ * TweetCounts* obj = new TweetCounts();
+ * obj->recordTweet(tweetName,time);
+ * vector<int> param_2 = obj->getTweetCountsPerFrequency(freq,tweetName,startTime,endTime);
+ */
