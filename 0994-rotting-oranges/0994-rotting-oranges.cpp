3bd9dd61a3293c3937f53929590093a3ddbc5beb
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-03 19:48:27 +0530

    Time: 4 ms (85.52%), Space: 13.2 MB (51.27%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,6 +3,7 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n= grid.size();
         int m = grid[0].size();
+        int totalFresh=0;
         
         queue<pair<pair<int, int>, int>>q;
     // int vis[n][m];
@@ -13,10 +14,11 @@ public:
                     q.push({{i, j}, 0});
                     vis[i][j]=2;
                 }
+                if(grid[i][j]==1){totalFresh++;}
             }
         }
         
-        int time=0;
+        int time=0, cnt=0;
         int delRow[]={-1, 0, 1, 0};
         int delCol[]= {0, 1, 0, -1};
         while(!q.empty()){
@@ -32,18 +34,24 @@ public:
                  int neighCol = c + delCol[i];
                 if(neighRow>=0 && neighRow<n && neighCol>=0 && neighCol<m && vis[neighRow][neighCol]!=2 && grid[neighRow][neighCol]==1){
                     q.push({{neighRow, neighCol}, t+1});
-                    vis[neighRow][neighCol]=2;
+                    vis[neighRow][neighCol]=2;\
+                        cnt++;
                 }
             }   
         }
         
         // check if all fresh oranges were converted into rotten or not
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(vis[i][j]!=2 && grid[i][j]==1){
-                    return -1;
-                }
-            }
+        // instead of this check we can count te fresh oranges when we put in queue and compare it with total fresh oranges if its not equal then return -1 
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(vis[i][j]!=2 && grid[i][j]==1){
+        //             return -1;
+        //         }
+        //     }
+        // }
+        
+        if(cnt!=totalFresh){
+            return -1;
         }
         return time;
     }
