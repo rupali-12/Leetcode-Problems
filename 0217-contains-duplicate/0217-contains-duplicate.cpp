c2e2f1b3fb73b4ebf9f7dffa0b62088c19a699e5
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-03 20:07:23 +0530

    Time: 134 ms (88.38%), Space: 57.2 MB (93.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int>mp;
-        for(auto a: nums){
-            mp[a]++;
-        }
-        for(auto a: mp){
-            if(a.second>1){
+        // Approach-1 using map
+        // map<int, int>mp;
+        // for(auto a: nums){
+        //     mp[a]++;
+        // }
+        // for(auto a: mp){
+        //     if(a.second>1){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
+        // Approach-2> using sorting
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i]==nums[i+1]){
                 return true;
             }
         }
