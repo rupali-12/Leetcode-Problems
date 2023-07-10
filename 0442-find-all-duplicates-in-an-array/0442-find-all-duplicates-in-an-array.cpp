55b1c38eae15cfbde7815e4aa4e0022be7a8a4b3
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-10 19:47:07 +0530

    Time: 84 ms (44.96%), Space: 33.5 MB (88.67%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,16 +1,45 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<int>a;
-        map<int, int>m;
-        for(auto x: nums){
-            m[x]++;
-        }
-        for(auto x:m){
-            if(x.second>1){
-                a.push_back(x.first);
+        
+        // Approach-1: Using map
+        // vector<int>a;
+        // map<int, int>m;
+        // for(auto x: nums){
+        //     m[x]++;
+        // }
+        // for(auto x:m){
+        //     if(x.second>1){
+        //         a.push_back(x.first);
+        //     }
+        // }
+        // return a;
+        
+        // Approach-2: Using sorting
+        vector<int>ans;
+        sort(nums.begin(), nums.end());
+        pair<int,int>p= {nums[0], 1};
+        
+         if(nums.size()==1){
+             return ans;
+         }
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]!= nums[i-1]){
+                if(p.second>1){
+                    ans.push_back(p.first);
+                }
+                 p= {nums[i], 1};
+            }
+            else{
+                p.second++;
+                if(i==nums.size()-1){
+                    ans.push_back(p.first);
+                }
             }
         }
-        return a;
+        if(ans.size()==0 && p.second>1){
+            ans.push_back(nums[0]);
+        }
+        return ans;
     }
 };
