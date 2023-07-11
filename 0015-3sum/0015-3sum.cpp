30a5d28dc8fb6d8f49a1c2f607c08d5345e93f2d
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-11 18:54:35 +0530

    Time: 153 ms (78.99%), Space: 24.1 MB (64.27%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,32 +3,34 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>>ans;
         sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size(); i++){
-            int a =nums[i];
-            int t=0-a;
-            int start=i+1, end=nums.size()-1;
-            while(start<end){
-                if(nums[start]+nums[end]==t){
-                    ans.push_back({nums[i], nums[start], nums[end]});
-                    while(start<end && nums[start]==nums[start+1]){
-                        start++;
-                    }
-                    while(start<end && nums[end]==nums[end-1]){
-                        end--;
-                    }
-                    start++; end--;
-                }
-                else if(nums[start]+nums[end]<t){
-                    start++;
-                }
-                else{
-                    end--;
-                }
-            }
-            while((i+1)<nums.size() && (nums[i+1]==nums[i])){
-                i++;
-            }            
-        }
+       for(int i=0; i<nums.size(); i++){
+           int a = nums[i];
+           int t= 0-a;
+           int start=i+1, end= nums.size()-1;
+           while(start<end){
+               if(nums[start]+nums[end]==t){
+                   ans.push_back({nums[i], nums[start], nums[end]});
+                   while(start<end && nums[start]==nums[start+1]){
+                       start++;
+                   }
+                    while(end<end && nums[end]==nums[end-1]){
+                       end--;
+                   }
+                   start++; end--;
+               }
+               else if(nums[start]+nums[end]<t){
+                   start++;
+               }
+               else{
+                   end--;
+               }
+           }
+           
+           // process all elements which is equal to value at current index 
+           while(i+1<nums.size()-1 && nums[i]==nums[i+1]){
+               i++;
+           }
+       }
         return ans;
     }
 };
