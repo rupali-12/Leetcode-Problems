3eee4d8514c435ad24d8b95a36b82afc0936f0c2
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-11 18:38:54 +0530

    Time: 316 ms (77.73%), Space: 92.2 MB (52.23%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -25,15 +25,17 @@ public:
       priority_queue<pair<int, int>>p;
       int ans=INT_MIN;
       for(int i=0; i<points.size(); i++){
-          while(!p.empty() && (points[i][0]-p.top().second)>k){
+          while(!p.empty() && (points[i][0]- p.top().second)>k){
               p.pop();
           }
           if(!p.empty()){
-              ans= max(ans, (points[i][1]+points[i][0]+ p.top().first));
+              ans= max(ans, (points[i][1]+points[i][0]+p.top().first));
           }
           p.push({points[i][1]-points[i][0], points[i][0]});
       }
       return ans;
 }
-
 };
+
+
+
