2a030612bfd733f1ca347f0859f32e8fd51bdfb0
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-12 23:19:25 +0530

    Time: 4 ms (36.60%), Space: 8.9 MB (80.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,21 +3,25 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         vector<int>ans = nums1;
         int i=0, j=0, k=0; 
-        while(i<m && j<n){
-                if(ans[i]<=nums2[j]){
-                nums1[k]= ans[i];
-                i++; k++;
-            }
-            else{
-                nums1[k]= nums2[j];
-                j++;k++;
+       while(i<m && j<n){
+           if(ans[i]<nums2[j]){
+               nums1[k]= ans[i];
+               k++; i++;
            }
-        }
+           else{
+               nums1[k]=nums2[j];
+               k++; j++;
+           }
+       }
         while(i<m){
-            nums1[k++]= ans[i++];
+            nums1[k]=ans[i];
+            k++; i++;
         }
         while(j<n){
-            nums1[k++]= nums2[j++];
+            nums1[k]=nums2[j];
+            k++; j++;
         }
     }
 };
+
+
