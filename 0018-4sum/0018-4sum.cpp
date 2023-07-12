14453eacda502b70d2faf10bba7a37d0f59cdc3e
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-12 20:00:28 +0530

    Time: 124 ms (37.72%), Space: 13.1 MB (72.62%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,73 +1,45 @@
 class Solution {
 public:
-//     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-//         vector<vector<int>>ans;
-//         int n= nums.size();
-//         sort(nums.begin(), nums.end());
-//         for(int i=0; i<n-3;i++){
-//             if(i>0 && nums[i]==nums[i-1]){
-//                 continue;
-//             }
-//             for(int j=i+1; j<n-2; j++){
-//                   if(j>i+1 && nums[j]==nums[j-1]){
-//                 continue;
-//             }
-                
-//                   long long tSum = (long long)target - ((long long)nums[i] + (long long)nums[j]);
-//                 int l = j + 1, h = n - 1;
-//                 while(l<h){
-//                     if(nums[l] + nums[h]==tSum){
-//                     ans.push_back({nums[i], nums[j], nums[l], nums[h]});
-//                           while(l<h && nums[l]==nums[l+1]){
-//                 l++;
-//             }
-//                         while(l < h && nums[h] == nums[h - 1]){
-//                             h--;
-//                         }
-//                         l++;
-//                         h--;
-//                     }
-//                     else if(nums[l] + nums[h] > (target- (nums[i]+ nums[j]))){
-//                         h--;
-//                     }
-//                     else{
-//                         l++;
-//                     }
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-    
-    // ******************************************************************************
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
+        vector<vector<int>>ans;
+        int n= nums.size();
         sort(nums.begin(), nums.end());
-        vector<vector<int>> output;
-        for(int i=0; i<n-3; i++){
+        for(int i=0; i<n-3;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
             for(int j=i+1; j<n-2; j++){
-                long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
-                int low = j+1, high = n-1;
-                while(low < high){
-                    if(nums[low] + nums[high] < newTarget){
-                        low++;
+                  if(j>i+1 && nums[j]==nums[j-1]){
+                continue;
+            }
+                
+                  long long tSum = (long long)target - ((long long)nums[i] + (long long)nums[j]);
+                int l = j + 1, h = n - 1;
+                while(l<h){
+                    if(nums[l] + nums[h]==tSum){
+                    ans.push_back({nums[i], nums[j], nums[l], nums[h]});
+                          while(l<h && nums[l]==nums[l+1]){
+                l++;
+            }
+                        while(l < h && nums[h] == nums[h - 1]){
+                            h--;
+                        }
+                        l++;
+                        h--;
                     }
-                    else if(nums[low] + nums[high] > newTarget){
-                        high--;
+                    else if(nums[l] + nums[h] > tSum){
+                        h--;
                     }
                     else{
-                        output.push_back({nums[i], nums[j], nums[low], nums[high]});
-                        int tempIndex1 = low, tempIndex2 = high;
-                        while(low < high && nums[low] == nums[tempIndex1]) low++;
-                        while(low < high && nums[high] == nums[tempIndex2]) high--;
+                        l++;
                     }
                 }
-                while(j+1 < n && nums[j] == nums[j+1]) j++;
             }
-            while(i+1 < n && nums[i] == nums[i+1]) i++;
         }
-        return output;
+        return ans;
     }
+    
+   
 };
 
 
