06fb035fcd292bdf10f350e915743fd21ab5a72d
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-15 14:22:04 +0530

    Time: 7 ms (32.68%), Space: 5.9 MB (90.53%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-int s=0, e=x-1;
-      long long int ans=-1;
-      long long int mid= s+(e-s)/2;
-        if(x==0 || x==1) return x;
-        while(s<=e){
-          long long int sq= mid*mid;
-            if(sq==x){
-                return mid;
-            }
-            else if(sq<x){
-                ans= mid;
-                s= mid+1;
-            }
-            else{
-                e= mid-1;
-            }
-            mid= s+(e-s)/2;
+// if you want precision after decimal 
+double morePrecision(int n, int prec, int tempSol){
+    double factor=1;
+    double ans= tempSol;
+    for(int i=1; i<=prec; i++){
+        factor = factor/10;
+        for(double j=ans; j*j<n; j+=factor){
+           ans=j;
         }
-        return ans;
     }
+    return ans;
+}
+int sqrt(int N){
+      int s=0, e= N;
+ long long mid = s+(e-s)/2;
+    int ans=-1;
+    if(N==0 || N==1){
+        return N;
+    }
+while(s<=e){
+    if(mid*mid==N){
+        return mid;
+    }
+    else if(mid*mid < N){
+        ans=mid;
+        s= mid+1;
+    }
+    else{
+        e =mid-1;
+    }
+    mid= s+(e-s)/2;
+}
+    return ans;
+}
+
+int  mySqrt(int N)
+{
+    int tempSol = sqrt(N);
+    
+    return morePrecision(N, 3, tempSol);
+}
+
 };
-
-
-
-       
-
-       
