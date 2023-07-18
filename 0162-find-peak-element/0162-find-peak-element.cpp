cef4752860b0a1c0ea4fcf32bfa65d2e50d7e81e
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-18 17:15:50 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (51.53%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,7 +5,7 @@ public:
         if(n==1){
             return 0;
         }
-    
+       // handle extreme cases 
          if(nums[0]>nums[1])     return 0;
         if(nums[n-1]>nums[n-2])         return n-1;
     
