cf3deec25ff458ad31be2a34e1f56f07743b712c
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-18 18:14:40 +0530

    Time: 3 ms (57.33%), Space: 7.2 MB (60.86%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,30 +1,63 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int k) {
-        int n= nums.size();
-        int mx=INT_MIN, sum=0;
-        
+    bool isPossible(vector<int>& nums, int n, int m, int mid){
+        int currSum=0, numsArray=1;
         for(int i=0; i<n; i++){
-            sum+= nums[i];
-            if(nums[i]>mx){
-                mx= nums[i];
+            if(currSum+nums[i]<=mid){
+                currSum+=nums[i];
+            }
+            else{
+                numsArray++;
+                if(numsArray>m || nums[i]>mid){
+                    return false;
+                }
+                currSum= nums[i];
             }
         }
+        return true;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int n= nums.size();
+//         int mx=INT_MIN, sum=0;
         
-        int s=mx, e= sum, ans=-1;
+//         for(int i=0; i<n; i++){
+//             sum+= nums[i];
+//             if(nums[i]>mx){
+//                 mx= nums[i];
+//             }
+//         }
+        
+//         int s=mx, e= sum, ans=-1;
+//         while(s<=e){
+//             int mid= s+(e-s)/2;
+            
+//      int currSum=0, numArrays=1;
+//             for(int i=0; i<n; i++){
+//                currSum+= nums[i];
+//                 if(currSum>mid){
+//                     numArrays++;
+//                     currSum= nums[i];
+//                 }
+//             }
+            
+//             if(numArrays<=k){
+//                 ans= mid;
+//                 e= mid-1;
+//             }
+//             else{
+//                 s= mid+1;
+//             }
+//         }
+//         return ans;
+        
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+= nums[i];
+        }
+        int s=0, e= sum, ans=-1;
         while(s<=e){
             int mid= s+(e-s)/2;
-            
-     int currSum=0, numArrays=1;
-            for(int i=0; i<n; i++){
-               currSum+= nums[i];
-                if(currSum>mid){
-                    numArrays++;
-                    currSum= nums[i];
-                }
-            }
-            
-            if(numArrays<=k){
+            if(isPossible(nums, n, k, mid)){
                 ans= mid;
                 e= mid-1;
             }
