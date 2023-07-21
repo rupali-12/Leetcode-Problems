cc4c66f4c318359f7ee94a0d5e580114252ddf4f
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-07-21 11:32:11 +0530

    Time: 345 ms (30.78%), Space: 10.2 MB (52.05%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,16 +1,44 @@
 class Solution {
 public:
-
+    // Give TLE for large inputs 
+// bool isPrime(int n){
+//     if(n==0 || n==1){
+//         return false;
+//     }
+    
+//         for(int i=2; i<=n/2; i++){
+//             if(n%i==0){
+//                 return false;
+//             }
+//         }
+//         return true;
+// }
+//     int countPrimes(int n) {
+//         int count=0;
+//       for(int i=1; i<n; i++){
+//           if(isPrime(i)){
+//               count++;
+//           }
+//       }
+//         return count;
+//     }
+    
+ // Approach-2>> sieve method 
     int countPrimes(int n) {
-     int count=0; 
-        vector<bool>res(n+1, true);
-        res[0]=res[1]= false;
+        int count=0;
+        if(n==0 || n==1){
+            return 0;
+        }
+      vector<bool>isPrime(n+1,true);
+        isPrime[0]=isPrime[1]=false;
         for(int i=2; i<n; i++){
-            if(res[i]){
+            int j=1;
+            if(isPrime[i]){
                 count++;
-                for(int j=2*i; j<=n; j=j+i){
-                    res[j]=false;
-                }
+                 while(i*j <n){
+                isPrime[i*j]=false;
+                j++;
+            }
             }
         }
         return count;
