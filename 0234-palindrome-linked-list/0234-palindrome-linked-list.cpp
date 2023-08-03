28f12a1cd2e23d7adb300ed41cc38e09b8fa3db4
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-03 10:37:29 +0530

    Time: 164 ms (98.10%), Space: 116.3 MB (66.92%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,24 +10,71 @@
  */
 class Solution {
 public:
-     bool isPalin(vector<int>arr){
-        int i=0, j=arr.size()-1;
-        while(i<=j){
-            if(arr[i]!=arr[j]){
-                return false;
-            }
-            i++; j--;
-        }
-        return true;
-    }
-    bool isPalindrome(ListNode* head) {
-          // Approach-1> using array
-        vector<int>arr;
+    // Approach-1
+    //  bool isPalin(vector<int>arr){
+    //     int i=0, j=arr.size()-1;
+    //     while(i<=j){
+    //         if(arr[i]!=arr[j]){
+    //             return false;
+    //         }
+    //         i++; j--;
+    //     }
+    //     return true;
+    // }
+    
+    // Approach-2
+    int getLength(ListNode*head){
+        int count=0;
         ListNode* temp= head;
         while(temp!=NULL){
-            arr.push_back(temp->val);
+            count++;
             temp= temp->next;
         }
-        return isPalin(arr);
+        return count;
+    }
+    ListNode* findMid(ListNode* head, int count){
+        int len=0;
+        ListNode* temp=head;
+        while(len<count/2){
+            temp= temp->next;
+            len++;
+        }
+        return temp;
+    }
+    ListNode* reverse(ListNode* head){
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        auto ans= reverse(head->next);
+        head->next->next= head;
+        head->next=NULL;
+        return ans;
+    }
+    bool isPalindrome(ListNode* head) {
+        //   // Approach-1> using array
+        // vector<int>arr;
+        // ListNode* temp= head;
+        // while(temp!=NULL){
+        //     arr.push_back(temp->val);
+        //     temp= temp->next;
+        // }
+        // return isPalin(arr);
+        
+        // Approach-> 2->Find mid , Revrse after mid and compare the two halves
+        int length= getLength(head);
+        ListNode* mid= findMid(head, length);
+        ListNode* revList =reverse(mid);
+        ListNode* rev= revList;
+        ListNode* temp= head;
+        
+        while(temp!=mid && rev!=NULL){
+            if(rev->val!= temp->val){
+                return false;
+            }
+            rev= rev->next;
+            temp= temp->next;
+        }
+        
+        return true;
     }
 };
