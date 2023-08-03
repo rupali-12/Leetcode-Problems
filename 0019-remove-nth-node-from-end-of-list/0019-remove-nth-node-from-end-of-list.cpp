26e78686e877149a7647378b4d204bfee45d393e
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-03 12:13:36 +0530

    Time: 4 ms (52.81%), Space: 10.6 MB (87.83%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getLength(ListNode* head){
+        int cnt=0;
+        ListNode* temp= head;
+        while(temp!=NULL){
+            cnt++;
+            temp= temp->next;
+        }
+        return cnt;
+    }
+    void deleteNode(ListNode* &head, int count, int length){
+        if(count==1){
+           //  ListNode* temp= head->next;
+           //  head->next=NULL;
+           // head= temp;
+           //  return;
+            head= head->next;
+            cout<<head->val;
+            return;
+        }
+        // cout<<count<<length;
+        ListNode* temp= head;
+        int i=1;
+        while(i<count-1){
+            i++;
+            temp=temp->next;
+        }
+        temp->next= temp->next->next;
+        return;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head==NULL){
+            return head;
+        }
+        if(head->next==NULL && n==1){
+            return NULL;
+        }
+        int length= getLength(head);
+        int cntFromStart= length-n+1;
+        if(length>=n){
+              deleteNode(head, cntFromStart, length);
+        }
+        return head;
+    }
+};
