318e07a44b0c50dec9f7f7f17da0100f4dea5ed0
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-03 17:10:12 +0530

    Time: 35 ms (5.58%), Space: 8.9 MB (47.48%) - LeetHub

---
+++ 1117-building-h2o/1117-building-h2o.cpp
@@ -0,0 +1,34 @@
+class H2O {
+    private:
+    mutex m; 
+    condition_variable cv;
+    int turn;
+public:
+    H2O() {
+        turn =0;
+    }
+
+    void hydrogen(function<void()> releaseHydrogen) {
+        
+        // releaseHydrogen() outputs "H". Do not change or remove this line.
+        unique_lock<mutex>lock(m);
+        while(turn==2){
+            cv.wait(lock);
+        }
+        releaseHydrogen();
+        turn++;
+        cv.notify_all();
+    }
+
+    void oxygen(function<void()> releaseOxygen) {
+        
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+         unique_lock<mutex>lock(m);
+        while(turn<2){
+            cv.wait(lock);
+        }
+         releaseOxygen();
+          turn=0;
+        cv.notify_all();
+    }
+};
