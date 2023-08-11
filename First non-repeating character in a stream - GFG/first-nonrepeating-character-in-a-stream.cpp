6d13170da0439a3f52bb44a5a7c66055b54a6d62
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-12 00:36:36 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -7,32 +7,34 @@ class Solution {
 	public:
 		string FirstNonRepeating(string A){
 		    // Code here
-		   queue<char>q;
+		    unordered_map<char, int>mp;
+		    queue<int>q;
 		    string ans="";
-		    map<char, int>count;
 		    for(int i=0; i<A.length(); i++){
-		        char ch =A[i];
-		        count[ch]++;
-		        q.push(ch);
+		        char ch= A[i];
 		        
-		      //  check for q.front is repeating or non repeating 
-		        while(!q.empty()){
-		            if(count[q.front()]>1){
-		                q.pop();
-		            }
-		            else{
-		                ans+=q.front();
-		                break;
-		            }
-		        }
-		        
-		      //  case q.empty means there is no  non-repeating character  
-		        if(q.empty()){
+		      //  increase count 
+		       mp[A[i]]++;
+		       
+		       q.push(ch);
+		       
+		       while(!q.empty()){
+		           if(mp[q.front()]>1){
+		              // means repeating character 
+		              q.pop();
+		           }
+		           else{
+		               ans+=q.front();
+		               break;
+		           }
+		       }
+		       if(q.empty()){
 		            ans+="#";
-		        }
+		       }
 		    }
-		    	return ans;
+		  return ans;
 		}
+
 };
 
 //{ Driver Code Starts.
