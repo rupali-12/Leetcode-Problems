708e33317ed21c10e84cce60cea6f45f6be2cc28
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-13 17:31:18 +0530

    Added solution - LeetHub

--- Circular tour - GFG/circular-tour.cpp
+++ Circular tour - GFG/circular-tour.cpp
@@ -27,20 +27,19 @@ class Solution{
     int tour(petrolPump p[],int n)
     {
        //Your code here
-        int deficit=0;
-      int balance=0, start=0;
-      for(int i=0; i<n; i++){
-          balance+= p[i].petrol - p[i].distance;
-          if(balance<0){
-              deficit+= balance;
-              balance=0;
-              start= i+1;
-          }
+int deficit=0, balance=0, start=0;
+  for(int i=0; i<n; i++){
+      balance+= p[i].petrol - p[i].distance;
+      if(balance<0){
+          deficit+= balance;
+          balance=0;
+          start= i+1;
       }
-      if(deficit+balance>=0){
-          return start;
-      }
-      return -1;
+  }
+  if(deficit+balance >=0){
+      return start;
+  }
+  return -1;
     }
 };
 
