54bbac3293dc81543158b1a1f85843bd53f9ed20
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-13 15:35:39 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (89.51%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,47 +1,95 @@
+// class MyQueue {
+//     private:
+//      int *arr;
+//       int size;
+//       int frnt, rear;
+// public:
+//     MyQueue() {
+//          size= 100001;
+//        arr= new int[size];
+//        frnt=rear=0;
+//     }
+    
+//     void push(int x) {
+//           if(rear==size)
+//            return;
+        
+//         arr[rear]=x;
+//         rear++;
+//         return;
+//     }
+    
+//     int pop() {
+//         if(frnt==rear){
+//             return -1;
+//         }
+//         int temp=arr[frnt];
+//         arr[frnt]=-1;
+//         frnt++;
+//         return temp;
+//     }
+    
+//     int peek() {
+//             if(frnt==-1){
+//             return -1;
+//         }
+//         return arr[frnt];
+//     }
+    
+//     bool empty() {
+//           if(frnt==rear){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+// /**
+//  * Your MyQueue object will be instantiated and called as such:
+//  * MyQueue* obj = new MyQueue();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->peek();
+//  * bool param_4 = obj->empty();
+//  */
+
+
+// ***************************************************************************
 class MyQueue {
     private:
-     int *arr;
-      int size;
-      int frnt, rear;
+    stack<int>s;
 public:
     MyQueue() {
-         size= 100001;
-       arr= new int[size];
-       frnt=rear=0;
     }
     
     void push(int x) {
-          if(rear==size)
-           return;
-        
-        arr[rear]=x;
-        rear++;
+        stack<int>temp;
+     while(!s.empty()){
+         temp.push(s.top());
+         s.pop();
+     }
+        s.push(x);
+          while(!temp.empty()){
+         s.push(temp.top());
+         temp.pop();
+     }
         return;
     }
     
     int pop() {
-        if(frnt==rear){
-            return -1;
-        }
-        int temp=arr[frnt];
-        arr[frnt]=-1;
-        frnt++;
+        int temp= s.top();
+        s.pop();
         return temp;
     }
     
     int peek() {
-            if(frnt==-1){
-            return -1;
-        }
-        return arr[frnt];
+        return s.top();
     }
     
     bool empty() {
-          if(frnt==rear){
-            return true;
-        }
-        return false;
+        return s.empty();
     }
+    
 };
 
 /**
