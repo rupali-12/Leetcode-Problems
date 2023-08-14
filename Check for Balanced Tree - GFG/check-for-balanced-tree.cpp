652e808a6570ca0fa6a4bd616610822fb47c2e39
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-14 22:45:31 +0530

    Added solution - LeetHub

--- Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
+++ Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
@@ -112,22 +112,55 @@ class Solution{
         int right = height(root->right);
         return max(left, right) +1;
     }
-    bool isBalanced(Node *root)
+    // bool isBalanced(Node *root)
+    // {
+    //     //  Your Code here
+    //     if(root==NULL){
+    //         return true;
+    //     }
+    //     int left= height(root->left);
+    //     int right= height(root->right);
+    //     if(abs(left-right)>1){
+    //         return false;
+    //     }
+    //     int ansleft = isBalanced(root->left);
+    //     int ansRight =isBalanced(root->right);
+    //     return ansleft && ansRight;
+        
+    // }
+    
+    // Approach-2> Optimized Approach 
+    pair<bool, int> isBalancedFast(Node *root)
     {
         //  Your Code here
+        
         if(root==NULL){
-            return true;
+            pair<bool, int>p =  make_pair(true, 0);
+            return p;
         }
-        int left= height(root->left);
-        int right= height(root->right);
-        if(abs(left-right)>1){
-            return false;
-        }
-        int ansleft = isBalanced(root->left);
-        int ansRight =isBalanced(root->right);
-        return ansleft && ansRight;
+         pair<bool, int>left= isBalancedFast(root->left);
+         pair<bool, int>right= isBalancedFast(root->right);
+         bool leftAns = left.first;
+         bool rightAns = right.first;
+       bool diff = abs(left.second- right.second)<=1;
+       pair<bool,int>ans;
+       ans.second = max(left.second, right.second)+1;
+       if(leftAns && rightAns && diff){
+           ans.first =true;
+       }
+       else{
+        ans.first =false;   
+       }
+       return ans;
+    }
+      bool isBalanced(Node *root)
+    {
+        //  Your Code here
+pair<bool, int>ans = isBalancedFast(root);
+return ans.first;
         
     }
+    
 };
 
 
