2ab1a68d0dc2ed7e52317c146610c26ae9bad9c5
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-14 20:16:40 +0530

    Added solution - LeetHub

--- Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -93,56 +93,25 @@ struct Node
 
 class Solution {
   public:
-  int height(struct Node* node){
-        // code here 
-        int h=0;
-        if(node==NULL){
+    // Function to return the diameter of a Binary Tree.
+    int height(Node* root){
+        if(root==NULL){
             return 0;
         }
-        int leftH = height(node->left);
-        int rightH = height(node->right);
-        
-        h=max(leftH, rightH)+1;
-        return h;
+        int left = height(root->left);
+        int right = height(root->right);
+        return max(left, right) +1;
     }
-    
-    // Approach-2> 
-    pair<int, int>diameterFast(Node*root){
-        if(root==NULL){
-            pair<int, int>p = make_pair(0,0);
-            return p;
-        }
-        pair<int, int> left = diameterFast(root->left);
-       pair<int, int> right = diameterFast(root->right);
-        int op1 = left.first;
-        int op2 =right.first;
-        int op3 = left.second + right.second +1;
-        
-        pair<int, int>ans;
-        ans.first = max(op1, max(op2, op3));
-        ans.second = max(left.second, right.second)+1;
-        return ans;
-    }
-    // Function to return the diameter of a Binary Tree.
     int diameter(Node* root) {
         // Your code here
-        
-        // // Approach-1 > using height --TC= O(n^2)
-        // int op1, op2, op3;
-        // if(root==NULL){
-        //     return 0;
-        // }
-        
-        // op1 = diameter(root->left);
-        // op2 = diameter(root->right);
-        // op3 = height(root->left) +height(root->right)+1;
-        
-        // int ans =max(op1, max(op2, op3));
-        // return ans;
-        
-        
-        // Approach-2 >> using pair -- TC=O(n)
-        return diameterFast(root).first;
+        if(root==NULL){
+            return 0;
+        }
+        int left =diameter(root->left);
+        int right= diameter(root->right);
+       int combRightLeft = height(root->left) + height(root->right) +1;
+        int ans= max(left, max(right, combRightLeft));
+        return ans;
     }
 };
 
