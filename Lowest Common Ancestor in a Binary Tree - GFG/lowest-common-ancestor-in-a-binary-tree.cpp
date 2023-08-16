00c040df326667684d8dd050708fbc5277ef0d5e
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-16 17:16:05 +0530

    Added solution - LeetHub

--- Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp
+++ Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp
@@ -41,28 +41,21 @@ class Solution
     Node* lca(Node* root ,int n1 ,int n2 )
     {
        //Your code here 
-      if(root==NULL){
-          return NULL;
-      }
-      
-      if(root->data==n1 || root->data==n2){
-          return root;
-      }
-      Node* left = lca(root->left, n1, n2);
-      Node* right = lca(root->right, n1, n2);
-      
-      if(left!=NULL && right!=NULL){
-          return root;
-      }
-      else if((left!=NULL && right==NULL)|| (left==NULL && right!=NULL)){
-          if(left==NULL){
-          return right;
-          }
-          else{
-              return left;
-          }
-      }
-    //   return NULL;
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->data==n1 || root->data==n2){
+        return root;
+    }
+    Node* leftAns = lca(root->left, n1, n2);
+    Node* rightAns= lca(root->right, n1, n2);
+    if(leftAns!=NULL && rightAns!=NULL){
+        return root;
+    }
+    else if(leftAns!=NULL && rightAns==NULL){
+        return leftAns;
+    }
+    return rightAns;;
     }
 };
 
