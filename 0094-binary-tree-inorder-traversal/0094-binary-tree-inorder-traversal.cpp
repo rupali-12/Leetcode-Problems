4ab0b6e71b725c276c4df11aee43dc27f4be2c40
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-17 15:58:01 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (52.35%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,15 +12,47 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(root==NULL){
+        // vector<int>ans;
+        // if(root==NULL){
+        //     return ans;
+        // }
+        // vector<int>left =inorderTraversal(root->left);
+        // ans.insert(ans.end(), left.begin(), left.end());
+        // ans.push_back(root->val);
+        //  vector<int>right =inorderTraversal(root->right);
+        // ans.insert(ans.end(), right.begin(), right.end());
+        // return ans;
+        
+        // Morris traversal
+           vector<int>ans;
+            if(root==NULL){
             return ans;
         }
-        vector<int>left =inorderTraversal(root->left);
-        ans.insert(ans.end(), left.begin(), left.end());
-        ans.push_back(root->val);
-         vector<int>right =inorderTraversal(root->right);
-        ans.insert(ans.end(), right.begin(), right.end());
+        TreeNode* curr= root;
+     while(curr!=NULL){
+        if(curr->left==NULL){
+            ans.push_back(curr->val);
+            curr= curr->right;
+        }
+        else{
+            // find inorder predecessor 
+            TreeNode*pre= curr->left;
+            while(pre->right!=NULL && pre->right!=curr){
+                pre= pre->right;
+            }
+            // Make current as the right child of its inorder predecessor
+            if(pre->right==NULL){
+                pre->right= curr;
+                curr= curr->left;
+            }
+            else{
+                // revert change 
+                pre->right=NULL;
+                ans.push_back(curr->val);
+                curr= curr->right;
+            }
+        }
+     }
         return ans;
     }
 };
