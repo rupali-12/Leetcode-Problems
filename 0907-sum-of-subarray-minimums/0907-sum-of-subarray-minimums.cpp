7c71d620c4d1f7762bea7d285d484e817e177fe9
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-20 12:52:42 +0530

    Time: 80 ms (79.68%), Space: 49.1 MB (5.62%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -5,32 +5,34 @@ public:
         vector<long>next_smaller(n);
         vector<long>prev_smaller(n);
         stack<pair<long, long>>s1, s2;
-        for(int i=0;i<n; i++){
-            long count=1;
-         while(!s1.empty() && arr[i]<s1.top().first){
-            count+= s1.top().second;
-            s1.pop();
-         }
-        s1.push({arr[i], count});
-         next_smaller[i]=count;
+      // next smaller contribution
+        for(int i=0; i<n; i++){
+            int count=1;
+            while(!s1.empty() && arr[i]<= s1.top().first){
+                count+= s1.top().second;
+                s1.pop();
+            }
+            s1.push({arr[i], count});
+            next_smaller[i]= count;
         }
-    
-     for(int i=n-1;i>=0; i--){
-            long count=1;
-         while(!s2.empty() && arr[i]<=s2.top().first){
-            count+= s2.top().second;
-            s2.pop();
-         }
-         s2.push({arr[i], count});
-         prev_smaller[i]=count;
+        
+        // prev smaller contribution 
+        for(int i=n-1; i>=0; i--){
+            int count=1;
+            while(!s2.empty() && arr[i]< s2.top().first){
+                count+= s2.top().second;
+                s2.pop();
+            }
+            s2.push({arr[i], count});
+            prev_smaller[i]=count;
         }
-
-    long sum=0;
-    int mod =1e9+7;
-     for(int i=0; i<n; i++){
-      sum+=(arr[i]*next_smaller[i]*prev_smaller[i]);
-      sum%=mod;
-    }
+        
+        long long sum=0;
+        long long mod=  1e9+7;
+        for(int i=0; i<n; i++){
+            sum+= arr[i]*next_smaller[i]*prev_smaller[i];
+            sum%=mod;
+        }
     return sum;
 }
 };
