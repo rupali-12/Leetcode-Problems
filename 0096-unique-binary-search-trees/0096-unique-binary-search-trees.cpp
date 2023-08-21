230aa857a8ecb014a0208db6b5d428958f7bb163
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-21 22:00:30 +0530

    Time: 2 ms (55.68%), Space: 6.2 MB (13.62%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-//     int solveRec(int n){
-//         if(n==0 || n==1){
-//             return 1;
-//         }
-//         int ans=0;
-//        for(int i=1; i<=n; i++){
-//            ans+= solveRec(i-1)*solveRec(n-i);
-//        }
-//         return ans;
-//     }
-//     int solveMem(int n, vector<int>&dp){
-//         if(n<=1){
-//             return 1;
-//         }
-//         if(dp[n]!=-1){
-//             return dp[n];
-//         }
-//         int ans =0;
-//         for(int i=1; i<=n; i++){
-//             ans+= solveMem(i-1, dp) *solveMem(n-i, dp);
-//         }
-//         return dp[n]=ans;
-//     }
-//     int solveTab(int n){
-//         vector<int>dp(n+1, 0);
-//         dp[0]=1;
-//         dp[1]=1;
-//         for(int i=2; i<=n; i++){
-//             for(int j=1; j<=i; j++){
-//                 dp[i]+= dp[j-1]*dp[i-j];
-//             }
-//         }
-//         return dp[n];
-//     }
+    int solveRec(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        int ans=0;
+       for(int i=1; i<=n; i++){
+           ans+= solveRec(i-1)*solveRec(n-i);
+       }
+        return ans;
+    }
+    int solveMem(int n, vector<int>&dp){
+        if(n<=1){
+            return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int ans =0;
+        for(int i=1; i<=n; i++){
+            ans+= solveMem(i-1, dp) *solveMem(n-i, dp);
+        }
+        return dp[n]=ans;
+    }
+    int solveTab(int n){
+        vector<int>dp(n+1, 0);
+        dp[0]=1;
+        dp[1]=1;
+      for(int i=2; i<=n; i++){
+          for(int j=1; j<=i; j++){
+              dp[i]+= dp[j-1]* dp[i-j];
+          }
+      }
+        return dp[n];
+    }
 
 //       int solveSpOpt(int n){
 //         if(n<=1){
@@ -47,29 +47,19 @@ public:
 //         }
 //         return ans;
 //     }
-//     int numTrees(int n) {
+    int numTrees(int n) {
         
-//         // // Approach 1>> Using Recursion 
-//         // return solveRec(n);
+        // // Approach 1>> Using Recursion 
+        // return solveRec(n);
         
-//         // // Approach 2>> Using Recursion+MEmoization  
-//         // vector<int>dp(n+1, -1);
-//         // return solveMem(n, dp);
+        // // Approach 2>> Using Recursion+MEmoization  
+        // vector<int>dp(n+1, -1);
+        // return solveMem(n, dp);
         
-//         //  // Approach 3>> Using Tabulation  
-//         // return solveTab(n);
+        //  // Approach 3>> Using Tabulation  
+        return solveTab(n);
         
-//           // Approach 34> Using space optimization  
-//         return solveSpOpt(n);
-    // }
-     int numTrees(int n) {  
-         if(n<=1){
-             return 1;
-         }
-     int count=0;
-         for(int i=1; i<=n; i++){
-             count+= numTrees(i-1)* numTrees(n-i);
-         }
-         return count;
-     }
+          // Approach 34> Using space optimization  
+        // return solveSpOpt(n);
+    }
 };
