156dc8ff153fce25bc9dcff08f9ca3fd48c08258
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-21 19:23:07 +0530

    Time: 29 ms (86.57%), Space: 32.4 MB (51.43%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* &root1, TreeNode* &root2){
+        if(root1==NULL ||  root2==NULL){
+            return;
+        }
+        if(root1->left && root2->left){
+            root1->left->val= root1->left->val + root2->left->val;
+        }
+        if(root1->left && !root2->left){
+         root1->left->val = root1->left->val + 0;
+        }
+       if(!root1->left && root2->left){
+           TreeNode* lnode= new TreeNode(root2->left->val);
+           root1->left= lnode;
+       }
+      
+         // Right Traversing 
+       if(root1->right && root2->right){
+            root1->right->val= root1->right->val + root2->right->val;
+        }
+        if(root1->right && !root2->right){
+            root1->right->val = root1->right->val+0;
+        }
+         if(!root1->right && root2->right){
+           TreeNode* rnode= new TreeNode(root2->right->val);
+           root1->right= rnode;
+       }
+        solve(root1->left, root2->left);
+        solve(root1->right, root2->right);
+        
+    }
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1 && root2){
+            root1->val= root1->val + root2->val;
+        }
+        if(root1 && !root2){
+         root1->val = root1->val + 0;
+        }
+       if(!root1 && root2){
+           TreeNode* node= new TreeNode(root2->val);
+           root1= node;
+       }
+       solve(root1, root2);
+           return root1;
+    }
+};
