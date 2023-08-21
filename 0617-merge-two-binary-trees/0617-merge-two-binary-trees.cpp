bad8d190d8e4b3ea553d391ff0d9840fa8f960b6
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-21 19:26:51 +0530

    Time: 30 ms (84.52%), Space: 32.2 MB (71.74%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -11,48 +11,59 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* &root1, TreeNode* &root2){
-        if(root1==NULL ||  root2==NULL){
-            return;
-        }
-        if(root1->left && root2->left){
-            root1->left->val= root1->left->val + root2->left->val;
-        }
-        if(root1->left && !root2->left){
-         root1->left->val = root1->left->val + 0;
-        }
-       if(!root1->left && root2->left){
-           TreeNode* lnode= new TreeNode(root2->left->val);
-           root1->left= lnode;
-       }
+//     void solve(TreeNode* &root1, TreeNode* &root2){
+//         if(root1==NULL ||  root2==NULL){
+//             return;
+//         }
+//         if(root1->left && root2->left){
+//             root1->left->val= root1->left->val + root2->left->val;
+//         }
+//         if(root1->left && !root2->left){
+//          root1->left->val = root1->left->val + 0;
+//         }
+//        if(!root1->left && root2->left){
+//            TreeNode* lnode= new TreeNode(root2->left->val);
+//            root1->left= lnode;
+//        }
       
-         // Right Traversing 
-       if(root1->right && root2->right){
-            root1->right->val= root1->right->val + root2->right->val;
-        }
-        if(root1->right && !root2->right){
-            root1->right->val = root1->right->val+0;
-        }
-         if(!root1->right && root2->right){
-           TreeNode* rnode= new TreeNode(root2->right->val);
-           root1->right= rnode;
-       }
-        solve(root1->left, root2->left);
-        solve(root1->right, root2->right);
+//          // Right Traversing 
+//        if(root1->right && root2->right){
+//             root1->right->val= root1->right->val + root2->right->val;
+//         }
+//         if(root1->right && !root2->right){
+//             root1->right->val = root1->right->val+0;
+//         }
+//          if(!root1->right && root2->right){
+//            TreeNode* rnode= new TreeNode(root2->right->val);
+//            root1->right= rnode;
+//        }
+//         solve(root1->left, root2->left);
+//         solve(root1->right, root2->right);
         
-    }
-    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(root1 && root2){
-            root1->val= root1->val + root2->val;
-        }
-        if(root1 && !root2){
-         root1->val = root1->val + 0;
-        }
-       if(!root1 && root2){
-           TreeNode* node= new TreeNode(root2->val);
-           root1= node;
-       }
-       solve(root1, root2);
-           return root1;
+//     }
+//     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+//         if(root1 && root2){
+//             root1->val= root1->val + root2->val;
+//         }
+//         if(root1 && !root2){
+//          root1->val = root1->val + 0;
+//         }
+//        if(!root1 && root2){
+//            TreeNode* node= new TreeNode(root2->val);
+//            root1= node;
+//        }
+//        solve(root1, root2);
+//            return root1;
+//     }
+    
+    // Approach-2 >
+        TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+          if(root1==NULL || root2==NULL){
+              return root1?root1:root2;
+          }
+        root1->val+= root2->val;
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right =  mergeTrees(root1->right, root2->right);
+        return root1;
     }
 };
