d4c344769d071efa7a9488c3e6002d0bdb066c7c
rupali-12 <78897399+rupali-12@users.noreply.github.com>
2023-08-23 16:12:54 +0530

    Time: 14 ms (74.68%), Space: 25.2 MB (52.45%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -9,35 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include<bits/stdc++.h>
 class Solution {
 public:
-    // void solve(TreeNode* root, int &mnDiff){
-    //     if(root==NULL){
-    //         return;
-    //     }
-    //     mnDiff= min(root)
-    // }
-    void solve(TreeNode* root, vector<int>&ans){
+
+    void solveBrute(TreeNode* root, vector<int>&ans){
         if(root==NULL){
             return;
         }
         ans.push_back(root->val);
-        solve(root->left, ans);
-         solve(root->right, ans);
+        solveBrute(root->left, ans);
+         solveBrute(root->right, ans);
         return;
     }
-    int getMinimumDifference(TreeNode* root) {
-        // int mnDiff=0;
-        // solve(root, mnDiff);
-        // return mnDiff;
-        
-        vector<int>ans;
-        solve(root, ans);
-        sort(ans.begin(), ans.end());
-        int res= ans[1]-ans[0];
-        for(int i=2; i<ans.size(); i++){
-            res=min(res, ans[i]-ans[i-1]);
+      void solve(TreeNode* root, int &mnDiff, int &prev){
+        if(root==NULL){
+            return;
         }
-        return res;
+          solve(root->left, mnDiff, prev);
+       if(prev!=-1){
+           mnDiff = min(mnDiff, abs(root->val- prev));
+       }
+          prev= root->val;
+          solve(root->right, mnDiff, prev);
+          return;
+    }
+    int getMinimumDifference(TreeNode* root) {
+        // // Approach 1>>Brute Force
+        // vector<int>ans;
+        // solveBrute(root, ans);
+        // sort(ans.begin(), ans.end());
+        // int res= ans[1]-ans[0];
+        // for(int i=2; i<ans.size(); i++){
+        //     res=min(res, ans[i]-ans[i-1]);
+        // }
+        // return res;
+        
+        // Approach-2>> Using Recursion 
+        int mnDiff=INT_MAX;
+        int prevValue=-1;
+        solve(root, mnDiff, prevValue);
+        return mnDiff;
     }
 };
