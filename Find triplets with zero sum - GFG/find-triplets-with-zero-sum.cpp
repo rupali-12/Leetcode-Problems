d26f91f0d5cf23635736d50257fce7b63eaa47e4
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-02 13:55:29 +0530

    Added solution - LeetHub

--- Find triplets with zero sum - GFG/find-triplets-with-zero-sum.cpp
+++ Find triplets with zero sum - GFG/find-triplets-with-zero-sum.cpp
@@ -15,23 +15,40 @@ class Solution{
     bool findTriplets(int arr[], int n)
     { 
         //Your code here
-    //  Approach-1: Hashing
+    // //  Approach-1: Hashing
+    // for(int i=0; i<n-1; i++){
+    //     unordered_set<int>s;
+    //     for(int j=i+1; j<n; j++){
+    //         int temp= -1*(arr[i]+arr[j]);
+    //         if(s.find(temp)!=s.end()){
+    //             return true;
+    //         }
+    //         else{
+    //             s.insert(arr[j]);   // arr[j] bcoz in order to check further values of jth position for each value of ith position 
+    //         }
+    //     }
+    // }
+    // return false;
     
-    for(int i=0; i<n-1; i++){
-        unordered_set<int>s;
-        for(int j=i+1; j<n; j++){
-            int temp= -1*(arr[i]+arr[j]);
-            if(s.find(temp)!=s.end()){
+    // Approach-2: Sorting
+    sort(arr, arr+n);
+    for(int i=0; i<n; i++){
+        int x= arr[i];
+        int l= i+1; 
+        int h= n-1;
+        while(l<h){
+            if(arr[l]+arr[h]+x==0){
                 return true;
             }
+            else if(x+arr[l]+arr[h]<0){
+                l++;
+            }
             else{
-                s.insert(arr[j]);   // arr[j] bcoz in order to check further values of jth position for each value of ith position 
+                h--;
             }
         }
     }
     return false;
-    
-    
     }
 };
 
