1591b67c26640edf9efe5ab6f0557bcda7f37184
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-02 08:41:53 +0530

    Added solution - LeetHub

--- Key Pair - GFG/key-pair.cpp
+++ Key Pair - GFG/key-pair.cpp
@@ -10,6 +10,22 @@ class Solution{
 public:	
 	// Function to check if array has 2 elements
 	// whose sum is equal to the given value
+	bool binSearch(int *arr, int element, int low, int high){
+	    if(low>high){
+	        return false;
+	    }
+	    int mid= low+(high-low)/2;
+	    if(arr[mid]==element){
+	           return true;
+	       }
+	       else if(arr[mid]<element){
+	         return binSearch(arr, element, mid+1, high);
+	       }
+	       else{
+	        return  binSearch(arr, element, low, mid-1);
+	       }
+	
+	}
 	bool hasArrayTwoCandidates(int arr[], int n, int x) {
 	    // code here
 	   //// Approach-1: Naive 
@@ -22,21 +38,44 @@ public:
 	   //}
 	   //return false;
 	   
-	   //Approach-2: Two pointer approach 
-	   sort(arr, arr+n);
-	   int i=0, j=n-1;
-	   while(i<j){
-	       if(arr[i]+arr[j]==x){
-	           return true;
-	       }
-	       else if(arr[i]+arr[j]<x){
-	           i++;
-	       }
-	       else{
-	           j--;
-	       }
-	   }
-	   return false;
+	   ////Approach-2: Two pointer approach 
+	   //sort(arr, arr+n);
+	   //int i=0, j=n-1;
+	   //while(i<j){
+	   //    if(arr[i]+arr[j]==x){
+	   //        return true;
+	   //    }
+	   //    else if(arr[i]+arr[j]<x){
+	   //        i++;
+	   //    }
+	   //    else{
+	   //        j--;
+	   //    }
+	   //}
+	   //return false;
+	   
+// 	   //Approach-3: Binary search 
+// 	      sort(arr, arr+n);
+// 	  for(int i=0; i<n-1; i++){
+// 	      int temp= x-arr[i];
+// 	      if(binSearch(arr, temp, i+1, n-1)){
+// 	          return true;
+// 	      }
+// 	  }
+// 	  return false;
+
+// Aproach-4: Hashing 
+            unordered_set<int>s;
+       for(int i=0; i<n; i++){
+           int temp= x-arr[i];
+         if(s.find(temp)!=s.end()){
+               return true;
+           }
+           else{
+               s.insert(arr[i]);
+           }
+       }
+       return false;
 	}
 };
 
