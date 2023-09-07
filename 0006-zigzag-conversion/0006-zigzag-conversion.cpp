a6081db7d151e903f0ccb2f4998427e37cb6fee7
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-07 07:19:59 +0530

    Time: 12 ms (50.17%), Space: 10.5 MB (43.89%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-int  j=0, move=-1;
-        string ans;
-            if(numRows<2) return s;
-        vector<string>temp(numRows, "");
+// int  j=0, move=-1;
+//         string ans;
+//             if(numRows<2) return s;
+//         vector<string>temp(numRows, "");
         
-        for(int i=0; i<s.length(); i++){
-            if(j== numRows-1 || j==0){
-                move*=-1;
-            }
-            temp[j]+= s[i];
-            if(move==1) j++;
-            else j--;
+//         for(int i=0; i<s.length(); i++){
+//             if(j== numRows-1 || j==0){
+//                 move*=-1;
+//             }
+//             temp[j]+= s[i];
+//             if(move==1) j++;
+//             else j--;
+//         }
+//         for(auto &it:temp){
+//             ans += it;
+//         }
+//         return ans;
+        
+        vector<string>temp(numRows);
+        if(numRows==1){
+            return s;
         }
-        for(auto &it:temp){
-            ans += it;
+        
+        int i=0;
+        bool flag=false;
+       for(auto ch: s){
+           temp[i]+= ch;
+            if(i==0 || i==numRows-1){
+            flag= !flag;
+        }
+           if(flag){
+               i+=1;
+           }
+           else{
+               i-= 1;
+           }
+       }
+        
+        string ans="";
+        for(auto ch: temp){
+            ans+= ch;
         }
         return ans;
     }
