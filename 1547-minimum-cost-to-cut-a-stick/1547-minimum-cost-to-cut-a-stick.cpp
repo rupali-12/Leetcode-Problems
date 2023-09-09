11ecedadd82b8cca07b69d8eaffd3af6e6044ffa
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-09 16:32:26 +0530

    Time: 51 ms (75.44%), Space: 10.5 MB (15.36%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -26,18 +26,37 @@ public:
         }
         return dp[i][j]= mini;
     }
+    
+    int solveTab(vector<int>cuts, int n){
+         vector<vector<int>>dp(cuts.size()+2, vector<int>(cuts.size()+2, 0));
+        for(int i= cuts.size()-2; i>=1; i--){
+            for(int j= 1; j<=cuts.size()-2; j++){
+                if(i>j) continue;
+                 int mini= INT_MAX;
+                for(int ind= i; ind<=j; ind++){
+                    int cost = (cuts[j+1]-cuts[i-1]) + dp[i][ind-1] + dp[ind+1][j];
+                    mini= min(mini, cost);
+                }
+             dp[i][j]= mini;   
+            }
+        }
+        return dp[1][cuts.size()-2];
+    }
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(n);
         cuts.insert(cuts.begin(), 0);
         sort(cuts.begin(), cuts.end());
         int i= 1, j= cuts.size()-2;   //   0, 1, 3, 4, 5 , 7  --? j points to 5 which is at index 4 (6-2, here 6 is new size of array)
-        
+        // c = cuts.size()-2  // 
        //  // Approach-1: Recursion 
        // return solveRec(cuts, i, j);
         
-        // Approach-2: Moemoization 
-        vector<vector<int>>dp(cuts.size()+1, vector<int>(cuts.size()+1, -1));
-        return solveMem(cuts, i, j, dp);
+        // // Approach-2: Moemoization 
+        // vector<vector<int>>dp(cuts.size()+1, vector<int>(cuts.size()+1, -1));
+        // return solveMem(cuts, i, j, dp);
+        
+        // Approach -3: tabulation 
+        return solveTab(cuts, n);
         
         
     }
