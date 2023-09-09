af3d39ae5006d1085622fb87d54a23ba90cf632c
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-09 23:29:48 +0530

    Time: 253 ms (88.47%), Space: 10.3 MB (42.34%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -21,6 +21,21 @@ public:
 	}
 	return dp[i][j]= maxi;
 }
+    int solveTab(vector<int>&nums, int n){
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+        for(int i= n-2; i>=1; i--){
+            for(int j=1; j<= n-2; j++){
+                if(i>j) continue;
+                int maxi= INT_MIN;
+                for(int k=i; k<=j; k++){
+                    int cost= (nums[i-1] * nums[k]*nums[j+1]) + dp[i][k-1] + dp[k+1][j];
+                    maxi= max(maxi, cost);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n-2];
+    }
     int maxCoins(vector<int>& nums) {
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
@@ -30,9 +45,11 @@ public:
         // // Approach-1: Recursion 
         // return solveRec(nums, i, j);
         
-         // // Approach-2: Recursion + Memoization
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return solveMem(nums, i, j, dp);
+        //  // // Approach-2: Recursion + Memoization
+        // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        // return solveMem(nums, i, j, dp);
 
+        // Approach-3: Tabulation 
+        return solveTab(nums, n);
     }
 };
