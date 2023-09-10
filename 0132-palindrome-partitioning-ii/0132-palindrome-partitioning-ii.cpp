64ad4f33aa97e74b4d814e86ad8d48082b7efc56
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-10 18:35:37 +0530

    Time: 1273 ms (62.02%), Space: 6.5 MB (78.40%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -22,6 +22,28 @@ public:
         }
         return mini;
     }
+    
+    //     // 1nd way -->TLE 
+    //  int solveMem(string s, int i,int j,  vector<vector<int>>&dp){
+    //     if(i>=j){
+    //         return 0;
+    //     }
+    //      if(isPalindrome(s, i, j)){
+    //          return 0;
+    //      }
+    //       if(dp[i][j]!=-1){
+    //           return dp[i][j];
+    //       }
+    //     int mini= INT_MAX;
+    //     for(int k= i; k<=j-1; k++){
+    //         int temp= 1 + solveMem(s, i, k, dp) + solveMem(s, k+1, j, dp);
+    //         mini= min(mini, temp);
+    //      }
+    //     return dp[i][j]= mini;
+    // }
+    
+    
+    // Memoization-2->optimized 
       int solveMem(string s, int i, vector<int>&dp){
         if(i==s.length()){
             return 0;
@@ -32,12 +54,35 @@ public:
         int mini= INT_MAX;
         for(int k= i; k<=s.length()-1; k++){
          if(isPalindrome(s, i, k)){
-            int temp= 1 + solveRec(s, k+1);
+            int temp= 1 + solveMem(s, k+1, dp);
             mini= min(mini, temp);
          }
         }
         return dp[i]= mini-1;
     }
+    
+
+    
+
+//     // Tabulation way-1 -->TLE
+//     int solveTab(string s, int n){
+//         vector<vector<int>>dp(n, vector<int>(n, 0));
+        
+//         for(int i=n-1;i>=0; i--){
+//             for(int j=0; j<=n-1; j++){
+//                 if(isPalindrome(s, i, j) || i>=j) continue;
+//          int mini= INT_MAX;
+//         for(int k= i; k<=j-1; k++){
+//             int temp= 1 + dp[i][k] + dp[k+1][j];
+//             mini= min(mini, temp);
+//          }
+//                 dp[i][j]= mini;
+//             }
+//         }
+//         return dp[0][n-1];
+//     }
+    
+    // Tabulation way-2 -->  optimized
     int solveTab(string s, int n){
         vector<int>dp(n+1, 0);
         for(int i=n-1; i>=0; i--){
@@ -52,6 +97,7 @@ public:
           }
         return dp[0]-1;    //  // -1 to avoid last partition which is done at end of string 
     }
+    
     int minCut(string s) {
         int i=0;
         // // Approach-1: Recursion 
@@ -62,11 +108,16 @@ public:
         // vector<int>dp(s.length(), -1);
         // return solveMem(s, i, dp);
         
-        // // Approach-3: Tabulation 
+        // // way-2: 
+        // vector<vector<int>>dp(s.length(), vector<int>(s.length(), -1));
+        // return solveMem(s, i, s.length()-1, dp);
+        
+        // // // Approach-3: Tabulation 
         int n= s.length();
+        // return solveTab(s, n);
+        
+        // way-2: 
         return solveTab(s, n);
-        
-        
     }
 };
 
