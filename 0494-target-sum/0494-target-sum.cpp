2977ae86eb289e54b7e5bf2050fc7d6515ce6ff4
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-13 18:50:32 +0530

    Time: 19 ms (59.65%), Space: 38.8 MB (22.32%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -21,19 +21,38 @@ int target){
 	return ans;
 
 }
+        int solveMem(vector<int>&nums, int index, int currSum, 
+int target, vector<vector<int>>&dp){
+	// if only one item is to steal
+	if(index==nums.size()){
+		if(currSum==target){
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+	if(dp[index][currSum+1010]!=-1){
+        return dp[index][currSum+1010];
+    }
+	// case of include 
+	int include= solveMem(nums, index+1, currSum+nums[index], target, dp);
+	// case of exclude 
+	int exclude = solveMem(nums, index+1, currSum-nums[index], target, dp);
+	int ans= include + exclude;   // total ways
+	return  dp[index][currSum+1010]= ans;
+
+}
     int findTargetSumWays(vector<int>& nums, int target) {
-      //  // approach-1>> Using Recursion 
+	// //  // approach-1>> Using Recursion 
         int n= nums.size();
-	return solveRec(nums, 0, target);
+	// return solveRec(nums, 0, target);
 
-	// // // approach-2>> Using Recursion+Memoization 
-    // vector<vector<int>>dp(n, vector<int>(target+1, -1));
-	// return solveMem(nums, value, n-1, target, dp);
+	// // approach-2>> Using Recursion+Memoization 
+//  This is a standard backtrack problem where we try for every approach and calculate the desired result.
+// But as the max size of array is 20 and total possibilities would be 2^20, this aprroach would result in TLE. Thus, we will need the help of memoization.
+    vector<vector<int>>dp(n, vector<int>(2010, -1));
+	return solveMem(nums, 0, 0,target, dp);
 
-	// // approach-3>> Using Tabulation 
-    //  return solveTab(nums, value, n, target);
-
-	// // approach-4>> Using Space optimization 
-	// return solveSpOpt(nums,  n,  target);
     }
 };
