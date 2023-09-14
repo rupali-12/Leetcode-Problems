c7e34b0fc76160d2c57bc08fd97782c8795ac10b
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-14 16:10:11 +0530

    Time: 0 ms (100.00%), Space: 10 MB (19.89%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -43,13 +43,39 @@ public:
         return dp[index]= ans;
     }
     
+           int solveTab(int n, vector<int>& days, vector<int>& costs){
+           vector<int>dp(n+1, 0);
+       for(int index=n-1; index>=0; index--){
+            // 1 day pass 
+        int option1= costs[0]+ dp[index+1];
+        
+        // 7 day pass 
+        // we need to find index for which next call is made 
+        int i;
+        for(i= index; i<n && days[i]<days[index]+7; i++);
+        int option2= costs[1] +dp[i];
+        
+         // 30 day pass 
+        // we need to find index for which next call is made 
+        for(i= index; i<n && days[i]<days[index]+30; i++);
+        int option3= costs[2] + dp[i];
+        
+        int ans = min(option1, min(option2, option3));
+           dp[index] = ans;
+       }
+        return dp[0];
+    }
+    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
        int n = days.size();
         // // Approach-1: Recursion 
         // return solveRec(n, days, costs, 0);
         
-        // Approach-2: Memozation 
-        vector<int>dp(n+1, -1);
-        return solveMem(n, days, costs, 0, dp);
+        // // Approach-2: Memozation 
+        // vector<int>dp(n+1, -1);
+        // return solveMem(n, days, costs, 0, dp);
+        
+        // Approach-3: Tabulation 
+        return solveTab(n, days, costs);
     }
 };
