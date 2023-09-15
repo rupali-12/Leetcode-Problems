0b6baa00b9246f6af9ce691407572f1b54d866cf
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-15 15:02:38 +0530

    Time: 240 ms (76.02%), Space: 288.9 MB (34.80%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,122 +1,52 @@
 class Solution {
 public:
-    int solveRec(vector<int>& obstacles, int currlane, int currpos){
-        int n=obstacles.size()-1;
-        if(currpos==n){
-            return 0;
-        }
-         
-        // no obstacle in curr lane 
-         int ans=INT_MAX;
-        if(obstacles[currpos+1]!=currlane){
-           return solveRec(obstacles, currlane, currpos+1);
-        }
-        else{
-            // obstacle not in currlane, and case of side jump
-            // but we want min ans
-            // int ans=INT_MAX;
-            for(int i=1; i<=3; i++){
-                if(currlane!=i && obstacles[currpos]!=i){
-                    // update ans 
-                    // add 1 bcoz we already jumped adn make call for recursion 
-                    ans =min(ans, 1 + solveRec(obstacles, i, currpos));
+    int solveRec(vector<int>&obst, int currlane, int currpos){
+        if(currpos==obst.size()-1) return 0;
+        
+        // case when there is not obst in currlane
+        int ans= INT_MAX;
+        for(int i=1; i<=3; i++){
+            if(obst[currpos+1]!=currlane){
+                return solveRec(obst, currlane, currpos+1);
+            }
+            else{
+                // case of side jump
+                if(currlane!=i && obst[currpos]!=i){
+                ans =  min(ans, 1 + solveRec(obst, i, currpos));
                 }
             }
-              // return ans;
-        }
-          return ans;
-    }
-    int solveMem(vector<int>& obstacles, int currlane, int currpos, vector<vector<int>>&dp){
-        int n =obstacles.size()-1;
-        if(currpos==n){
-            return 0;
-        }
-        if(dp[currlane][currpos]!=-1){
-            return dp[currlane][currpos];
-        }
-        int ans =INT_MAX;
-        if(obstacles[currpos+1]!=currlane){
-            return solveMem(obstacles, currlane, currpos+1, dp);
-        }
-        else{
-            for(int i=1; i<=3; i++){
-                if(currlane!=i && obstacles[currpos]!=i){
-                    ans =min(ans, 1+solveMem(obstacles, i, currpos, dp));
-                }
-            }
-            dp[currlane][currpos]=ans;
         }
         return ans;
     }
-    int solveTab(vector<int>& obstacles){
-        int n=obstacles.size()-1;
-          vector<vector<int>>dp(4, vector<int>(obstacles.size(), 1e9));
-        // we know that
-         dp[0][n]=0;
-         dp[1][n]=0;
-         dp[2][n]=0;
-         dp[3][n]=0;
+      int solveMem(vector<int>&obst, int currlane, int currpos, vector<vector<int>>&dp){
+        if(currpos==obst.size()-1) return 0;
         
-        for(int currpos= n-1; currpos>=0; currpos--){
-            for(int currlane=1; currlane<=3; currlane++){
-                if(obstacles[currpos+1]!=currlane){
-                 dp[currlane][currpos] = dp[currlane][currpos+1];
-                }
-                else{
-                    int ans= 1e9;
-                    for(int i=1; i<=3; i++){
-                        if(currlane!=i && obstacles[currpos]!=i){
-                            ans =min(ans, 1+dp[i][currpos+1]);
-                        }
-                    }
-                dp[currlane][currpos]=ans;
+        if(dp[currlane][currpos]!=-1){
+            return dp[currlane][currpos];
+        }
+          
+        // case when there is not obst in currlane
+        int ans= INT_MAX;
+        for(int i=1; i<=3; i++){
+            if(obst[currpos+1]!=currlane){
+                return dp[currlane][currpos] = solveMem(obst, currlane, currpos+1, dp);
+            }
+            else{
+                // case of side jump
+                if(currlane!=i && obst[currpos]!=i){
+                ans =  min(ans, 1 + solveMem(obst, i, currpos, dp));
                 }
             }
         }
-        return min(dp[2][0], min(1+dp[1][0], 1+dp[3][0]));
+        return dp[currlane][currpos] = ans;
     }
-    int solveSpOpt(vector<int>& obstacles){
-        int n=obstacles.size()-1;
-      vector<int>curr(4);
-        vector<int>next(4);
-         next[0]=0;
-         next[1]=0;
-         next[2]=0;
-         next[3]=0;
+    int minSideJumps(vector<int>& obst) {
+        // // Approach-1: Recursion 
+        // return solveRec(obst, 2, 0);
         
-        for(int currpos= n-1; currpos>=0; currpos--){
-            for(int currlane=1; currlane<=3; currlane++){
-                if(obstacles[currpos+1]!=currlane){
-                 curr[currlane] = next[currlane];
-                }
-                else{
-                    int ans= 1e9;
-                    for(int i=1; i<=3; i++){
-                        if(currlane!=i && obstacles[currpos]!=i){
-                            ans =min(ans, 1+next[i]);
-                        }
-                    }
-                curr[currlane]=ans;
-                }
-                }
-                next=curr;
-
-        }
-        return min(curr[2], min(1+curr[1], 1+curr[3]));
-    }
-    int minSideJumps(vector<int>& obstacles) {
-        // // Approach-1>. Using Recursion 
-        // return solveRec(obstacles, 2, 0);
-        
-        // // // Approach-2>. Using Recursion +memoization
-        // vector<vector<int>>dp(4, vector<int>(obstacles.size(), -1));
-        // // dp[lane][position ]
-        // return solveMem(obstacles, 2, 0, dp);
-        
-        //  // // Approach-3>. Using Tabulation
-        // return solveTab(obstacles);
-        
-         // // Approach-4>. Using space optimization
-        return solveSpOpt(obstacles);
+        // Approach-2: Memoization
+        vector<vector<int>>dp(4, vector<int>(obst.size(), -1));
+        return solveMem(obst, 2, 0, dp);
     }
 };
+
