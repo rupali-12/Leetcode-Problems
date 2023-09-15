307b4fe8022958cef00f55b014728d20c7762452
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-15 15:39:07 +0530

    Time: 265 ms (58.55%), Space: 239 MB (67.45%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -40,13 +40,44 @@ public:
         }
         return dp[currlane][currpos] = ans;
     }
+    int solveTab(vector<int>obst){
+        int n= obst.size()-1;
+         vector<vector<int>>dp(4, vector<int>(obst.size(), 1e9));
+        dp[0][n]=0;
+        dp[1][n]=0;
+        dp[2][n]=0;
+        dp[3][n]=0;
+         for(int currpos= n-1; currpos>=0; currpos--){
+           for(int currlane = 1; currlane<=3; currlane++){
+                if(obst[currpos+1]!=currlane){
+                    dp[currlane][currpos] = dp[currlane][currpos+1];
+                }
+                else{
+                    int ans= 1e9+7;
+                    for(int i=1; i<=3; i++){
+                        if(currlane!=i && obst[currpos]!=i){
+                            ans = min(ans, 1 + dp[i][currpos+1]);
+                        }
+                    }
+                    dp[currlane][currpos] = ans;
+                }
+            }
+        }
+      
+        int res= min(dp[2][0], min(1+dp[1][0], 1+dp[3][0]));
+        return res;
+    }
     int minSideJumps(vector<int>& obst) {
         // // Approach-1: Recursion 
         // return solveRec(obst, 2, 0);
         
-        // Approach-2: Memoization
-        vector<vector<int>>dp(4, vector<int>(obst.size(), -1));
-        return solveMem(obst, 2, 0, dp);
+        // // Approach-2: Memoization
+        // vector<vector<int>>dp(4, vector<int>(obst.size(), -1));
+        // return solveMem(obst, 2, 0, dp);
+        
+        // Approach-3: Tabulation 
+        int n = obst.size();
+        return solveTab(obst );
     }
 };
 
