08fb27381e5a9c7c3dc0df28a44ee4319b1d3035
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-15 17:49:52 +0530

    Time: 43 ms (24.49%), Space: 21.1 MB (15.85%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,78 +1,34 @@
 class Solution {
 public:
-    int solveRec(vector<int>& satisfaction, int i,int time){
-        if(i==satisfaction.size()){
-            return 0;
-        }
+    int solveRec(vector<int>&satisfaction, int index, int time){
+        if(index==satisfaction.size()) return 0;
         
-        // case of include
-        // int include =satisfaction[i]*time +  solveRec(satisfaction, i+1, time+1);
-         int include =satisfaction[i]*(time+1) +  solveRec(satisfaction, i+1, time+1);
-        // case of exclude 
-        int exclude = solveRec(satisfaction, i+1, time);
-        
-        return max(include, exclude);
+        // include 
+        int inc  = satisfaction[index]*(time+1) + solveRec(satisfaction, index+1, time+1);
+        // exclude
+        int exc = 0 + solveRec(satisfaction, index+1, time);
+        int ans = max(inc, exc);
+        return ans;
     }
-    int solveMem(vector<int>& satisfaction, int i, int time, vector<vector<int>>&dp){
-        if(i==satisfaction.size()){
-            return 0;
+     int solveMem(vector<int>&satisfaction, int index, int time, vector<vector<int>>&dp){
+        if(index==satisfaction.size()) return 0;
+        if(dp[index][time]!=-1){
+            return dp[index][time];
         }
-        if(dp[time][i]!=-1){
-            return dp[time][i];
-        }
-        int include =satisfaction[i]*time +solveMem(satisfaction, i+1,time+1, dp);
-        int exclude =solveMem(satisfaction, i+1, time, dp);
-          
-       dp[time][i] =max(include, exclude);
-        return dp[time][i];
-    }
-    int solveTab(vector<int>& satisfaction){
-        int n=satisfaction.size();
-         vector<vector<int>>dp(n+2,vector<int>(n+1, 0));
-        dp[n][n]=0;
-        for(int index=n-1; index>=0; index--){
-            for(int time=n-1; time>=0;time--){
-                int include =satisfaction[index]*(time+1) + dp[index+1][time+1];
-                int exclude =dp[index+1][time];
-                dp[index][time]=max(include, exclude);
-            }
-        }
-        return dp[0][0];
-        }
-    int solveSpOpt(vector<int>& satisfaction){
-        int n=satisfaction.size();
-        vector<int>curr(n+1, 0);
-        vector<int>next(n+1,0);
-        next[n]=0;
-        for(int index=n-1; index>=0;index--){
-            for(int time =n-1; time>=0; time--){
-                int include= satisfaction[index]*(time+1) + next[time+1];
-                int exclude = curr[time];
-                   // bcoz in last iteration curr represent last  row
-                curr[time]=max(include, exclude);
-            }
-           next=curr;
-        }
-        return next[0];
+        // include 
+        int inc  = satisfaction[index]*(time+1) + solveMem(satisfaction, index+1, time+1, dp);
+        // exclude
+        int exc = 0 + solveMem(satisfaction, index+1, time, dp);
+        int ans = max(inc, exc);
+        return dp[index][time] = ans;
     }
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(), satisfaction.end());
+        // // Approach-1: Recursion 
+        // return solveRec(satisfaction, 0, 0);
         
-        // // Approach-1> Using Recursion 
-        //  return solveRec(satisfaction, 0, 1);
-        //  return solveRec(satisfaction, 0, 0);
-        
-        //  // Approach-2> Using Recursion +Memoization 
-        // int n =satisfaction.size();
-        // vector<vector<int>>dp(n+2,vector<int>(n+1, -1));
-        //  return solveMem(satisfaction, 0, 1,dp);
-        
-        //    // Approach-3 Using Tabulation
-        // int n =satisfaction.size();
-        //  return solveTab(satisfaction);
-        
-          // Approach-4> using space optimization
-        int n =satisfaction.size();
-         return solveSpOpt(satisfaction);
+        // Approach-2: Memoization 
+        vector<vector<int>>dp(satisfaction.size(), vector<int>(satisfaction.size(), -1));
+        return solveMem(satisfaction, 0, 0, dp);
     }
 };
