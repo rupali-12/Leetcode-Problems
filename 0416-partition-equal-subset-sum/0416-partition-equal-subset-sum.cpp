1445423cf77fc77f88849016951405560a36366f
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-18 16:38:25 +0530

    Time: 223 ms (62.90%), Space: 11.8 MB (79.65%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -40,6 +40,24 @@ public:
         }
         return dp[0][target];
     }
+      bool solveTabSpOpt(vector<int>&nums, int n, int target){
+       vector<int>curr(target+1, 0);
+       vector<int>next(target+1, 0);
+       curr[0]= next[0]=1;
+        for(int index=n-1; index>=0; index--){
+            for(int t=1; t<=target; t++){
+                
+                bool inc=false;
+               if(t-nums[index]>=0){
+                   inc =next[t-nums[index]];
+               }
+                bool exc = next[t];
+                curr[t] = inc | exc;
+            }
+            next= curr;
+        }
+        return next[target];
+    }
     bool canPartition(vector<int>& nums) {
         int total=0;
         int n = nums.size();
@@ -58,7 +76,10 @@ public:
         // vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
         // return solveMem(0, nums, n, target, dp);
         
-          // Approach-3:Tabulation
-        return solveTab(nums, n, target);
+        //   // Approach-3:Tabulation
+        // return solveTab(nums, n, target);
+        
+           // Approach-4:Tabulation + space optimization
+        return solveTabSpOpt(nums, n, target);
     }
 };
