9cdec0ba116076f90fd99be143c529a17fa3cb29
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-19 18:23:28 +0530

    Time: 2 ms (60.43%), Space: 6.3 MB (7.05%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -9,8 +9,23 @@ public:
         }
         return ans;
     }
+    int solveMem(int n, vector<int>&dp){
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int ans=0;
+        for(int i=1; i<=n; i++){
+            ans+= solveMem(i-1, dp)*solveMem(n-i, dp);
+        }
+        return dp[n]=ans;
+    }
     int numTrees(int n) {
-        // Approach-1: Recursion 
-        return solveRec(n);
+        // // Approach-1: Recursion 
+        // return solveRec(n);
+        
+         // Approach-2: Memoization
+        vector<int>dp(n+1, -1);
+        return solveMem(n, dp);
     }
 };
