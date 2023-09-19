69c662c8c7c5913355acf1d9b4ae930a722bf551
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-19 19:38:03 +0530

    Time: 78 ms (87.18%), Space: 8.5 MB (6.41%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -19,13 +19,34 @@ public:
         }
         return dp[start][end] = mini;
     }
+    int solveTab(int n){
+         vector<vector<int>>dp(n+2, vector<int>(n+1, 0));
+        for(int start=n; start>=1; start--){
+            for(int end= start; end<=n; end++){
+// start=end case when correct number is choosen, else we further proceedto find worst case
+//     i.e sum of all wrong predictions 
+                if(start==end) continue;
+                else{
+                    int mini= INT_MAX;
+                    for(int i=start; i<=end; i++){
+                        mini= min(mini, i+max(dp[start][i-1], dp[i+1][end]));
+                    }
+                    dp[start][end]=mini;
+                }
+            }
+        }
+        return dp[1][n];
+    }
     int getMoneyAmount(int n) {
         // // Approach-1: Recursion 
         // // (start, end)
         // return solveRec(1, n);
         
-         // Approach-2: Memoization 
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return solveMem(1, n, dp);
+        //  // Approach-2: Memoization 
+        // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        // return solveMem(1, n, dp);
+        
+         // Approach-3: Tabulation 
+        return solveTab(n);
     }
 };
