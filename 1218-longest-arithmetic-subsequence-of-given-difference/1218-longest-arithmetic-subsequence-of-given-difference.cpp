5ca3cc9cbe6ceddb4480c6c4bf8691b8835a9e9f
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-19 17:53:00 +0530

    Time: 102 ms (87.22%), Space: 55.2 MB (48.43%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -4,19 +4,18 @@ public:
         // <prev temp (current element-diff), temp ans(no. of timesthe temp occor previously other than current)>
         unordered_map<int, int>dp;
         int ans=0;
-        for(int i=0; i<arr.size(); i++){
-            int temp = arr[i]-difference;
-            int tempAns=0;
+       for(int i=0; i<arr.size(); i++){
+           int temp = arr[i]-difference;
             
-            // if answer for temp exist already or not 
-            if(dp.count(temp)){
-                tempAns = dp[temp];
-            }
-            
-            // update current answer --> 1+ to store current ans as well
-            dp[arr[i]]=1+ tempAns;
-            ans= max(ans,dp[arr[i]]);
-        }
+           // check if ans for temp already exist or not
+           if(dp.find(temp)!=dp.end()){
+               dp[arr[i]] = 1 + dp[temp];    // +1 to store current ans 
+           }
+           else{
+               dp[arr[i]] = 1+0;
+           }
+           ans= max(ans, dp[arr[i]]);
+       }
         return ans;
     }
 };
