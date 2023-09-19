0030383963f296c72b2d6c33545a12d03d0f9b94
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-19 16:10:15 +0530

    Time: 1204 ms (64.40%), Space: 242.8 MB (52.30%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -1,21 +1,79 @@
 class Solution {
 public:
-    int longestArithSeqLength(vector<int>& nums) {
-        int n= nums.size();
-                if(n<=2){
-            return n;
-        }
+//       // Approach-1-> Top down 
+//     int solveRec(int index, int currDiff, vector<int>&nums){
+//         if(index<0) return 0;
+//         int ans=0;
+//         for(int j= index-1; j>=0; j--){
+//             if(nums[index]-nums[j] ==currDiff){
+//                 ans= max(ans, 1+ solveRec(j, currDiff, nums));
+//             }
+//         }
+//         return ans;
+//     }
+//      int solveMem(int index, int diff, vector<int>&nums, unordered_map<int, int>dp[]){
+//         if(index<0) return 0;
+//          if(dp[index].count(diff)){
+//              return dp[index][diff];
+//          }
+//         int ans=0;
+//         for(int j= index-1; j>=0; j--){
+//             if(nums[index]-nums[j] ==diff){
+//                 ans= max(ans, 1+ solveMem(j, diff, nums, dp));
+//             }
+//         }
+//         return dp[index][diff] = ans;
+//     }
+//     int longestArithSeqLength(vector<int>& nums) {
+// //         // Approach-1-i  Recursion
+// //         int n= nums.size();
+// //            if(n<=2) return 0;
         
-        unordered_map<int, int>dp[n+1];   
+// //         int ans=0;
+// //         for(int i=0; i<n; i++){
+// //             for(int j=i+1; j<n; j++){
+// //                 int currDiff = nums[j]- nums[i];
+// //                ans = max(ans, 2 + solveRec(i, currDiff, nums));     
+// //             }
+// //         }
+// //         return ans;
+        
+//         // Approach-1-ii  Memoization
+//         int n= nums.size();
+//         unordered_map<int, int>dp[n+1];
+//            if(n<=2) return 0;
+        
+//         int ans=0;
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 int currDiff = nums[j]- nums[i];
+//                ans = max(ans, 2 + solveMem(i, currDiff, nums, dp));     
+//             }
+//         }
+//         return ans;
+//     }
+    
+    // *****************************************************************
+    // Approach-2: Bottom up 
+    int longestArithSeqLength(vector<int>& nums) {
+     int n= nums.size();
+        if(n<=2) return n;
+        unordered_map<int, int>dp[n+1];
         int ans=0;
-        for(int i=1; i<n; i++){
+        for(int i= 1; i<n; i++){
             for(int j=0; j<i; j++){
-                int diff = nums[i]-nums[j];
-                int cnt=1;   // a i wala include hoga of j k liye we nee dto check
+                int diff = nums[i]- nums[j];
+                int cnt=1;   // included element ith index
+                
+                // check if ans already exist or not 
                 if(dp[j].count(diff)){
                     cnt= dp[j][diff];
                 }
-                dp[i][diff]= 1+cnt;
+                
+                // update ans of ith index 
+                dp[i][diff] = 1 +cnt;
+                
+                // update ans 
                 ans = max(ans, dp[i][diff]);
             }
         }
