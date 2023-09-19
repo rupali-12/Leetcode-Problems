ae6ffed19cf963fc7b9c78ae05f0fae4abe8294a
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-19 22:54:22 +0530

    Time: 36 ms (37.52%), Space: 11.2 MB (8.60%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -21,6 +21,23 @@ public:
         }
         return dp[left][right] =ans;
     }
+    
+         int solveTab(vector<int>&arr, map<pair<int, int>, int>&maxi, int n){
+              vector<vector<int>>dp(n, vector<int>(n, 0));
+      for(int left= n-1; left>=0; left--){
+          for(int right= left;right<n; right++){
+              if(left==right) continue;
+               int ans= INT_MAX;
+        for(int i=left; i<right; i++){
+            ans= min(ans, (maxi[{left, i}]*maxi[{i+1, right}] + dp[left][i] + dp[i+1][right]));
+        }
+              dp[left][right]=ans;
+          }
+      }
+       
+        return dp[0][n-1];
+    }
+    
     int mctFromLeafValues(vector<int>& arr) {
         // store max
         map<pair<int, int>, int>maxi;
@@ -35,8 +52,11 @@ public:
         // // Approach-1: Recursion
         // return solveRec(arr, maxi, 0, n-1);
         
-        // Approach-2: Memoization
-        vector<vector<int>>dp(n, vector<int>(n, -1));
-        return solveMem(arr, maxi, 0, n-1, dp);
+        // // Approach-2: Memoization
+        // vector<vector<int>>dp(n, vector<int>(n, -1));
+        // return solveMem(arr, maxi, 0, n-1, dp);
+        
+        // // Approach-3: Tabulation
+        return solveTab(arr, maxi, n);
     }
 };
