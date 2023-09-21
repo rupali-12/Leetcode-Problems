c5f9f0e97ffb44a97b48531caea3ff161d3c1b4c
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-21 12:49:20 +0530

    Time: 160 ms (76.22%), Space: 11.3 MB (90.25%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -30,6 +30,24 @@ public:
          }
          return ans;
     } 
+     int solveTabSpOpt(vector<int>& nums1, vector<int>& nums2){
+         vector<int>prev(nums2.size()+1, 0);
+         vector<int>curr(nums2.size()+1, 0);
+         int ans=0;
+         for(int i=1; i<=nums1.size(); i++){
+             for(int j=1; j<=nums2.size(); j++){
+                 if(nums1[i-1]==nums2[j-1]){    // as 1-based indexing is used
+                     curr[j]= 1+ prev[j-1];
+                 }
+                 else{
+                     curr[j]=0;
+                 }
+                 ans= max(ans, curr[j]);
+             }
+             prev=curr;
+         }
+         return ans;
+    } 
     
     int findLength(vector<int>& nums1, vector<int>& nums2) {
                 int n=nums1.size(),m=nums2.size();
@@ -37,7 +55,10 @@ public:
         // // (i, j, count)
         // return solveRec(nums1, nums2, 0, 0, 0);
         
-          // Approach-2: (Tabulation)
-        return solveTab(nums1, nums2);
+         //  // Approach-2: (Tabulation)
+        // return solveTab(nums1, nums2);
+        
+         // Approach-3: (Tabulation + space optimization)
+        return solveTabSpOpt(nums1, nums2);
     }
 };
