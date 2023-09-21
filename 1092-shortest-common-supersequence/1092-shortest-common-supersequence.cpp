2ecb979e6dcf9958de4d4d864ccd6ae65f72593d
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-21 17:12:14 +0530

    Time: 21 ms (57.31%), Space: 13.2 MB (20.60%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -13,21 +13,6 @@ public:
 	}
 	return dp[n][m];
 }
-    string solve(string str1, string str2){
-        int m= str1.length(), n= str2.length();
-        vector<vector<string>>dp(m+1, vector<string>(n+1, ""));
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                if(str1[i-1]==str2[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + str1[i-1];
-                }
-                else{
-                    dp[i][j]+= dp[i-1][j].size() > dp[i][j-1].size() ? dp[i-1][j]:dp[i][j-1];
-                }
-            }
-        }
-        return dp[m][n];
-    }
     string shortestCommonSupersequence(string str1, string str2) {
         
 	// // Approach-1:Tabualtion 
@@ -63,11 +48,6 @@ public:
         
 	reverse(s.begin(), s.end());
 	return s;
-        
-        
-        // Approach-2: optimized
-        // string lcs = solve(str1, str2);
-        // return lcs;
-    }
 
+    }
 };
