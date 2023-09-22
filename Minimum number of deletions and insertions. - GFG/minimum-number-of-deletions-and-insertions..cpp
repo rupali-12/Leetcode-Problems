a3e9c4af839a0d57b9ae5830ddfa340e00c7045e
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-22 21:59:15 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -21,13 +21,35 @@ class Solution{
 	    }
 	    return dp[i][j]=ans;
 	}
+	int solveTab(string str1, string str2, int n, int m){
+	    vector<vector<int>>dp(n+1, vector<int>(m+1, -0));
+	   
+	   for(int i=1; i<=n; i++){
+	       for(int j=1; j<=m; j++){
+	    int ans=0;
+	    if(str1[i-1]==str2[j-1]){
+	       ans = 1 + dp[i-1][j-1]; 
+	    }
+	    else{
+	     ans=max(dp[i-1][j], dp[i][j-1]);   
+	    }
+	    dp[i][j]=ans;
+	       }
+	   }
+	    return dp[n][m];
+	}
 	int minOperations(string str1, string str2) 
 	{ 
 	    // Your code goes here
 	    int n= str1.length(), m= str2.length();
-	   // approach-2:Memoization 
-	   vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-	   int lcs =  solveMem(str1, str2, 0, 0, dp);
+	    
+	   //// approach-2:Memoization 
+	   //vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+	   //int lcs =  solveMem(str1, str2, 0, 0, dp);
+	   
+	   //Approach-2: Tabulation 
+	   int lcs= solveTab(str1, str2, n, m);
+	   
 	    int numOfDeletions = str1.length() - lcs;
 	    int numOfInsertions = str2.length() - lcs;
 	    int ans= numOfDeletions + numOfInsertions;
