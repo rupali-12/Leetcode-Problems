9b0feac77495e3daa98f85f1a08dabd3401c429d
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-24 17:05:01 +0530

    Time: 18 ms (72.67%), Space: 13.4 MB (19.93%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,53 +1,52 @@
 class Solution {
 public:
-    int solveTab(string s1, string s2, int n, int m, vector<vector<int>>&dp){
-	for(int i=1;i<=n; i++){
-		for(int j=1; j<=m; j++){
-			if(s1[i-1]==s2[j-1]){
-				  dp[i][j]= 1 + dp[i-1][j-1];
-			}
-			else{
-               dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
-			}
-		}
-	}
-	return dp[n][m];
-}
+    int solveTab(string str1, string str2, vector<vector<int>>&dp, int n,int m){
+        int ans=0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(str1[i-1]==str2[j-1]){
+                    ans= 1 + dp[i-1][j-1];
+                }
+                else{
+                    ans= max(dp[i][j-1], dp[i-1][j]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[n][m];
+    }
     string shortestCommonSupersequence(string str1, string str2) {
+        int n=str1.length(), m= str2.length();
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        solveTab(str1, str2, dp, n, m);
+        string ans="";
+        int i=n, j=m;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                ans.push_back(str1[i-1]);
+                i--; j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1]){
+                ans.push_back(str1[i-1]);
+                i--;
+            }
+            else{
+                ans.push_back(str2[j-1]);
+                j--;
+            }
+        }
         
-	// // Approach-1:Tabualtion 
-	vector<vector<int>>dp(str1.length()+1, vector<int>(str2.length()+1, 0));
-	solveTab(str1, str2, str1.length(), str2.length(), dp);
-	string s="";
-	int i=str1.length(), j=str2.length();
-	while(i>0 && j>0){
-		if(str1[i-1]==str2[j-1]){
-          s.push_back(str1[i-1]);
-		  i--; j--;
-		}
-		else{
-           if(dp[i-1][j]>dp[i][j-1]){
-               s.push_back(str1[i-1]);
-			   i--;
-		   }
-		   else{
-               s.push_back(str2[j-1]);
-			   j--;
-		   }
-		}
-	}
-        // print is there is any remaining element in both string
+        // insert remaining characters  if exist
         while(i>0){
-            s.push_back(str1[i-1]);
-            i--;
+               ans.push_back(str1[i-1]);
+                i--;
         }
-         while(j>0){
-            s.push_back(str2[j-1]);
-            j--;
+        while(j>0){
+              ans.push_back(str2[j-1]);
+                j--;
         }
+        reverse(ans.begin(), ans.end());
+        return ans;
         
-	reverse(s.begin(), s.end());
-	return s;
-
     }
 };
