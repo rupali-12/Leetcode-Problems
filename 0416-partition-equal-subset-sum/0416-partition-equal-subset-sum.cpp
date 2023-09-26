e51da2daf9c37fd0e571770a4b54967257c70eb8
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-26 23:32:25 +0530

    Time: 220 ms (62.36%), Space: 96 MB (19.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    bool solveRec(int index, vector<int>& nums, int n, int target){
-        if(index==n || target<0) return 0;
-        if(target==0) return 1;
-        
-        bool include =  solveRec(index+1, nums, n, target-nums[index]);
-        bool exclude =solveRec(index+1, nums, n, target);
-        
-        // if any one outof two cases then return true; 
-        return include || exclude;
-    }
-     bool solveMem(int index, vector<int>& nums, int n, int target, vector<vector<int>>&dp){
-        if(index==n || target<0) return 0;
-        if(target==0) return 1;
-        if(dp[index][target]!=-1){
-            return dp[index][target];
-        }
-        bool include =  solveMem(index+1, nums, n, target-nums[index], dp);
-        bool exclude =solveMem(index+1, nums, n, target, dp);
-        
-        // if any one outof two cases then return true; 
-        return dp[index][target] = (include || exclude);
-    }
+    bool solveRec(vector<int>&arr, int index, int sum){
+      if(sum==0) return true;
+      if(index==0) return arr[index]==sum;
+
+      bool notTake =solveRec(arr, index-1, sum);
+      bool take= false;
+      if(arr[index]<=sum){
+          take = solveRec(arr, index-1, sum-arr[index]);
+      }
+      return take | notTake;
+   }
+   bool solveMem(vector<int>&arr, int index, int sum, vector<vector<int>>&dp){
+      if(sum==0) return true;
+      if(index==0) return arr[index]==sum;
+   if(dp[index][sum]!=-1){
+       return dp[index][sum];
+   }
+      bool notTake =solveMem(arr, index-1, sum, dp);
+      bool take= false;
+      if(arr[index]<=sum){
+          take = solveMem(arr, index-1, sum-arr[index], dp);
+      }
+      return dp[index][sum] =  take | notTake;
+   }
      bool solveTab(vector<int>&nums, int n, int target){
         vector<vector<int>>dp(n+1, vector<int>(target+1, 0));
         for(int i=0; i<n;i++){
@@ -70,16 +72,16 @@ public:
         int target= total/2;
         
         // // Approach-1: Recursion 
-        // return solveRec(0, nums, n, target);
+        // return solveRec(nums, n-1, target);
         
         //  // Approach-2: Recursion + Memoization
-        // vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
-        // return solveMem(0, nums, n, target, dp);
+        vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
+        return solveMem(nums, n-1, target, dp);
         
         //   // Approach-3:Tabulation
         // return solveTab(nums, n, target);
         
            // Approach-4:Tabulation + space optimization
-        return solveTabSpOpt(nums, n, target);
+        // return solveTabSpOpt(nums, n, target);
     }
 };
