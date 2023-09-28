5137874ed5669e9761248622f0b619b113c81e36
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-28 12:52:36 +0530

    Time: 1232 ms (9.90%), Space: 9.3 MB (69.56%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,58 +1,42 @@
 class Solution {
 public:
     
-    int solveRec(vector<int>&nums, int index, 
-int target){
-	// if only one item is to steal
-	if(index==nums.size()){
-		if(target==0){
-			return 1;
-		}
-		else{
-			return 0;
-		}
-	}
-	
-	// case of include 
-	int include= solveRec(nums, index+1, target+nums[index]);
-	// case of exclude 
-	int exclude = solveRec(nums, index+1, target-nums[index]);
-	int ans= include + exclude;   // total ways
-	return ans;
-
+//     int solveRec(vector<int>&nums, int index, 
+// int target){
+//     if(index==nums.size()){
+//         if(target==0) return 1;
+//         else return 0;
+//     }	
+        
+//         int include =solveRec(nums, index+1, target+nums[index]);
+//         int exclude =solveRec(nums, index+1, target-nums[index]);
+//         int ans = include + exclude;
+//         return ans;
+// }
+    
+       int solveRec(vector<int>&nums, int index, 
+int target, int currSum){
+    if(index==nums.size()){
+        if(target==currSum) return 1;
+        else return 0;
+    }	
+        
+        int include =solveRec(nums, index+1, target, currSum+nums[index]);
+        int exclude =solveRec(nums, index+1, target, currSum-nums[index]);
+        int ans = include + exclude;
+        return ans;
 }
-        int solveMem(vector<int>&nums, int index, int currSum, 
-int target, vector<vector<int>>&dp){
-	// if only one item is to steal
-	if(index==nums.size()){
-		if(currSum==target){
-			return 1;
-		}
-		else{
-			return 0;
-		}
-	}
-	if(dp[index][currSum+1010]!=-1){
-        return dp[index][currSum+1010];
-    }
-	// case of include 
-	int include= solveMem(nums, index+1, currSum+nums[index], target, dp);
-	// case of exclude 
-	int exclude = solveMem(nums, index+1, currSum-nums[index], target, dp);
-	int ans= include + exclude;   // total ways
-	return  dp[index][currSum+1010]= ans;
 
-}
     int findTargetSumWays(vector<int>& nums, int target) {
 	// //  // approach-1>> Using Recursion 
         int n= nums.size();
-	// return solveRec(nums, 0, target);
+	return solveRec(nums, 0, target, 0);
 
 	// // approach-2>> Using Recursion+Memoization 
 //  This is a standard backtrack problem where we try for every approach and calculate the desired result.
 // But as the max size of array is 20 and total possibilities would be 2^20, this aprroach would result in TLE. Thus, we will need the help of memoization.
-    vector<vector<int>>dp(n, vector<int>(2010, -1));
-	return solveMem(nums, 0, 0,target, dp);
+    // vector<vector<int>>dp(n, vector<int>(2010, -1));
+	// return solveMem(nums, 0, 0,target, dp);
 
     }
 };
