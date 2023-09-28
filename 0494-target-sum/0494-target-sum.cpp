de71b278d6d63bb629f7e4e8479a085809f62d80
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-28 13:17:34 +0530

    Time: 13 ms (68.57%), Space: 12.6 MB (39.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     
-//     int solveRec(vector<int>&nums, int index, 
-// int target){
+//     int solveRec(vector<int>&nums, int index,  int target){
 //     if(index==nums.size()){
 //         if(target==0) return 1;
 //         else return 0;
@@ -14,8 +13,7 @@ public:
 //         return ans;
 // }
     
-       int solveRec(vector<int>&nums, int index, 
-int target, int currSum){
+       int solveRec(vector<int>&nums, int index, int target, int currSum){
     if(index==nums.size()){
         if(target==currSum) return 1;
         else return 0;
@@ -26,17 +24,40 @@ int target, int currSum){
         int ans = include + exclude;
         return ans;
 }
+int solveMem(vector<int>&nums, int index, int sum, vector<vector<int>>&dp){
+       if(index==0){
+           if(sum==0 && nums[0]==0) return 2;
+           if(sum==0 || sum==nums[0]) return 1;
+           return 0;
+       }   
+      if(dp[index][sum]!=-1){
+          return dp[index][sum];
+      }
+        int notTake =solveMem(nums, index-1, sum, dp);
+        int take=0;
+      if(nums[index]<=sum){
+          take=solveMem(nums, index-1, sum - nums[index], dp);
+      }
+    
+        int ans = take + notTake;
+        return dp[index][sum] = ans;
+}
+    
 
     int findTargetSumWays(vector<int>& nums, int target) {
 	// //  // approach-1>> Using Recursion 
         int n= nums.size();
-	return solveRec(nums, 0, target, 0);
+	// return solveRec(nums, 0, target, 0);
 
-	// // approach-2>> Using Recursion+Memoization 
-//  This is a standard backtrack problem where we try for every approach and calculate the desired result.
-// But as the max size of array is 20 and total possibilities would be 2^20, this aprroach would result in TLE. Thus, we will need the help of memoization.
-    // vector<vector<int>>dp(n, vector<int>(2010, -1));
-	// return solveMem(nums, 0, 0,target, dp);
+	// // approach-2>> Using Recursion+Memoization (count of subset with given diff)
+        int totalSum=0;
+        for(int i=0; i<n;i++){
+            totalSum+= nums[i];
+        }
+      int s1 = (totalSum-target)/2;
+        if(target>totalSum || (totalSum-target)%2) return 0;
+        vector<vector<int>>dp(n+1, vector<int>(s1+1, -1));
+	return solveMem(nums, n-1, s1,  dp);
 
     }
 };
