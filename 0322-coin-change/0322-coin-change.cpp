811bbc66bb8dd6762c86526299dc8fd1c1f5514e
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-09-30 12:02:35 +0530

    Time: 2524 ms (5.05%), Space: 589.2 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,53 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount){
-        vector<int>dp(amount+1, INT_MAX);
-        dp[0]=0;
-        for(int i=1; i<=amount; i++){
-            for(int j=0; j<coins.size(); j++){
-               if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX){
-                    dp[i]= min(dp[i], 1+dp[i-coins[j]]);
-               }
+ 
+    int  solveRec(vector<int>coins, int amount, int index){
+        if(index==0) {
+            if(amount%coins[0]==0){
+                return amount/coins[0];   // min coin tha we can return
             }
+            else return 1e9;
         }
-        return dp[amount];
+        
+        int ans= 0, take=INT_MAX;
+        int notTake= solveRec(coins, amount, index-1);
+        if(coins[index]<= amount){
+            take = 1 + solveRec(coins, amount-coins[index], index);
+        }
+        ans= min(take, notTake);
+        return ans;
     }
-    int coinChange(vector<int>& coins, int amount) {
-        // using tabulation 
-        int ans =solve(coins, amount);
-        if(ans==INT_MAX){
-            return -1;
+     int  solveMem(vector<int>coins, int amount, int index, vector<vector<int>>&dp){
+        if(index==0) {
+            if(amount%coins[0]==0){
+                return amount/coins[0];   // min coin tha we can return
+            }
+            else return 1e9;
         }
+         if(dp[index][amount]!=-1){
+             return dp[index][amount];
+         }
+        
+        int ans= 0, take=INT_MAX;
+        int notTake= solveMem(coins, amount, index-1, dp);
+        if(coins[index]<= amount){
+            take = 1 + solveMem(coins, amount-coins[index], index, dp);
+        }
+        ans= min(take, notTake);
+        return dp[index][amount]= ans;
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+      int n = coins.size();
+        
+        // // Approach-1: Recursion 
+        // int ans = solveRec(coins, amount, n-1);
+        
+          // Approach-2: Recursion +Memoization
+        vector<vector<int>>dp(n+1, vector<int>(amount+1, -1));
+        int ans = solveMem(coins, amount, n-1, dp);
+        
+        if(ans==(1e9)) return -1;
         return ans;
     }
 };
