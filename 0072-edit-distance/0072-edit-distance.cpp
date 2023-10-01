62b1b8fbae75e5cf9b901d7472633dd9b4f47538
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-10-02 01:56:10 +0530

    Time: 12 ms (54.24%), Space: 9.4 MB (23.10%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -51,13 +51,47 @@ public:
         }
         return dp[i][j] = ans;
     }
+    int solveTab(string word1, string word2){
+        vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1, 0));
+        for(int j=0; j<word2.length(); j++){
+            dp[word1.length()][j]=word2.length()-j;
+        }
+        for(int i=0; i<word1.length(); i++){
+            dp[i][word2.length()]=word1.length()-i;
+        }
+       for(int i=word1.length()-1; i>=0; i--){
+           for(int j=word2.length()-1; j>=0; j--){
+            int ans= 0;
+        if(word1[i]==word2[j]){
+            ans= dp[i+1][j+1];
+        }
+        else{
+            // insert 
+            int insertAns= 1 + dp[i][j+1];
+            
+            // delete 
+            int deleteAns = 1 + dp[i+1][j];
+            
+            // replace 
+            int replaceAns =1 + dp[i+1][j+1];
+            ans = min(insertAns, min(deleteAns, replaceAns));
+        }
+               dp[i][j]=ans;
+           }
+       }
+        return dp[0][0];
+    }
+    
     int minDistance(string word1, string word2) {
         
 //         // Approach-1: Recursion 
 //         return solveRec(word1, word2, 0, 0);
         
-        // Approach-2: Recursion + Mamoization 
-        vector<vector<int>>dp(word1.length(),vector<int>(word2.length()+1, -1));
-        return solveMem(word1, word2, 0, 0, dp);
+//         // Approach-2: Recursion + Mamoization 
+//         vector<vector<int>>dp(word1.length(),vector<int>(word2.length()+1, -1));
+//         return solveMem(word1, word2, 0, 0, dp);
+        
+        // Approach-3: Tabulation 
+        return solveTab(word1, word2);
     }
 };
