199cf2798c68ce794c3d017125f2efe1dc040630
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-10-04 19:40:39 +0530

    Added solution - LeetHub

--- Huffman Encoding - GFG/huffman-encoding.cpp
+++ Huffman Encoding - GFG/huffman-encoding.cpp
@@ -6,60 +6,62 @@ using namespace std;
 
 class Node{
   public:
-  int data;
-  Node* left;
-  Node* right;
-  
-  Node(int d){
-      data=d;
-      left=NULL;
-      right=NULL;
-  }
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int d){
+        data = d;
+        left =right =NULL;
+    }
 };
 class cmp{
   public:
-  bool operator()(Node*a, Node* b){
-      return a->data > b->data;
-  }
+   bool operator()(Node* a, Node* b){
+	    return a->data > b->data;
+	}
 };
 class Solution
 {
 	public:
+	
 	void traverse(Node* root, vector<string>&ans, string temp){
-	    if(root->left==NULL && root->right==NULL){
+	    if(root->left ==NULL && root->right==NULL){
 	        ans.push_back(temp);
 	        return;
 	    }
-	    traverse(root->left, ans, temp+"0");
-	    traverse(root->right, ans, temp+"1");
+	    
+	    traverse(root->left, ans, temp+'0');
+	    traverse(root->right, ans, temp+'1');
 	}
-		vector<string> huffmanCodes(string S,vector<int> f,int N)
+		vector<string> huffmanCodes(string S, vector<int> f, int N)
 		{
 		    // Code here
 		    priority_queue<Node*, vector<Node*>, cmp>pq;
 		    for(int i=0; i<N; i++){
-		        Node* temp= new Node(f[i]);
-		        pq.push(temp);
+		        Node* nod = new Node(f[i]);
+		        pq.push(nod);
 		    }
 		    
 		    while(pq.size()>1){
-		        Node* left = pq.top();
+		        Node* leftNode = pq.top();
 		        pq.pop();
 		        
-		        Node* right =pq.top();
+		        Node* rightNode = pq.top();
 		        pq.pop();
 		        
-		        Node* newNode = new Node(left->data + right->data);
-		        newNode->left= left;
-		        newNode->right= right;
+		        Node* newNode= new Node(leftNode->data + rightNode->data);
+		        newNode->left = leftNode;
+		        newNode->right =rightNode;
 		        pq.push(newNode);
 		    }
 		    
-		    Node* root= pq.top();
-		    vector<string>ans;
-		    string temp="";
-		    traverse(root, ans, temp);
-		    return ans;
+		  //  one node left in queue 
+		  Node* root= pq.top();
+		  vector<string>ans;
+		  string temp= "";
+		  traverse(root, ans, temp);
+		  return ans;
 		}
 };
 
