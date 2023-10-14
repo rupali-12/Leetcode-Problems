76802af1eaae8e35644dd7dcf6a68132fb0c9cf8
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-10-14 11:22:14 +0530

    Time: 4 ms (33.31%), Space: 8.6 MB (35.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        for(int i=0; i<nums.size()-1; i++){
-           for(int j=i+1; j<nums.size(); j++){
-               if(nums[i]>nums[j]){
-                   int temp =nums[i];
-                   nums[i]= nums[j];
-                   nums[j]=temp;
-               }
-           }
+        // for(int i=0; i<nums.size()-1; i++){
+        //    for(int j=i+1; j<nums.size(); j++){
+        //        if(nums[i]>nums[j]){
+        //            int temp =nums[i];
+        //            nums[i]= nums[j];
+        //            nums[j]=temp;
+        //        }
+        //    }
+        // }
+        
+        // Approach-2 ->Binary Search 
+        int i=0, mid=0, j=nums.size()-1;
+        while(mid<=j){
+            if(nums[mid]==0){
+                swap(nums[mid], nums[i]);
+                i++; mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[j]);
+                j--;
+            }
         }
+        return;
     }
 };
