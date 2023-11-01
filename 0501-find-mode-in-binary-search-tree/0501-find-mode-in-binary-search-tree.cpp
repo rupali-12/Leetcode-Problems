40c5db623aa2b88d973db005cd6c2d18410b3b7a
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-01 22:51:33 +0530

    Time: 13 ms (77.45%), Space: 24.4 MB (82.92%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,6 +11,8 @@
  */
 class Solution {
 public:
+    
+    
     void inOrder(TreeNode* root, unordered_map<int, int>&mp){
         if(root==NULL) return;
         
@@ -18,24 +20,59 @@ public:
         mp[root->val]++;
         inOrder(root->right, mp);
     }
+    void dfs(TreeNode* root, int &currNum, int &currFreq, int &maxFreq, vector<int>&ans){
+        if(!root) return;
+        
+        // left call 
+        dfs(root->left, currNum,currFreq, maxFreq, ans);
+        
+        if(root->val == currNum){
+            currFreq++;
+        }
+        else{
+            currNum = root->val;
+            currFreq=1;
+        }
+        
+        // update max frequency 
+        if(currFreq > maxFreq){
+            // update  
+            ans ={}; 
+            maxFreq= currFreq;
+        }
+        
+        // update ans 
+        if(currFreq==maxFreq){
+            ans.push_back(root->val);
+        }
+        
+        // right call 
+        dfs(root->right, currNum,currFreq, maxFreq, ans);
+    }
     vector<int> findMode(TreeNode* root) {
         // base case
-         vector<int>ans;
-       // Approach-1: Unordered_map 
-        int maxFreq= 0;
-        unordered_map<int, int>mp;
-        inOrder(root, mp);
+//        // Approach-1: Unordered_map 
+          // vector<int>ans;
+//         int maxFreq= 0;
+//         unordered_map<int, int>mp;
+//         inOrder(root, mp);
         
-        for(auto a: mp){
-            if(a.second>maxFreq){
-                maxFreq = a.second;
-                ans ={};    // reset ans
-                ans.push_back(a.first);
-            }
-            else if(a.second == maxFreq){
-                ans.push_back(a.first);
-            }
-        }
+//         for(auto a: mp){
+//             if(a.second>maxFreq){
+//                 maxFreq = a.second;
+//                 ans ={};    // reset ans
+//                 ans.push_back(a.first);
+//             }
+//             else if(a.second == maxFreq){
+//                 ans.push_back(a.first);
+//             }
+//         }
+//         return ans;
+        
+        // Approach-2: optimized Approach 
+        int currNum=0, currFreq=0, maxFreq=0;
+          vector<int>ans;
+        dfs(root, currNum, currFreq, maxFreq, ans);
         return ans;
     }
 };
