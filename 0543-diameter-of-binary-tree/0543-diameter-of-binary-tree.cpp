bd48ce4f39f33b870252ea61e0350025abb1f778
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-05 19:48:58 +0530

    Time: 21 ms (5.57%), Space: 20.6 MB (32.59%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,17 +11,17 @@
  */
 class Solution {
 public:
-//         // Approahc-2:  works on GFG 
+// //         // Approahc-2:  works on GFG 
 //      int solve(TreeNode* root, int& res){
-//           if(root==NULL) return 0;
-//         int left= solve(root->left, res);
-//         int right= solve(root->right, res);
-//         // case when root combined with left and right and pass to root
-//          int temp= max(left, right) +1;
-//          int ans= max(temp, 1+left+right);
+//        if(root==NULL) return 0;
+//          int left= solve(root->left, res);
+//          int right = solve(root->right, res);
+//         int temp = 1 + max(left, right);
+//          // case when root combined with left and right and pass to root 
+//         int ans= max(temp, 1 +left+right);
+//          res= max(ans, res);
+//          return res;
          
-//          res= max(res, ans);
-//         return temp;
 //     }
 //      int diameterOfBinaryTree(TreeNode* root) {
 //       int res=INT_MIN;
@@ -43,7 +43,7 @@ public:
         if(root==NULL) return 0;
         int left= diameterOfBinaryTree(root->left);
         int right= diameterOfBinaryTree(root->right);
-        int combLeftRight = height(root->left) + height(root->right);
+        int combLeftRight = height(root->left) + height(root->right);  // no need to add 1 as  1 already count
         int ans= max(left, max(right, combLeftRight));
         return ans;
     }
