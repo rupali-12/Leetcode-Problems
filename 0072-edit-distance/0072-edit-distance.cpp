f7c82eb1dc7ce73058faa99aa6cd7222a819c41b
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-08 21:03:45 +0530

    Time: 12 ms (53.25%), Space: 9.1 MB (71.47%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,137 +1,130 @@
 class Solution {
 public:
     int solveRec(string word1, string word2, int i, int j){
-        if(i==word1.length()){
-            return word2.length()-j;
-        }
+       if(i==word1.length()){
+           return word2.length()-j;
+       }
         if(j==word2.length()){
-            return word1.length()-i;
-        }
+           return word1.length()-i;
+       }
         int ans= INT_MAX;
         if(word1[i]==word2[j]){
-            return solveRec(word1, word2, i+1,j+1);
+            return solveRec(word1, word2, i+1, j+1);
         }
         else{
             // insert 
-            int insertAns= 1 + solveRec(word1, word2, i, j+1);
+            int insertAns=  1+ solveRec(word1, word2, i, j+1);
             
             // delete 
-            int deleteAns = 1 + solveRec(word1, word2, i+1, j);
+             int deleteAns=  1+ solveRec(word1, word2, i+1, j);
             
             // replace 
-            int replaceAns =1 + solveRec(word1, word2, i+1, j+1);
-            ans = min(insertAns, min(deleteAns, replaceAns));
+             int replaceAns=  1+ solveRec(word1, word2, i+1, j+1);
+            ans= min(insertAns, min(deleteAns, replaceAns));
         }
         return ans;
     }
      int solveMem(string word1, string word2, int i, int j, vector<vector<int>>&dp){
-        if(i==word1.length()){
-            return word2.length()-j;
-        }
+       if(i==word1.length()){
+           return word2.length()-j;
+       }
         if(j==word2.length()){
-            return word1.length()-i;
-        }
+           return word1.length()-i;
+       }
          if(dp[i][j]!=-1){
              return dp[i][j];
          }
         int ans= INT_MAX;
         if(word1[i]==word2[j]){
-            return solveMem(word1, word2, i+1,j+1, dp);
+            return solveRec(word1, word2, i+1, j+1);
         }
         else{
             // insert 
-            int insertAns= 1 + solveMem(word1, word2, i, j+1, dp);
+            int insertAns=  1+ solveRec(word1, word2, i, j+1);
             
             // delete 
-            int deleteAns = 1 + solveMem(word1, word2, i+1, j, dp);
+             int deleteAns=  1+ solveRec(word1, word2, i+1, j);
             
             // replace 
-            int replaceAns =1 + solveMem(word1, word2, i+1, j+1, dp);
-            ans = min(insertAns, min(deleteAns, replaceAns));
+             int replaceAns=  1+ solveRec(word1, word2, i+1, j+1);
+            ans= min(insertAns, min(deleteAns, replaceAns));
         }
-        return dp[i][j] = ans;
+        return dp[i][j]= ans;
     }
     int solveTab(string word1, string word2){
-        vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1, 0));
+     vector<vector<int>>dp(word1.length()+1, vector<int>(word2.length()+1, 0)); 
         for(int j=0; j<word2.length(); j++){
-            dp[word1.length()][j]=word2.length()-j;
+            dp[word1.length()][j]= word2.length()-j;
         }
         for(int i=0; i<word1.length(); i++){
-            dp[i][word2.length()]=word1.length()-i;
+            dp[i][word2.length()]= word1.length()-i;
         }
-       for(int i=word1.length()-1; i>=0; i--){
-           for(int j=word2.length()-1; j>=0; j--){
-            int ans= 0;
-        if(word1[i]==word2[j]){
-            ans= dp[i+1][j+1];
+        
+        for(int i=word1.length()-1; i>=0; i--){
+            for(int j= word2.length()-1;  j>=0; j--){
+                int ans= 0;
+                if(word1[i]==word2[j]){
+                    ans = dp[i+1][j+1];
+                }
+                else{
+                    int insertAns = 1 + dp[i][j+1];
+                    int deleteAns = 1 + dp[i+1][j];
+                    int replaceAns = 1 + dp[i+1][j+1];
+                    ans= min(insertAns, min(deleteAns, replaceAns));
+                }
+                dp[i][j]= ans;
+            }
         }
-        else{
-            // insert 
-            int insertAns= 1 + dp[i][j+1];
-            
-            // delete 
-            int deleteAns = 1 + dp[i+1][j];
-            
-            // replace 
-            int replaceAns =1 + dp[i+1][j+1];
-            ans = min(insertAns, min(deleteAns, replaceAns));
-        }
-               dp[i][j]=ans;
-           }
-       }
         return dp[0][0];
     }
-    int solveTabSpOpt(string word1, string word2){
-       vector<int>curr(word2.length()+1, 0);
-         vector<int>next(word2.length()+1, 0);
-      if(word1.length()==0){
-            return word2.length();
-        }
-        if(word2.length()==0){
-            return word1.length();
-        }
-        for(int j=0; j<word2.length(); j++){
-            next[j]=word2.length()-j;
-        }
-       for(int i=word1.length()-1; i>=0; i--){
-           for(int j=word2.length()-1; j>=0; j--){
-                curr[word2.length()]=word1.length()-i;
-            int ans= 0;
-        if(word1[i]==word2[j]){
-            ans= next[j+1];
-        }
-        else{
-            // insert 
-            int insertAns= 1 + curr[j+1];
+//     int solveTabSpOpt(string word1, string word2){
+//        vector<int>curr(word2.length()+1, 0);
+//          vector<int>next(word2.length()+1, 0);
+//       if(word1.length()==0){
+//             return word2.length();
+//         }
+//         if(word2.length()==0){
+//             return word1.length();
+//         }
+//         for(int j=0; j<word2.length(); j++){
+//             next[j]=word2.length()-j;
+//         }
+//        for(int i=word1.length()-1; i>=0; i--){
+//            for(int j=word2.length()-1; j>=0; j--){
+//                 curr[word2.length()]=word1.length()-i;
+//             int ans= 0;
+//         if(word1[i]==word2[j]){
+//             ans= next[j+1];
+//         }
+//         else{
+//             // insert 
+//             int insertAns= 1 + curr[j+1];
             
-            // delete 
-            int deleteAns = 1 + next[j];
+//             // delete 
+//             int deleteAns = 1 + next[j];
             
-            // replace 
-            int replaceAns =1 + next[j+1];
-            ans = min(insertAns, min(deleteAns, replaceAns));
-        }
-               curr[j]=ans;
-           }
-           next= curr;
-       }
-        return next[0];
+//             // replace 
+//             int replaceAns =1 + next[j+1];
+//             ans = min(insertAns, min(deleteAns, replaceAns));
+//         }
+//                curr[j]=ans;
+//            }
+//            next= curr;
+//        }
+//         return next[0];
         
-    }
+//     }
     
     int minDistance(string word1, string word2) {
+        // // Approach-1: Recursion 
+        // return solveRec(word1, word2, 0, 0);
         
-//         // Approach-1: Recursion 
-//         return solveRec(word1, word2, 0, 0);
+        // // Approach-2: Recursion + Memoization
+        // vector<vector<int>>dp(word1.length(), vector<int>(word2.length(), -1));
+        // return solveMem(word1, word2, 0, 0, dp);
         
-//         // Approach-2: Recursion + Mamoization 
-//         vector<vector<int>>dp(word1.length(),vector<int>(word2.length()+1, -1));
-//         return solveMem(word1, word2, 0, 0, dp);
-        
-//         // Approach-3: Tabulation 
-//         return solveTab(word1, word2);
-          
-        // Approach-4: Tabulation + space optimization 
-        return solveTabSpOpt(word1, word2);
+           // Approach-3: Tabulation
+        return solveTab(word1, word2);
+
     }
 };
