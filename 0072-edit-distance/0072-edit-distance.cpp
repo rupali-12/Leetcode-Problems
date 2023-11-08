c0ce9ae3712ad0661c9d6f6d94168d2ad1891618
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-08 21:15:01 +0530

    Time: 4 ms (92.16%), Space: 6.7 MB (97.75%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -77,43 +77,38 @@ public:
         }
         return dp[0][0];
     }
-//     int solveTabSpOpt(string word1, string word2){
-//        vector<int>curr(word2.length()+1, 0);
-//          vector<int>next(word2.length()+1, 0);
-//       if(word1.length()==0){
-//             return word2.length();
-//         }
-//         if(word2.length()==0){
-//             return word1.length();
-//         }
-//         for(int j=0; j<word2.length(); j++){
-//             next[j]=word2.length()-j;
-//         }
-//        for(int i=word1.length()-1; i>=0; i--){
-//            for(int j=word2.length()-1; j>=0; j--){
-//                 curr[word2.length()]=word1.length()-i;
-//             int ans= 0;
-//         if(word1[i]==word2[j]){
-//             ans= next[j+1];
-//         }
-//         else{
-//             // insert 
-//             int insertAns= 1 + curr[j+1];
-            
-//             // delete 
-//             int deleteAns = 1 + next[j];
-            
-//             // replace 
-//             int replaceAns =1 + next[j+1];
-//             ans = min(insertAns, min(deleteAns, replaceAns));
-//         }
-//                curr[j]=ans;
-//            }
-//            next= curr;
-//        }
-//         return next[0];
+    int solveTabSpOpt(string word1, string word2){
+        if(word1.length()==0 && word2.length()==0) return 0;
+        if(word1.length()==0 || word2.length()==0) return 1;
+       vector<int>curr(word2.length()+1, 0);
+         vector<int>next(word2.length()+1, 0);
         
-//     }
+       // Base case 
+         for(int j=0; j<word2.length(); j++){
+            next[j]= word2.length()-j;
+        }
+       
+        
+        for(int i=word1.length()-1; i>=0; i--){
+            for(int j= word2.length()-1;  j>=0; j--){
+                curr[word2.length()] = word1.length()-i;
+                int ans= 0;
+                if(word1[i]==word2[j]){
+                    ans = next[j+1];
+                }
+                else{
+                    int insertAns = 1 + curr[j+1];
+                    int deleteAns = 1 + next[j];
+                    int replaceAns = 1 + next[j+1];
+                    ans= min(insertAns, min(deleteAns, replaceAns));
+                }
+                curr[j]= ans;
+            }
+            next=curr;
+        }
+        return next[0];
+        
+    }
     
     int minDistance(string word1, string word2) {
         // // Approach-1: Recursion 
@@ -123,8 +118,12 @@ public:
         // vector<vector<int>>dp(word1.length(), vector<int>(word2.length(), -1));
         // return solveMem(word1, word2, 0, 0, dp);
         
-           // Approach-3: Tabulation
-        return solveTab(word1, word2);
+//            // Approach-3: Tabulation
+//         return solveTab(word1, word2);
+        
+                
+           // Approach-4: Tabulation + space Optimization
+        return solveTabSpOpt(word1, word2);
 
     }
 };
