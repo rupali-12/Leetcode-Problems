55318176bd9dcba7590b6c5064f54dbce1d3f8a3
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-10 13:58:43 +0530

    Time: 0 ms (100.00%), Space: 21 MB (31.48%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,44 +1,26 @@
-#include <regex>
-#include <string>
-
 class Solution {
 public:
-    string reverseWords(string S) {
-         S = std::regex_replace(S, std::regex("^\\s+|\\s+$"), "");
-        string t="";
-      bool lastCharSpace=false;
-        for(int i=0; i<S.length(); i++){
-            if(S[i]==' '){
-                if(!lastCharSpace){
-                    t.push_back(S[i]);
-                    lastCharSpace=true;
-                }
-            }
-            else{
-               t.push_back(S[i]);
-                    lastCharSpace=false;
-            }
-        }
-        // cout<<"T=> "<<t<<" ";
-        S=t;
-          string ans="";
+    string reverseWords(string s) {
+        string ans="";
         string temp="";
-        for(int i=S.length()-1; i>=0; i--){
-            if(S[i]!=' '){
-                temp.push_back(S[i]);
+        int i=0, n= s.length();
+        while(i<n){
+            while(i<n && s[i]==' ') i++;
+            while(i<n && s[i]!=' '){
+                temp+= s[i];
+                i++;
             }
-            else{
-                reverse(temp.begin(),temp.end());
-                ans = ans+temp+" ";
+            
+            if(temp.size()>0){
+                if(ans.size()==0){
+                    ans=temp;
+                }
+                else{
+                    ans = temp+" "+ans;
+                }
                 temp="";
-                // count=0;
             }
         }
-        // Handle last move 
-         reverse(temp.begin(),temp.end());
-        ans+= temp;
         return ans;
     }
 };
-
-
