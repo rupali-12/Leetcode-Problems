52e4164c784d74ab7ee1d315147ca501a221ce75
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-13 13:22:27 +0530

    Time: 27 ms (76.40%), Space: 12.6 MB (73.95%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.size() || j>= grid[0].size()) return;
+        if(grid[i][j]=='0') return;
+        
+        grid[i][j]='0';
+        
+        solve(grid, i, j+1);
+        solve(grid, i, j-1);
+        solve(grid, i+1, j);
+        solve(grid, i-1, j);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int count=0;
+        
+        for(int i=0; i< grid.size(); i++){
+            for(int j= 0; j<grid[0].size(); j++){
+               if(grid[i][j]=='1'){
+                    solve(grid, i, j);
+                    count++;
+               }
+            }
+        }
+        return count;
+    }
+};
