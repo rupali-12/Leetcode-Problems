e6a5ed7dbb6ff36ef4e93ee1746c0da184a40ff7
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-14 12:05:50 +0530

    Time: 196 ms (84.15%), Space: 135 MB (64.13%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-               vector<int>ans;
-       deque<int>dq;
-      
-       for(int i=0; i<k; i++){
-           while(!dq.empty() && dq.back()<arr[i]){
-               dq.pop_back();
-           }
-           dq.push_back(arr[i]);
-       }
-       ans.push_back(dq.front());
-      for(int i=k; i<arr.size(); i++){
-          if(dq.front()==arr[i-k]){    // bcoz if this is true then that element can never be the answer for nxt window 
-              dq.pop_front();     
-          }
-          
-        while(!dq.empty() && dq.back()<arr[i]){
-               dq.pop_back();
-           }
-           dq.push_back(arr[i]);
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int>ans;
+        deque<int>dq;
+        
+        for(int i=0; i<k; i++){
+            while(!dq.empty() && dq.back()<nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(nums[i]);
+        }
            ans.push_back(dq.front());
-      }
+        
+        for(int i=k; i<nums.size(); i++){
+            if(dq.front() == nums[i-k]){    // bcoz if this is true then that element can never be the answer for nxt window 
+                dq.pop_front();           
+            }
+            
+             while(!dq.empty() && dq.back()<nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(nums[i]);
+            ans.push_back(dq.front());
+        }
         return ans;
     }
 };
