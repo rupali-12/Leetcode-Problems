9d0cf77d90914363e102b1ced600b543a69daff0
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-14 13:15:15 +0530

    Time: 3 ms (97.89%), Space: 10 MB (91.27%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -45,6 +45,30 @@ public:
         }
         return dp[m-1][n-1];
     }
+    int solveTabSpOpt(vector<vector<int>>& grid,int m,int n){
+        vector<int>prev(n,0);
+        vector<int>curr(n,0);
+        
+        for(int row=0; row<m;row++){
+            for(int col=0; col<n;col++){
+                if(row==0 && col==0){
+                    curr[row]= grid[row][col];
+                }
+                else{
+                    int up=1e9, left=1e9;
+                    if(row>0){
+                        up= grid[row][col] + prev[col];
+                    }
+                    if(col>0){
+                        left= grid[row][col]+curr[col-1];
+                    }
+                    curr[col]= min(up,left);
+                }
+            }
+            prev= curr;
+        }
+        return prev[n-1];
+    }
     int minPathSum(vector<vector<int>>& grid) {
          int m=grid.size(), n= grid[0].size(); 
      
@@ -57,7 +81,10 @@ public:
         // vector<vector<int>>dp(m, vector<int>(n, -1));
         // return solveMem(grid, startRow, startCol, dp);
         
-        // Approach-3: Tabulation
-        return solveTab(grid,m, n);
+        // // Approach-3: Tabulation
+        // return solveTab(grid,m, n);
+        
+       // Approach-4: Tabulation +space optimization
+        return solveTabSpOpt(grid,m, n);
     }
 };
