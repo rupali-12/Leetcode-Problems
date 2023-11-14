d31c53348bcc722d6e97b63248ba87a214e17e12
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-14 13:09:09 +0530

    Time: 7 ms (71.43%), Space: 10.4 MB (31.07%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -23,6 +23,28 @@ public:
         int left = grid[startRow][startCol] + solveMem(grid, startRow, startCol-1, dp);
         return  dp[startRow][startCol]= min(up, left);
     }
+    int solveTab(vector<vector<int>>& grid,int m, int n){
+        vector<vector<int>>dp(m, vector<int>(n+1, 0)); 
+        
+        for(int srow=0; srow<m; srow++){
+            for(int scol=0; scol<n; scol++){
+                if(srow==0 && scol==0){
+                    dp[srow][scol] =grid[srow][scol];
+                }
+                else{
+                    int up=1e9, down=1e9;
+            if(srow>0){
+                up= grid[srow][scol] + dp[srow-1][scol];
+            }
+            if(scol>0){
+                down= grid[srow][scol] + dp[srow][scol-1];
+            }
+            dp[srow][scol]= min(up, down);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
     int minPathSum(vector<vector<int>>& grid) {
          int m=grid.size(), n= grid[0].size(); 
      
@@ -30,9 +52,12 @@ public:
         // int startRow= m-1, startCol=n-1;
         // return solveRec(grid, startRow, startCol);
         
-         // Approach-2: Recursion + Memoization
-        int startRow= m-1, startCol=n-1;
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        return solveMem(grid, startRow, startCol, dp);
+        //  // Approach-2: Recursion + Memoization
+        // int startRow= m-1, startCol=n-1;
+        // vector<vector<int>>dp(m, vector<int>(n, -1));
+        // return solveMem(grid, startRow, startCol, dp);
+        
+        // Approach-3: Tabulation
+        return solveTab(grid,m, n);
     }
 };
