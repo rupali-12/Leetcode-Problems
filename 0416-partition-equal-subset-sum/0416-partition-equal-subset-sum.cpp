45d8304471e0f5a0d5410c0ab1a6b1e41ae11548
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-11-23 18:02:33 +0530

    Time: 223 ms (70.46%), Space: 96.1 MB (22.08%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,87 +1,45 @@
 class Solution {
 public:
-    bool solveRec(vector<int>&arr, int index, int sum){
-      if(sum==0) return true;
-      if(index==0) return arr[index]==sum;
-
-      bool notTake =solveRec(arr, index-1, sum);
-      bool take= false;
-      if(arr[index]<=sum){
-          take = solveRec(arr, index-1, sum-arr[index]);
-      }
-      return take | notTake;
-   }
-   bool solveMem(vector<int>&arr, int index, int sum, vector<vector<int>>&dp){
-      if(sum==0) return true;
-      if(index==0) return arr[index]==sum;
-   if(dp[index][sum]!=-1){
-       return dp[index][sum];
-   }
-      bool notTake =solveMem(arr, index-1, sum, dp);
-      bool take= false;
-      if(arr[index]<=sum){
-          take = solveMem(arr, index-1, sum-arr[index], dp);
-      }
-      return dp[index][sum] =  take | notTake;
-   }
-     bool solveTab(vector<int>&nums, int n, int target){
-        vector<vector<int>>dp(n+1, vector<int>(target+1, 0));
-        for(int i=0; i<n;i++){
-            dp[i][0]=1;
+    bool solveRec(vector<int>nums, int index, int target){
+        if(target==0) return true;
+        if(index==0) return nums[index]==target;
+        
+        bool notTake= solveRec(nums, index-1, target);
+        bool take= false;
+        if(nums[index]<= target){
+            take= solveRec(nums, index-1, target-nums[index]);
         }
-        for(int index=n-1; index>=0; index--){
-            for(int t=1; t<=target; t++){
-                
-                bool inc=false;
-               if(t-nums[index]>=0){
-                   inc = dp[index+1][t-nums[index]];
-               }
-                bool exc = dp[index+1][t];
-                dp[index][t] = inc | exc;
-            }
-        }
-        return dp[0][target];
+        return take | notTake;
     }
-      bool solveTabSpOpt(vector<int>&nums, int n, int target){
-       vector<int>curr(target+1, 0);
-       vector<int>next(target+1, 0);
-       curr[0]= next[0]=1;
-        for(int index=n-1; index>=0; index--){
-            for(int t=1; t<=target; t++){
-                
-                bool inc=false;
-               if(t-nums[index]>=0){
-                   inc =next[t-nums[index]];
-               }
-                bool exc = next[t];
-                curr[t] = inc || exc;
-            }
-            next= curr;
+    bool solveMem(vector<int>&nums, int index, int target,vector<vector<int>>&dp){
+        if(target==0) return true;
+        if(index==0) return nums[index]==target;
+        if(dp[index][target]!=-1){
+            return dp[index][target];
         }
-        return next[target];
+        
+        bool notTake= solveMem(nums, index-1, target, dp);
+        bool take= false;
+        if(nums[index]<= target){
+            take= solveMem(nums, index-1, target-nums[index], dp);
+        }
+        return dp[index][target]= take | notTake;
     }
     bool canPartition(vector<int>& nums) {
+        int n= nums.size();
         int total=0;
-        int n = nums.size();
         for(int i=0; i<n; i++){
-         total+= nums[i];   
+            total+= nums[i];
         }
-        if(total%2!=0){
-            return false;
-        }
-        int target= total/2;
         
-        // // Approach-1: Recursion 
+        if(total%2!=0) return false;
+        int target = total/2;
+        
+        // // Approach -1: Recursion
         // return solveRec(nums, n-1, target);
         
-        //  // Approach-2: Recursion + Memoization
-        // vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
-        // return solveMem(nums, n-1, target, dp);
-        
-        //   // Approach-3:Tabulation
-        // return solveTab(nums, n, target);
-        
-           // Approach-4:Tabulation + space optimization
-        return solveTabSpOpt(nums, n, target);
+         // Approach -2: Recursion + Memoization
+        vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
+        return solveMem(nums, n-1, target, dp);
     }
 };
