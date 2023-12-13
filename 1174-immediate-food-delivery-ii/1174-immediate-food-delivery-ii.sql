07173ed06b044010522c9d653626773f171f295d
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-13 11:03:00 +0530

    Time: 1162 ms (45.64%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -2,6 +2,15 @@
 
 # subquery 
 
-select round( (sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100)/ count(distinct customer_id) ,2) as immediate_percentage from delivery where (customer_id, order_date) in (select customer_id, min(order_date) as first_order_date from delivery group by customer_id);
+# select round( (sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100)/ count(distinct customer_id) ,2) as immediate_percentage from delivery where (customer_id, order_date) in (select customer_id, min(order_date) as first_order_date from delivery group by customer_id);
 
 
+
+
+# Using rank 
+
+with cte as (
+select *, rank() over(partition by customer_id order by order_date) as order_number, case when order_date= customer_pref_delivery_date then 'immediate' else 'scheduled' end as order_type from delivery
+)
+
+select round(sum(case when order_type='immediate' then 1 else 0 end)/count(*)*100 ,2) as immediate_percentage from cte where order_number =1;
