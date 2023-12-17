6808540ac312653b8cec5aa952ab5b9298ab8ac3
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-17 14:18:25 +0530

    Time: 379 ms (70.67%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
 # select * from cinema where description != "boring" and  id mod 2 !=0 order by rating desc; 
-# select * from cinema where description <> "boring" and  id mod 2 !=0 order by rating desc; 
 
 select * from cinema where description<> 'boring' and id mod 2=1 order by rating desc;
