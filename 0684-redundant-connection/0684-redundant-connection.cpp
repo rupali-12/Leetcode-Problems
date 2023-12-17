ea8466df3bf5e5e3c4c4db1ff0bc08fa76e62e76
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-17 17:03:11 +0530

    Time: 3 ms (93.60%), Space: 80.7 MB (5.07%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -5,12 +5,39 @@ public:
         return findParent(parent[element], parent);
     }
     
-    // Approach-1: Using disjoint set 
+//     // Approach-1: Using disjoint set 
+//     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+//         int n= edges.size();
+//         vector<int>parent(n+1);
+//         for(int i=0; i<=n; i++){
+//             parent[i]=i;
+//         }
+        
+//         // process each edge 
+//         for(vector<int>v: edges){
+//         // [[1,2],[2,3],[3,4],[1,4],[1,5]]
+//             int el1 = v[0];
+//             int el2 = v[1];
+            
+//             int p1 = findParent(el1, parent);
+//             int p2 = findParent(el2, parent);
+//             if(p1==p2) return v;
+//             else{
+//                 parent[p2] =p1;
+//             }
+//         }
+//         return {};
+//     }
+    
+    // **************************************************************
+     // Approach-2: Using Rank 
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n= edges.size();
         vector<int>parent(n+1);
+        vector<int>rank(n+1);
         for(int i=0; i<=n; i++){
             parent[i]=i;
+            rank[i]=1;
         }
         
         // process each edge 
@@ -23,7 +50,16 @@ public:
             int p2 = findParent(el2, parent);
             if(p1==p2) return v;
             else{
-                parent[p2] =p1;
+                if(rank[p1]<rank[p2]){
+                    parent[p1]=p2;
+                }
+                else if(rank[p1]>rank[p2]){
+                    parent[p2]=p1;
+                }
+                else{
+                    parent[p1]=p2;
+                    rank[p2]++;
+                }
             }
         }
         return {};
