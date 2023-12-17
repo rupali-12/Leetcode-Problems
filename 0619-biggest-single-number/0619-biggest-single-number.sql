03fbd224c739a5cf64247164e2e28dd37bdcd2f0
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-17 14:15:05 +0530

    Time: 856 ms (28.43%), Space: 0B (100.00%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -2,4 +2,7 @@
 with cte as
 (select num from mynumbers group by num having count(num)=1)
 
-select max(num) as num from cte;
+# select max(num) as num from cte;
+
+# handle null case  
+select case when count(*)>0 then max(num) else null end as num from cte;
