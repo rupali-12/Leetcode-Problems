2e3ab6d62b45dfb00a9431097f621b74c4e0d04d
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-27 22:41:04 +0530

    Time: 2004 ms (5.02%), Space: 0B (100.00%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+# select employee_id, case when count(department_id)=1 then department_id when count(department_id)>1 then sum((primary_flag='Y')*department_id) end as department_id
+# from employee group by employee_id;
+
+
+select employee_id, department_id from employee where primary_flag='Y'
+union 
+select employee_id, department_id from employee group by employee_id having count(department_id) =1;
