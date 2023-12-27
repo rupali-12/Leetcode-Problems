823622aa9bff2f842390bae5cea77b1652208e06
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-27 22:47:39 +0530

    Time: 1079 ms (42.13%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,9 +1,14 @@
 # Write your MySQL query statement below
 
-# select employee_id, case when count(department_id)=1 then department_id when count(department_id)>1 then sum((primary_flag='Y')*department_id) end as department_id
-# from employee group by employee_id;
 
+with cte as(
+select employee_id, case when count(department_id)=1 then department_id when count(department_id)>1  then sum((primary_flag='Y')*department_id) end as department_id
+from employee group by employee_id
+)
 
-select employee_id, department_id from employee where primary_flag='Y'
-union 
-select employee_id, department_id from employee group by employee_id having count(department_id) =1;
+select * from cte where department_id>0;
+
+# # way-2 -> union 
+# select employee_id, department_id from employee where primary_flag='Y'
+# union 
+# select employee_id, department_id from employee group by employee_id having count(department_id) =1;
