bc0edc955ed5d77f0e4e4a09fe51201ea11465f6
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-29 14:49:59 +0530

    Time: 3615 ms (7.69%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -3,4 +3,8 @@ with cte as(
 select o.com_id , o.sales_id from orders o left join company c on o.com_id = c.com_id where c.name = 'RED'
 )
 
+# com_id   sales_id 
+#   1         1
+#   1         4
+
 select s.name from salesperson s left join cte c on c.sales_id = s.sales_id where c.com_id is null;
