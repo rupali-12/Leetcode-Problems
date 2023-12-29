c5a92c715b45fd47062d97c873a9e16fc0c5e6fc
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-29 14:53:43 +0530

    Time: 2469 ms (69.62%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -6,5 +6,8 @@ select o.com_id , o.sales_id from orders o left join company c on o.com_id = c.c
 # com_id   sales_id 
 #   1         1
 #   1         4
+# # Way-1>>>>
+# select s.name from salesperson s left join cte c on c.sales_id = s.sales_id where c.com_id is null;
 
-select s.name from salesperson s left join cte c on c.sales_id = s.sales_id where c.com_id is null;
+# # way-2>> 
+select name from salesperson where sales_id  not in (select distinct sales_id from cte);
