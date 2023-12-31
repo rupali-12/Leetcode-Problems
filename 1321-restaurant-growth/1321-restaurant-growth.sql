c531df617ff3430b465060cfb46b73d6ff806247
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-31 11:18:51 +0530

    Time: 651 ms (59.93%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+with cte as (
+select visited_on, sum(amount) over(order by visited_on range between interval 6 day preceding and current row) as amount, min(visited_on) over() as first_day from customer 
+)
+
+select visited_on, amount, round(amount/7, 2) as average_amount from cte where visited_on >= first_day + 6 group by visited_on;
+
+
+
