557868a8be9a6d800c330842ba97ec2ce346a0da
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2023-12-31 11:19:07 +0530

    Time: 590 ms (86.20%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -4,7 +4,7 @@ with cte as (
 select visited_on, sum(amount) over(order by visited_on range between interval 6 day preceding and current row) as amount, min(visited_on) over() as first_day from customer 
 )
 
-select visited_on, amount, round(amount/7, 2) as average_amount from cte where visited_on >= first_day + 6 group by visited_on;
+select distinct visited_on, amount, round(amount/7, 2) as average_amount from cte where visited_on >= first_day + 6;
 
 
 
