ba74663a144100a43b2ab797c4a33b336ba52f56
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-01-04 14:29:44 +0530

    Time: 256 ms (80.47%), Space: 10.7 MB (28.47%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-    int solveRec(vector<int>& nums, int i, int j){
-	if(i>j) return 0;
-    int maxi= INT_MIN;
-	for(int k=i; k<=j; k++){
-		int numOfCoins = (nums[i-1]*nums[k]*nums[j+1]) + solveRec(nums, i, k-1)+ solveRec(nums, k+1, j);
-		maxi= max(maxi, numOfCoins);
-	}
-	return maxi;
-}
-    int solveMem(vector<int>& nums, int i, int j, vector<vector<int>>&dp){
-	if(i>j) return 0;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+    int solveRec(vector<int>&nums, int i, int j){
+        if(i>j){
+            return 0;
         }
-    int maxi= INT_MIN;
-	for(int k=i; k<=j; k++){
-		int numOfCoins = (nums[i-1]*nums[k]*nums[j+1]) + solveMem(nums, i, k-1, dp)+ solveMem(nums, k+1, j, dp);
-		maxi= max(maxi, numOfCoins);
-	}
-	return dp[i][j]= maxi;
-}
+        int cost =INT_MIN;
+        for(int k=i; k<=j; k++){
+            int temp = (nums[i-1]*nums[k]*nums[j+1]) + solveRec(nums, i, k-1) + solveRec(nums, k+1, j);
+            cost= max(cost, temp);
+        }
+        return cost;
+    }
+    
+     int solveMem(vector<int>&nums, int i, int j, vector<vector<int>>&dp){
+        if(i>j){
+            return 0;
+        }
+         if(dp[i][j]!=-1){
+             return dp[i][j];
+         }
+        int cost =INT_MIN;
+        for(int k=i; k<=j; k++){
+            int temp = (nums[i-1]*nums[k]*nums[j+1]) + solveRec(nums, i, k-1) + solveRec(nums, k+1, j);
+            cost= max(cost, temp);
+        }
+        return dp[i][j] = cost;
+    }
     int solveTab(vector<int>&nums, int n){
-        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
-        for(int i= n-2; i>=1; i--){
-            for(int j=1; j<= n-2; j++){
+       vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+        for(int i=n-2; i>=1; i--){
+            for(int j=1; j<=n-2; j++){
                 if(i>j) continue;
-                int maxi= INT_MIN;
+                int cost= INT_MIN;
                 for(int k=i; k<=j; k++){
-                    int cost= (nums[i-1] * nums[k]*nums[j+1]) + dp[i][k-1] + dp[k+1][j];
-                    maxi= max(maxi, cost);
+                    int temp = nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    cost= max(cost, temp);
                 }
-                dp[i][j] = maxi;
+                dp[i][j]=cost;
             }
         }
         return dp[1][n-2];
@@ -39,17 +44,17 @@ public:
     int maxCoins(vector<int>& nums) {
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        int n= nums.size();       //  1  2  1 5 4 1 
-        int i= 1, j= n-2;
+        int n= nums.size();
+        int i=1, j= n-2;
         
-        // // Approach-1: Recursion 
-        // return solveRec(nums, i, j);
-        
-        //  // // Approach-2: Recursion + Memoization
+//         // Approach-1 Recursion 
+//         return solveRec(nums, i, j);
+          
+        // // Approach-2 Recursion + Memoization 
         // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
         // return solveMem(nums, i, j, dp);
-
-        // Approach-3: Tabulation 
+        
+        // Approach-3 Recursion + Memoization 
         return solveTab(nums, n);
     }
 };
