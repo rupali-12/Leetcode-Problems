a14f4dcd6339c5502824aff7ad553b358e20f4c2
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-01-17 08:05:53 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (69.36%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        // Approach-1: Brute force 
-        unordered_map<int, int>mp;
-     for(auto a: arr){
-         mp[a]++;
-     }
-        set<int>s;
-        for(auto m: mp){
-            s.insert(m.second);
+     //    // Approach-1: Brute force 
+     //    unordered_map<int, int>mp;
+     // for(auto a: arr){
+     //     mp[a]++;
+     // }
+     //    set<int>s;
+     //    for(auto m: mp){
+     //        s.insert(m.second);
+     //    }
+     //    return mp.size()==s.size();
+        
+        // Approach-2: Sorting 
+        vector<int>v;
+        sort(arr.begin(), arr.end());
+        // store count of occurences of each element 
+        for(int i=0; i<arr.size(); i++){
+            int cnt=1;
+            while(i+1 < arr.size() && arr[i]==arr[i+1]){
+                cnt++;
+                i++;
+            }
+            v.push_back(cnt);
         }
-        return mp.size()==s.size();
+        
+         sort(v.begin(), v.end());
+        for(int i=1; i<v.size(); i++){
+            if(v[i]==v[i-1]){
+                return false;
+            }
+        }
+        return true;
     }
 };
 
