37500cd88ed623157fa5d39c76401bcb73bf0561
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-01-18 08:27:15 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (8.66%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int solveRec(int n, int step){
+        if(n==0 || n==1 || step==n){
+            return 1;
+        }
+        if(step>n) return 0;
+        int take1 = solveRec(n, step+1);
+        int take2 = solveRec(n, step+2);
+        return take1 + take2;
+    }
+     int solveMem(int n, int step, vector<int>&dp){
+        if(n==0 || n==1 || step==n){
+            return 1;
+        }
+         if(step>n) return 0;
+         if(dp[n]!=-1){
+             return dp[n];
+         }
+        int take1 = solveMem(n, step+1, dp);
+        int take2 = solveMem(n, step+2, dp);
+        return dp[n] = take1 + take2;
+    }
+    int solveTab(int n, vector<int>&dp){
+        dp[0]=dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+    int climbStairs(int n) {
+      // // Approach-1: Recursion 
+      //   return solveRec(n, 0);
+        
+     // // Approach-2: Recursion + Memoization
+     //    vector<int>dp(n+1, -1);
+     //    return solveMem(n, 0, dp);
+        
+        // Approach- 3 Tabulation 
+        vector<int>dp(n+1, 0);
+        return solveTab(n, dp);
+    }
+};
