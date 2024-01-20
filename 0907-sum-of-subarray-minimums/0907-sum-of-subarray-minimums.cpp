577182b3d22c3fd9702bca7cb4224aec5a4ddd14
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-01-20 08:19:17 +0530

    Time: 84 ms (38.13%), Space: 51.6 MB (5.05%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-       int n= arr.size();
-        vector<long>next_smaller(n);
-        vector<long>prev_smaller(n);
-        stack<pair<long, long>>s1, s2;
-      // next smaller contribution
+        int n = arr.size();
+        vector<long long>next_smaller(n);
+        vector<long long>prev_smaller(n);
+        stack<pair<long long, long long>>s1, s2;
+        
+        // next smaller contribution 
         for(int i=0; i<n; i++){
             int count=1;
-            while(!s1.empty() && arr[i]<= s1.top().first){
+            while(!s1.empty() && arr[i]<=s1.top().first){
                 count+= s1.top().second;
                 s1.pop();
             }
             s1.push({arr[i], count});
-            next_smaller[i]= count;
+            next_smaller[i]=count;
         }
         
-        // prev smaller contribution 
-        for(int i=n-1; i>=0; i--){
+        // prev smaller constribution 
+        for(int i= n-1; i>=0; i--){
             int count=1;
-            while(!s2.empty() && arr[i]< s2.top().first){
+            while(!s2.empty() && arr[i]< s2.top().first){  // < only because subarray of length 1 taken already in next smaller contribution 
                 count+= s2.top().second;
                 s2.pop();
             }
@@ -27,12 +28,11 @@ public:
             prev_smaller[i]=count;
         }
         
-        long long sum=0;
-        long long mod=  1e9+7;
+        long long sum=0, mod= 1e9+7;
         for(int i=0; i<n; i++){
             sum+= arr[i]*next_smaller[i]*prev_smaller[i];
             sum%=mod;
         }
-    return sum;
-}
+        return sum;
+    }
 };
