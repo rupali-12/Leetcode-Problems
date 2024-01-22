38b7707cfd982dded09fb66e93d915920098534a
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-01-22 18:55:13 +0530

    Time: 19 ms (89.24%), Space: 23.7 MB (36.81%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int missingnum, repeatednum;
-        int calculatedSum=0, arraySum=0;
-        set<int>s;
-        int n=nums.size();
+        
+//         // Approach-1 : Using set 
+//         int missingnum, repeatednum;
+//         int calculatedSum=0, arraySum=0;
+//         set<int>s;
+//         int n=nums.size();
     
-        int expectedSum= (n*(n+1))/2;
+//         int expectedSum= (n*(n+1))/2;
+//         for(int i=0; i<nums.size(); i++){
+//             n= max(nums[i], n);
+//             s.insert(nums[i]);
+//             arraySum+= nums[i];
+//         }
+        
+//         for(auto a: s){
+//             calculatedSum += a;
+//         }
+//         missingnum = expectedSum - calculatedSum;
+//         repeatednum = arraySum - calculatedSum; 
+      
+//         return {repeatednum, missingnum};
+        
+        // ****************************************************************
+        
+        // Approach-2: Inplace 
+         int missingnum, repeatednum;
+        
         for(int i=0; i<nums.size(); i++){
-            n= max(nums[i], n);
-            s.insert(nums[i]);
-            arraySum+= nums[i];
+            if(nums[abs(nums[i])-1]<0){
+                repeatednum = abs(nums[i]);
+            }
+            else{
+                nums[abs(nums[i])-1]*=-1;
+            }
         }
         
-        for(auto a: s){
-            calculatedSum += a;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]>0) missingnum= i+1;
         }
-        missingnum = expectedSum - calculatedSum;
-        repeatednum = arraySum - calculatedSum; 
-      
-        return {repeatednum, missingnum};
+         return {repeatednum, missingnum};
     }
 };
