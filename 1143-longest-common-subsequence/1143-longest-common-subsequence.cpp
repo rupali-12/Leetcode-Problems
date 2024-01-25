7b3bafc4b539a4aee712942ee841401ce6b5fcbd
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-01-25 09:56:20 +0530

    Time: 24 ms (87.23%), Space: 25.7 MB (7.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,77 +1,48 @@
 class Solution {
 public:
     int solveRec(string text1, string text2, int i, int j){
-      if(i==text1.length() || j==text2.length()) return 0;
+        if(i==text1.length() || j==text2.length()) return 0;
         
-        if(text1[i]==text2[j]) return 1 + solveRec(text1, text2, i+1, j+1);
-        else{
-            return max(solveRec(text1, text2, i+1, j), solveRec(text1, text2, i, j+1));
-        }
-    }
-    int solveMem(string text1, string text2, int i, int j, vector<vector<int>>&dp){
-        if(i==text1.length() || j==text2.length()){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int ans=0;
         if(text1[i]==text2[j]){
-            ans= 1+solveMem(text1, text2, i+1, j+1, dp);
+            return 1+solveRec(text1, text2, i+1, j+1);
         }
-        else{
-            ans= max(solveMem(text1, text2, i+1, j, dp), solveMem(text1, text2, i, j+1, dp));
+        return max(solveRec(text1, text2, i+1, j), solveRec(text1, text2, i, j+1));
+    }
+    
+     int solveMem(string text1, string text2, int i, int j, vector<vector<int>>&dp){
+        if(i==text1.length() || j==text2.length()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(text1[i]==text2[j]){
+            return dp[i][j]= 1+solveMem(text1, text2, i+1, j+1, dp);
         }
-        return dp[i][j]=ans;
+        return dp[i][j]= max(solveMem(text1, text2, i+1, j, dp), solveMem(text1, text2, i, j+1, dp));
     }
-    int solveTab(string text1, string text2){
-         vector<vector<int>>dp(text1.length()+1, vector<int>(text2.length()+1, 0));
-        int ans=0;
-         for(int i= text1.length()-1; i>=0; i--){
-             for(int j= text2.length()-1; j>=0; j--){
-                 if(text1[i]==text2[j]){
-                     ans = 1 + dp[i+1][j+1];
-                 }
-                 else{
-                     ans= max(dp[i+1][j], dp[i][j+1]);
-                 }
-                 dp[i][j]=ans;
-             }
+    
+     int solveTab(string text1, string text2){
+          int len1= text1.length(), len2= text2.length();
+        vector<vector<int>>dp(len1+1, vector<int>(len2+1, 0));
+       for(int i=len1-1; i>=0; i--){
+           for(int j=len2-1; j>=0; j--){
+        if(text1[i]==text2[j]){
+            dp[i][j]= 1+ dp[i+1][j+1];
+        }
+         else{
+             dp[i][j]= max(dp[i+1][j], dp[i][j+1]);
          }
-        return dp[0][0];
+           }
+       }  
+       return dp[0][0];
     }
-    int solveSpOpt(string text1, string text2){
-        // i-> curr, i+1->next  so make vector of text2 length 
-       vector<int>curr(text2.length()+1, 0);
-        vector<int>next(text2.length()+1, 0);
-        int ans=0;
-         for(int i= text1.length()-1; i>=0; i--){
-             for(int j= text2.length()-1; j>=0; j--){
-                 if(text1[i]==text2[j]){
-                     ans = 1 + next[j+1];
-                 }
-                 else{
-                     ans= max(next[j], curr[j+1]);
-                 }
-                 curr[j]=ans;
-             }
-             next=curr;
-         }
-        return next[0];
-    }
+    
     int longestCommonSubsequence(string text1, string text2) {
-        // // // Approach-1> Using Recursion
-        // return solveRec(text1, text2, 0, 0);
-
-        // // // Approach-2> Using Recursion + Meemoization
-        // vector<vector<int>>dp(text1.length(), vector<int>(text2.length(), -1));
+//         // Approach-1: Recursion 
+//         return solveRec(text1, text2, 0, 0);
+                
+        // // Approach-2: Recursion + Memoization
+        // vector<vector<int>>dp(len1, vector<int>(len2, -1));
         // return solveMem(text1, text2, 0, 0, dp);
-       
-
-        // //  // Approach-3> Using Tabulation(Bottom up)
-        // return solveTab(text1, text2);
-
-           // Approach-4> Using Tabulation(Bottom up)+Space optimization
-        return solveSpOpt(text1, text2);
+        
+        // Approach-3: Tabulation
+        return solveTab(text1, text2);
     }
 };
