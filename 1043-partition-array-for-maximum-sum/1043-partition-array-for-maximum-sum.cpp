ed4f04b8a52076a003c7d899982372f74ff4666e
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-02-03 07:30:39 +0530

    Time: 3 ms (99.46%), Space: 10.9 MB (14.40%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -13,9 +13,29 @@ public:
         }
         return dp[i] = maxAns;
     }
+    int solveTab(vector<int>&arr, int k){
+        int n =arr.size();
+        vector<int>dp(n+1, 0);
+       for (int i = n - 1; i >= 0; i--) {
+            int len = 0, maxi = INT_MIN, maxAns = INT_MIN;
+
+            for (int j = i; j < min(n, i + k); j++) {
+                len++;
+                maxi = max(maxi, arr[j]);
+                int sum = len * maxi + dp[j + 1];
+                maxAns = max(maxAns, sum);
+            }
+
+            dp[i] = maxAns;
+        }
+        return dp[0];
+    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        // Memoization
-        vector<int>dp(arr.size(), -1);
-        return solveMem(arr, 0, k, dp);
+        // // Memoization
+        // vector<int>dp(arr.size(), -1);
+        // return solveMem(arr, 0, k, dp);
+        
+        // Tabulation 
+        return solveTab(arr, k);
     }
 };
