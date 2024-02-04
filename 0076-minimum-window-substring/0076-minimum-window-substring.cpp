4122f53826ac5de20b14ffaf1ea70789f9ff31e4
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-02-04 12:08:32 +0530

    Time: 24 ms (29.22%), Space: 10.3 MB (15.00%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+       if(t.length()>s.length()) return "";
+        int minWinSize=INT_MAX, i=0, j=0, start_i=0;
         unordered_map<char, int>mp;
-        for(int i=0; i<t.length(); i++){
-            mp[t[i]]++;
+        for(auto a: t){
+            mp[a]++;
         }
         int reqCount= t.length();
-        int i=0, j=0;
-        int minWinSize=INT_MAX;
-        int start_i=0;
         while(j<s.length()){
-            char ch= s[j];
-            if(mp[ch]>0){
+            char ch = s[j];
+            if(mp[ch]>0){    //  it is required so take from map and decrease its count
                 reqCount--;
             }
             mp[ch]--;
             
-            // Start shrinking window 
+            // shrink window 
             while(reqCount==0){
                 int currWinSize=j-i+1;
                 if(minWinSize>currWinSize){
-                    minWinSize = currWinSize;
-                    start_i= i;
+                    minWinSize= currWinSize;
+                    start_i = i;
                 }
-                
                 mp[s[i]]++;
                 if(mp[s[i]]>0){
                     reqCount++;
@@ -32,7 +30,7 @@ public:
             }
             j++;
         }
-
-        return minWinSize==INT_MAX? "": s.substr(start_i, minWinSize);
+        return minWinSize==INT_MAX ? "": s.substr(start_i, minWinSize);
     }
 };
+
