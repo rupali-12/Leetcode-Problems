95db1584a5a442c9faaf49c6bfffa97179226071
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-02-13 12:51:06 +0530

    Time: 364 ms (5.01%), Space: 9.2 MB (34.56%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -12,30 +12,66 @@ public:
         }
         return chkFromLeft;
     }
+     void removeSenateOpt(string &senate, vector<bool>&removed,  char ch, int idx){
+        while(true){
+            if(senate[idx]==ch && removed[idx]==false){
+               removed[idx]=true;
+                break;
+            }
+            idx= (idx+1)%senate.length();
+        }
+        return;
+    }
     string predictPartyVictory(string senate) {
-        // Approach-1: Brute force 
+//         // Approach-1: Brute force 
+//         // count D and R
+//         int count_R = count(begin(senate), end(senate), 'R');
+//         int count_D = count(begin(senate), end(senate), 'D');
+        
+//         int idx=0;
+//        while(count_R>0 && count_D>0){
+//             // if 'R' delete someone 'D' at right 
+//            if(senate[idx]=='R'){
+//             bool checkRemovalFromLeft = removeSenate(senate,'D', (idx+1)%(senate.length()));
+//                count_D--;
+//                if(checkRemovalFromLeft){
+//                    idx--;
+//                }
+//            }
+//            else{
+//             // if 'D' delete someone 'R' at right 
+//                bool checkRemovalFromLeft = removeSenate(senate, 'R', (idx+1)%(senate.length()));
+//                count_R--;
+//                if(checkRemovalFromLeft){
+//                    idx--;
+//                }
+//            }
+//            idx= (idx+1)%senate.length();
+//        }
+//         return count_R==0?"Dire":"Radiant";
+        
+        // *****************************************************************************
+         // Approach-2: Optimized (using vector)
         // count D and R
         int count_R = count(begin(senate), end(senate), 'R');
         int count_D = count(begin(senate), end(senate), 'D');
         
         int idx=0;
+        int n= senate.length();
+        vector<bool>removed(n, false);
        while(count_R>0 && count_D>0){
             // if 'R' delete someone 'D' at right 
-           if(senate[idx]=='R'){
-            bool checkRemovalFromLeft = removeSenate(senate,'D', (idx+1)%(senate.length()));
+          if(removed[idx]==false){
+               if(senate[idx]=='R'){
+           removeSenateOpt(senate, removed, 'D', (idx+1)%(senate.length()));
                count_D--;
-               if(checkRemovalFromLeft){
-                   idx--;
-               }
            }
            else{
             // if 'D' delete someone 'R' at right 
-               bool checkRemovalFromLeft = removeSenate(senate, 'R', (idx+1)%(senate.length()));
+                removeSenateOpt(senate, removed, 'R', (idx+1)%(senate.length()));
                count_R--;
-               if(checkRemovalFromLeft){
-                   idx--;
-               }
            }
+          }
            idx= (idx+1)%senate.length();
        }
         return count_R==0?"Dire":"Radiant";
