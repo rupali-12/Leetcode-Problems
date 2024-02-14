4d9fe27998b84acea6129772e67cc497edcb3e6e
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-02-14 10:41:34 +0530

    Time: 15 ms (65.32%), Space: 24.6 MB (14.96%) - LeetHub

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int n= nums.size();
+        deque<int>dq;
+        for(int i=n-1; i>=0; i--){
+            if(nums[i]%2==0){
+                dq.push_front(nums[i]);
+            }
+            else{
+                dq.push_back(nums[i]);
+            }
+        }
+        
+        int i=0;
+        while(!dq.empty()){
+            int num1= dq.front();
+            int num2= dq.back();
+            dq.pop_front();
+            dq.pop_back();
+            nums[i++]= num1;
+            nums[i++]= num2;
+        }
+        return nums;
+    }
+};
