e58d45ffd2e6e3b7b774f05d5106baebb6e679b9
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-03-05 18:12:02 +0530

    Time: 33 ms (7.93%), Space: 13.9 MB (70.34%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -4,14 +4,14 @@ public:
         int len = s.length();
         int i=0, j= len-1;
         int ans=0;
-        while(i<j && s[i]==s[j]){
-                while(s[j]==s[j-1] && j>i+1){
+
+        for(; i<j && s[i]==s[j]; i++, j--){
+             while(s[j]==s[j-1] && j>i+1){
                         j--;
                 }
                  while(s[i]==s[i+1] && i<j-1){
                         i++;
                 }
-            i++; j--;
         }
           ans = j-i+1;
         return ans;
