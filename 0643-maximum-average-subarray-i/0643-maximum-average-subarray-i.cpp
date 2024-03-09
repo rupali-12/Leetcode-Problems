4faab72cad31a7c6f5c5d604cc5f47d83a515584
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-03-09 12:09:38 +0530

    Time: 133 ms (40.74%), Space: 112.4 MB (63.77%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -7,10 +7,6 @@ public:
         for(int i=0; i<nums.size(); i++){
             sum+= nums[i];
             cnt++;
-            // if(sum<0){
-            //     sum=0;
-            //     cnt=0;
-            // }
             if(cnt==k){
                 mxSum= max(mxSum, sum);
             }
