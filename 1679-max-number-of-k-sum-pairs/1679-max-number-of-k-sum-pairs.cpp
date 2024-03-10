083d20eeb6785f78921e55a488e150fc007f0c9f
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-03-10 11:59:42 +0530

    Time: 128 ms (22.19%), Space: 71.5 MB (19.75%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-       int i=0, j= nums.size()-1;
-        int count=0;
-        sort(nums.begin(), nums.end());
-        while(i<j){
-            int sum= nums[i]+nums[j];
-            if(sum==k){
-                count++; 
-                i++;
-                j--;
+       //  // sorting 
+       // int i=0, j= nums.size()-1;
+       //  int count=0;
+       //  sort(nums.begin(), nums.end());
+       //  while(i<j){
+       //      int sum= nums[i]+nums[j];
+       //      if(sum==k){
+       //          count++; 
+       //          i++;
+       //          j--;
+       //      }
+       //      else if(sum<k){
+       //          i++;
+       //      }
+       //      else{
+       //          j--;
+       //      }
+       //  }
+       //  return count;
+        
+        // Hashmap
+         int total=0;
+       unordered_map<int,int>mp;
+        for(auto num: nums){
+            if(mp.count(k-num)){
+                total++;
+                mp[k-num]--;
+                if(mp[k-num]==0){
+                    mp.erase(k-num);
+                }
             }
-            else if(sum<k){
-                i++;
-            }
-            else{
-                j--;
+            else{   // if num not present in map then insert it 
+                mp[num]++;
             }
         }
-        return count;
+        return total;
     }
 };
