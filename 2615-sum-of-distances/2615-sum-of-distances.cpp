4560fc336772c6203686ab2ad3450a6d72e4ee40
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-03-25 23:49:01 +0530

    Time: 198 ms (42.09%), Space: 121.5 MB (50.36%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        map<int, vector<int>>mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]].push_back(i);
+            
+        }
+        
+        vector<long long>ans(nums.size(), 0);
+       for(auto m: mp){
+           vector<int>v = m.second;
+           
+           // calculate total sum
+           long long sum=0;
+           for(int a:v) sum+= (long long)a;
+           
+           // calculate prefix 
+           long long currsum=0, restsum=0;
+           for(int i=0; i<v.size(); i++){
+               restsum = sum - currsum - v[i];
+               long long sumleft= (v[i]*(long long)(i)) - currsum;
+               long long sumright = restsum- (v[i]*(long long)(v.size()-1-i));
+               
+               ans[v[i]] =  sumleft + sumright;  // actually v[i] stores index where the value occured   
+               currsum+= v[i];
+           }
+       }
+        return ans;
+    }
+};
