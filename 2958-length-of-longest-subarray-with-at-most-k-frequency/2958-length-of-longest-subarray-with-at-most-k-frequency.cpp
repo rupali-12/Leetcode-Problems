9b98d9c76066f8a89cc9372bf32dbafb3c2e6930
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-03-28 13:55:37 +0530

    Time: 205 ms (71.40%), Space: 148.4 MB (86.57%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -5,18 +5,40 @@ public:
         unordered_map<int, int>mp;
         int i=0, j=0, result=0;
         
-        // Approach-1: T.C-> O(n2)
+//         // Approach-1: T.C-> O(n2)
+//         while(j<n){
+//             mp[nums[j]]++;
+            
+//             while(i<j && mp[nums[j]]>k){
+//                 mp[nums[i]]--;
+//                 i++;
+//             }
+            
+//             result= max(result, (j-i+1));
+//             j++;
+//         }
+//         return result;
+        
+        // Approach-2: T.C-> O(n)
+        int culprit=0;
         while(j<n){
             mp[nums[j]]++;
+            if(mp[nums[j]]==k+1){
+                culprit++;
+            }
             
-            while(i<j && mp[nums[j]]>k){
+            if(culprit>0){
                 mp[nums[i]]--;
+                if(mp[nums[i]]==k){
+                    culprit--;
+                }
                 i++;
             }
             
             result= max(result, (j-i+1));
             j++;
         }
+        
         return result;
     }
 };
