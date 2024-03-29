e431601493570973d104b1fb0c026edc1dcea3c8
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-03-29 15:38:48 +0530

    Time: 127 ms (71.71%), Space: 123.6 MB (43.60%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -9,23 +9,40 @@ public:
             max_element = max(max_element, nums[i]);
         }
         
-        int i=0, j=0, count=0;
+        
+//         // Approach-1: Sliding Window 
+//         int i=0, j=0, count=0;
+//         long long ans=0;
+//         while(j<n){
+//             if(nums[j]==max_element){
+//                 count++;
+//             }
+            
+//            while(count>=k){
+//                ans+= (n-j);
+//                if(nums[i]==max_element){
+//                    count--;
+//                }
+//                i++;
+//            }
+            
+            
+//             j++;
+//         }
+//         return ans;
+        
+        // Approach-2: With Space Complexity
         long long ans=0;
-        while(j<n){
-            if(nums[j]==max_element){
-                count++;
+        vector<int>indices;
+        for(int i=0; i<n; i++){
+            if(nums[i]==max_element){
+                indices.push_back(i);
             }
-            
-           while(count>=k){
-               ans+= (n-j);
-               if(nums[i]==max_element){
-                   count--;
-               }
-               i++;
-           }
-            
-            
-            j++;
+            int size= indices.size();
+            if(size>=k){
+                int last_i = indices[size-k];
+                ans+= last_i +1;
+            }
         }
         return ans;
     }
