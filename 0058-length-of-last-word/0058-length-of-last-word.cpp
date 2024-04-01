95a92c806e3e73c8ffdebde309f010ea4577d72d
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-04-01 11:29:06 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (99.30%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-      int length=0;
-        bool counting= false;
+        // Aproach-1:
+        int len=0;
         for(int i= s.length()-1; i>=0; i--){
-            if(s[i]!= ' '){
-                counting = true;
-                length++;
-            }
-            else if(counting){
-                break;
-            }
+           if(len!=0 && s[i]==' '){
+               break;
+           }   
+            else if(s[i]!=' ')
+                len++;
         }
-        return length;
+        return len;
+        
+      //   Approach-2: 
+      // int length=0;
+      //   bool counting= false;
+      //   for(int i= s.length()-1; i>=0; i--){
+      //       if(s[i]!= ' '){
+      //           counting = true;
+      //           length++;
+      //       }
+      //       else if(counting){
+      //           break;
+      //       }
+      //   }
+      //   return length;
     }
 };
