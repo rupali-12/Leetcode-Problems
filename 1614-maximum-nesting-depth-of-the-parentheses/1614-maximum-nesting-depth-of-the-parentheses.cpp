8387a69b8176c3d2eda83f5268bd16962d0ad83b
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-04-04 19:37:46 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (22.85%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char>st;
-        int max_depth= 0;
+        
+        // // Approach-1: With space complexity
+        // stack<char>st;
+        // int max_depth= 0;
+        // for(int i=0; i<s.length(); i++){
+        //     if(s[i]=='('){
+        //         st.push(s[i]);
+        //     }
+        //     else if(s[i]==')'){
+        //         st.pop();
+        //     }
+        //     max_depth= max(max_depth, (int)st.size());
+        // }
+        // return max_depth;
+        
+        // Approach-2: Without space complexity
+        int max_depth= 0, depth=0;
         for(int i=0; i<s.length(); i++){
             if(s[i]=='('){
-                st.push(s[i]);
+                depth++;
             }
             else if(s[i]==')'){
-                st.pop();
+               depth--;
             }
-            max_depth= max(max_depth, (int)st.size());
+            max_depth= max(max_depth, depth);
         }
         return max_depth;
     }
