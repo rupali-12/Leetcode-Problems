b2ad29549ca01d33d7e1ebda72cd6421e9bf471c
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-04-05 15:33:43 +0530

    Time: 4 ms (42.41%), Space: 7.6 MB (89.71%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     string makeGood(string s) {
-        int n= s.length();
-        stack<char>st;
-        for(int i=0; i<s.length(); i++){
-            if(!st.empty() && ((st.top()-32)==s[i] || (st.top()+32)==s[i])){
-                st.pop();
+        
+        // // Approach-1: With stack 
+        // int n= s.length();
+        // stack<char>st;
+        // for(int i=0; i<s.length(); i++){
+        //     if(!st.empty() && ((st.top()-32)==s[i] || (st.top()+32)==s[i])){
+        //         st.pop();
+        //     }
+        //     else{
+        //       st.push(s[i]);    
+        //     }
+        // }
+        // string ans="";
+        // while(!st.empty()){
+        //     ans+= st.top();
+        //     st.pop();
+        // }
+        // reverse(ans.begin(), ans.end());
+        // return ans;
+        
+        // Approach-2: Without stack
+        string ans="";
+        for(char &ch: s){
+            if(!ans.empty() && (ans.back()-32==ch || ans.back()+32 == ch)){
+                ans.pop_back();
             }
             else{
-              st.push(s[i]);    
+                ans.push_back(ch);
             }
         }
-        string ans="";
-        while(!st.empty()){
-            ans+= st.top();
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
