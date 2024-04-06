c505c91b503fbee0fdb34999008725d87c89aed5
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-04-06 18:19:20 +0530

    Time: 19 ms (75.33%), Space: 13.5 MB (33.60%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -33,41 +33,78 @@ public:
 //         }
 //         return ans;
         
-         // Approach-2: Without Using Stack 
+      // *************************************************************************    
+//          // Approach-2: Without Using Stack 
+//         int open=0, close=0;
+//         string ans="";
+//         // forward traversing to remove unbalanced closed parentheses 
+//         for(int i=0; i<s.length(); i++){
+//            if(s[i]>='a' && s[i]<='z'){
+//                ans+= s[i];
+//            }    
+//             else if(s[i]=='('){
+//                 open++;
+//                 ans+= s[i];
+//             }
+//             else if(open>0){
+//                 open--;
+//                 ans+= s[i];
+//             }
+//         }
+        
+//         // backward traversing to remove unbalanced open parentheses 
+//         string final_ans="";
+//           for(int i=ans.length()-1; i>=0; i--){
+//            if(ans[i]>='a' && ans[i]<='z'){
+//                final_ans+= ans[i];
+//            }    
+//             else if(ans[i]==')'){
+//                 close++;
+//                 final_ans+= ans[i];
+//             }
+//             else if(close>0){
+//                 close--;
+//                 final_ans+= ans[i];
+//             }
+//         }
+//         reverse(final_ans.begin(), final_ans.end());
+//         return final_ans;
+        
+ // *************************************************************************
+        // Approach-3: Optimized Approach 
+           
         int n= s.length();
-        int open=0, close=0;
-        string ans="";
-        // forward traversing to remove unbalanced closed parentheses 
+        string temp="";
+         int ignore_open=0;
+        
         for(int i=0; i<n; i++){
-           if(s[i]>='a' && s[i]<='z'){
-               ans+= s[i];
-           }    
-            else if(s[i]=='('){
-                open++;
-                ans+= s[i];
+             if(s[i]>='a' && s[i]<='z'){
+                temp+= s[i];
             }
-            else if(open>0){
-                open--;
-                ans+= s[i];
+            else if(s[i]=='('){
+                ignore_open++;
+                temp+= s[i];
+            }
+            else if(s[i]==')'){
+                if(ignore_open>0){
+                    ignore_open--;
+                    temp+= s[i];
+                }
+            }
+        }
+
+        cout<<temp<<endl;
+        string ans="";
+        for(int i=temp.length()-1; i>=0; i--){
+          if(temp[i]=='(' && ignore_open>0){
+                ignore_open--;
+            }
+            else{
+                ans+= temp[i];
             }
         }
         
-        // backward traversing to remove unbalanced open parentheses 
-        string final_ans="";
-          for(int i=ans.length()-1; i>=0; i--){
-           if(ans[i]>='a' && ans[i]<='z'){
-               final_ans+= ans[i];
-           }    
-            else if(ans[i]==')'){
-                close++;
-                final_ans+= ans[i];
-            }
-            else if(close>0){
-                close--;
-                final_ans+= ans[i];
-            }
-        }
-        reverse(final_ans.begin(), final_ans.end());
-        return final_ans;
+        reverse(ans.begin(), ans.end());
+       return ans;
     }
 };
