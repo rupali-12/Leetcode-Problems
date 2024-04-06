ace4d9c30dafebf1f9ee624839562fb3d3d2bf15
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-04-06 16:31:49 +0530

    Time: 19 ms (75.33%), Space: 13.6 MB (26.27%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,35 +2,72 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         
-        // Approach-1: Using Stack 
+//         // Approach-1: Using Stack 
+//         int n= s.length();
+//         stack<int>st;
+//         unordered_set<int>removed_idx;
+        
+//         for(int i=0; i<n; i++){
+//            if(s[i]=='('){
+//                st.push(i);
+//            }    
+//             else if(s[i]==')'){
+//                 if(st.empty()){
+//                     removed_idx.insert(i);
+//                 }
+//                 else{
+//                     st.pop();
+//                 }
+//             }
+//         }
+        
+//         while(!st.empty()){
+//             removed_idx.insert(st.top());
+//             st.pop();
+//         }
+//        string ans="";
+//         for(int i=0; i<n; i++){
+//             if(removed_idx.find(i)==removed_idx.end()){
+//                 ans+= s[i];
+//             }
+//         }
+//         return ans;
+        
+         // Approach-2: Without Using Stack 
         int n= s.length();
-        stack<int>st;
-        unordered_set<int>removed_idx;
-        
+        int open=0, close=0;
+        string ans="";
+        // forward traversing to remove unbalanced closed parentheses 
         for(int i=0; i<n; i++){
-           if(s[i]=='('){
-               st.push(i);
+           if(s[i]>='a' && s[i]<='z'){
+               ans+= s[i];
            }    
-            else if(s[i]==')'){
-                if(st.empty()){
-                    removed_idx.insert(i);
-                }
-                else{
-                    st.pop();
-                }
+            else if(s[i]=='('){
+                open++;
+                ans+= s[i];
             }
-        }
-        
-        while(!st.empty()){
-            removed_idx.insert(st.top());
-            st.pop();
-        }
-       string ans="";
-        for(int i=0; i<n; i++){
-            if(removed_idx.find(i)==removed_idx.end()){
+            else if(open>0){
+                open--;
                 ans+= s[i];
             }
         }
-        return ans;
+        
+        // backward traversing to remove unbalanced open parentheses 
+        string final_ans="";
+          for(int i=ans.length()-1; i>=0; i--){
+           if(ans[i]>='a' && ans[i]<='z'){
+               final_ans+= ans[i];
+           }    
+            else if(ans[i]==')'){
+                close++;
+                final_ans+= ans[i];
+            }
+            else if(close>0){
+                close--;
+                final_ans+= ans[i];
+            }
+        }
+        reverse(final_ans.begin(), final_ans.end());
+        return final_ans;
     }
 };
