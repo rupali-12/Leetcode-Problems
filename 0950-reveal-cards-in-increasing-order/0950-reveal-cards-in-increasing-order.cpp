1f1640319b5059da791763061e73627c6919d27d
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-04-10 11:15:13 +0530

    Time: 7 ms (18.53%), Space: 10.8 MB (91.52%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -3,21 +3,40 @@ public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         int n= deck.size();
         vector<int>ans(n, 0);
-        
         sort(deck.begin(), deck.end());
         int i=0; // deck index
-        int j=0;  // ans index
-        bool skip= false;  // initially not skipped
+        
+//         // Approach-1: 
+//         int j=0;  // ans index
+//         bool skip= false;  // initially not skipped
+        
+//         while(i<n){
+//             if(ans[j]==0){
+//                 if(skip==false){
+//                     ans[j]= deck[i++];
+//                 }
+//                 skip=!skip;
+//             }
+            
+//             j= (j+1)%n;
+//         }
+//         return ans;
+        
+        // Approach-2: Using Queue
+        deque<int>dq;
+        for(int i=0; i<n; i++){
+            dq.push_back(i);
+        }
         
         while(i<n){
-            if(ans[j]==0){
-                if(skip==false){
-                    ans[j]= deck[i++];
-                }
-                skip=!skip;
-            }
+            int idx= dq.front();
+            dq.pop_front();
+            ans[idx] = deck[i++];
             
-            j= (j+1)%n;
+            // insert next element index at back of queue
+            int nextidx= dq.front();
+            dq.pop_front();
+            dq.push_back(nextidx);
         }
         return ans;
     }
