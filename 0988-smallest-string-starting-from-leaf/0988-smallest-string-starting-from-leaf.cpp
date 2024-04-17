b3b2ebc13f578d6dadfff46b4b8234345bb458c6
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-04-17 10:55:41 +0530

    Time: 12 ms (33.91%), Space: 20.3 MB (67.10%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -15,22 +15,49 @@ class Solution {
 public:
     string res="";
     
-    void dfs(TreeNode* root, string curr){
-       if(root==NULL) return;
+    // void dfs(TreeNode* root, string curr){
+//        if(root==NULL) return;
         
-        curr= char(root->val + 'a') + curr;
-        if(!root->left && !root->right){
-            if(res=="" || res>curr) res= curr;
-            return;
-        }
+//         curr= char(root->val + 'a') + curr;
+//         if(!root->left && !root->right){
+//             if(res=="" || res>curr) res= curr;
+//             return;
+//         }
        
-        dfs(root->left, curr);
-        dfs(root->right, curr);
-        
-    }
-    string smallestFromLeaf(TreeNode* root) {
+//         dfs(root->left, curr);
+//         dfs(root->right, curr);    
+//     }
     
-        dfs(root, "");
+    string smallestFromLeaf(TreeNode* root) {
+
+//         // Approach-1: DFS
+//         dfs(root, "");
+//         return res;
+        
+        // Approach-2: BFS
+        queue<pair<TreeNode*, string>>q;
+        q.push({root, string(1, char(root->val + 'a'))});
+        
+        while(!q.empty()){
+            auto p= q.front();
+            q.pop();
+            TreeNode* node= p.first;
+            string curr= p.second;
+            
+            if(!node->left && !node->right){
+                if(res=="" || res>curr){
+                res= curr;
+            }
+            }
+            
+            if(node->left){
+                q.push({node->left, (char(node->left->val +'a') + curr)});
+            }
+             if(node->right){
+                q.push({node->right, (char(node->right->val +'a') + curr)});
+            }
+        }
+        
         return res;
         
     }
