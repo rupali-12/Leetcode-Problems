571b8a8660a7d919bc38e1bdf0ca08f40d535994
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-04-20 18:29:41 +0530

    Time: 404 ms (21.46%), Space: 196.4 MB (5.09%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -20,15 +20,50 @@ public:
         }
     }
     
+    void bfs(vector<vector<int>>& land, int i, int j, int &r2, int &c2){
+        queue<pair<int, int>>q;
+        q.push({i, j});
+        land[i][j]=-1;
+        
+        while(!q.empty()){
+            auto front= q.front();
+            q.pop();
+            
+             r2= max(front.first, r2);
+             c2= max(front.second, c2);
+            for(auto dir:directions){
+                int r= front.first + dir[0];
+                int c= front.second + dir[1];
+                
+                if(r>=0 && c>=0 && r<m && c<n && land[r][c]==1){
+                    q.push({r, c});
+                    land[r][c]=-1;
+                }
+            }
+        }
+        return;
+    }
+    
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         m= land.size(), n= land[0].size();
         
-        // Approach-1: DFS 
-        for(int i=0; i<m; i++){
+        // // Approach-1: DFS 
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(land[i][j]==1){
+        //             int r1=i, c1=j, r2=-1, c2=-1;
+        //             dfs(land, i, j,  r2, c2);
+        //             ans.push_back({r1, c1, r2, c2});
+        //         }
+        //     }
+        // }
+        
+        // Approach-2: BFS 
+         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(land[i][j]==1){
                     int r1=i, c1=j, r2=-1, c2=-1;
-                    dfs(land, i, j,  r2, c2);
+                    bfs(land, i, j,  r2, c2);
                     ans.push_back({r1, c1, r2, c2});
                 }
             }
