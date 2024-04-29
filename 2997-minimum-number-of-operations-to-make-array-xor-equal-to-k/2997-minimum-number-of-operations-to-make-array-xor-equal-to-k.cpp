8e0bb49a46a301f6d880831a810104a7161f0b23
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-04-29 18:29:08 +0530

    Time: 105 ms (73.47%), Space: 91.4 MB (54.80%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -6,20 +6,22 @@ public:
             totalxor = totalxor^nums[i];
         }
         int ans=0;
-        // diff in bit is equivalent to different bits in (totalxor^k)
-        totalxor= totalxor^k;
-        cout<<totalxor<<endl;
-        // // way 1 to count different bit 
-        // while(totalxor!=0){
-        //     int bit_in_txor = totalxor>>1;
-        //     if(bit_in_txor != 1){  // 1 means different bit in totalxor and k 
-        //         ans++;
-        //     }
-        //     totalxor>>=1;
-        // }
         
-        // way 2 build in function
-        ans= __builtin_popcount(totalxor);
+        // way 1 to count different bit 
+        while(totalxor!=0 || k!=0){
+            int bit_in_txor = totalxor&1;
+            int bit_in_k= k&1;
+            if(bit_in_txor != bit_in_k){  // 1 means different bit in totalxor and k 
+                ans++;
+            }
+            totalxor>>=1;
+            k >>=1;
+        }
+        
+        // // way 2 build in function
+        // // diff in bit is equivalent to different bits in (totalxor^k)
+        // totalxor= totalxor^k;
+        // ans= __builtin_popcount(totalxor);
         return ans;
     }
 };
