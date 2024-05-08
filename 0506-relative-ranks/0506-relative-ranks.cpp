5e5ecc6c3926380438995ad86636007db8cb3e71
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-05-08 10:04:08 +0530

    Time: 9 ms (55.97%), Space: 13.8 MB (70.05%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        map<int, int>mp;
+        
+//         // Approach-1 Brute force 
+//         map<int, int>mp;
+//         for(int i=0; i<score.size(); i++){
+//             mp[score[i]]= i;
+//         }
+        
+//         sort(score.begin(), score.end(), greater<int>());
+//        vector<string>ans(score.size());
+        
+//         for(int i=0; i<score.size(); i++){
+//             int idx= mp[score[i]];
+//             if(i==0) ans[idx] = "Gold Medal";
+//             else if(i==1) ans[idx] = "Silver Medal";
+//             else if(i==2) ans[idx] = "Bronze Medal";
+//             else ans[idx] = to_string(i+1);
+//         }
+//         return ans;
+        
+        // Approach-2: Max heap
+        vector<string>ans(score.size());
+        priority_queue<pair<int, int>>pq;
         for(int i=0; i<score.size(); i++){
-            mp[score[i]]= i;
+            pair<int, int>p ={score[i], i};
+            pq.push(p);
         }
-        
-        sort(score.begin(), score.end(), greater<int>());
-       vector<string>ans(score.size());
-        
-        for(int i=0; i<score.size(); i++){
-            int idx= mp[score[i]];
+        int i=0;
+        while(!pq.empty()){
+            auto top= pq.top();
+            int val= top.first;
+            int idx= top.second;
+            pq.pop();
+            
             if(i==0) ans[idx] = "Gold Medal";
-            else if(i==1) ans[idx] = "Silver Medal";
+            else if(i==1) ans[idx] =  "Silver Medal";
             else if(i==2) ans[idx] = "Bronze Medal";
-            else ans[idx] = to_string(i+1);
+            else ans[idx] =  to_string(i+1);
+            i++;
         }
         return ans;
     }
