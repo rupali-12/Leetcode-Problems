1a86a0323cdf2e7035a46b487b8eef92d18aa404
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-05-13 10:31:28 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (75.07%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -1,65 +1,54 @@
 class Solution {
 public:
-     void flipRow(vector<vector<int>>& grid,int r){
-        int c= grid[0].size();
-        for(int i=0; i<c; i++){
-            grid[r][i]^=1;
+    int m, n;
+    void flipRow(vector<vector<int>>& grid, int row){
+        for(int i=0; i<n; i++){
+            grid[row][i]^=1;
         }
+        return;
     }
-      void flipCol(vector<vector<int>>& grid,int c){
-        int n= grid.size();
-        for(int r=0; r<n; r++){
-            grid[r][c]^=1;
+    void flipCol(vector<vector<int>>& grid, int col){
+        for(int i=0; i<m; i++){
+            grid[i][col]^=1;
         }
+        return;
     }
     int matrixScore(vector<vector<int>>& grid) {
-        int m= grid.size(), n= grid[0].size();
+         m= grid.size();
+        n= grid[0].size();
         
-        // check for Row, flip row when grid[i][0]=0(make msb as 1 if not)
+        // For Row, check if grid[i][0]=0 then flip Row 
         for(int i=0; i<m; i++){
             if(grid[i][0]==0){
-                flipRow(grid, i);
+               flipRow(grid, i); 
             }
-        }
+        } 
         
-        // for column if number of ones is less than zeros then flip column 
-        for(int c=0; c<n; c++){
+        // For Column if number of 1's less than 0's then flipColumn
+        for(int i=0; i<n; i++){
             int count1=0;
             for(int r=0; r<m; r++){
-                if(grid[r][c]==1) count1++;
+                if(grid[r][i]==1) count1++;
             }
-            if(count1< (m-count1)){
-                flipCol(grid, c);
+            
+            if(count1 < (m-count1)){
+                flipCol(grid, i);
             }
         }
+        
         int ans=0;
-        
-        // // Way-1: pow function
-        // for(int i=0;i<m; i++){
-        //     int num=0, power=0;
-        //     for(int j=n-1; j>=0; j--){
-        //        num+= pow(2, power) * grid[i][j];
-        //         power++;
-        //     }
-        //     ans+= num;
-        // }
-        
-        // Way-2: bit manipulation
-        for(int i=0;i<m; i++){
-            int num=0;
-            // for(int j=n-1; j>=0; j--){
-            //    if(grid[i][j]){
-            //        num= num | grid[i][j]<<(n-j-1);
-            //    }
-            // Way-2
+        // way-1: using power function 
+        for(int i=0; i<m; i++){
+            int num=0, power=0;
             for(int j=0; j<n; j++){
-               if(grid[i][n-1-j]){
-                   num= num | grid[i][n-1-j]<<j;
-               }
+                num+= pow(2, power)*grid[i][n-1-j];
+                power++;
             }
             ans+= num;
         }
-        return ans;
         
+        return ans;
     }
 };
+
+
