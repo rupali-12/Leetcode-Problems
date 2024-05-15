8ca595a100a6e081b825535ef8d0fdb12ce707e6
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-05-15 14:56:12 +0530

    Time: 1960 ms (7.66%), Space: 499.4 MB (5.11%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -1,36 +1,36 @@
-// class Solution {
-// public:
-//     int n;
-//       vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+class Solution {
+public:
+    int n;
+      vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
     
-// //     void multisourceBFS( vector<vector<int>>&disNearestThief,  vector<vector<int>>&grid,  vector<vector<bool>>&vis, queue<pair<int, int>>que){
-// //         int level=0;
-// //         while(!que.empty()){
-// //             int size= que.size();
-// //             while(size--){
-// //                 int curr_i = que.front().first;
-// //                 int curr_j = que.front().second;
-// //             que.pop();
+//     void multisourceBFS( vector<vector<int>>&disNearestThief,  vector<vector<int>>&grid,  vector<vector<bool>>&vis, queue<pair<int, int>>que){
+//         int level=0;
+//         while(!que.empty()){
+//             int size= que.size();
+//             while(size--){
+//                 int curr_i = que.front().first;
+//                 int curr_j = que.front().second;
+//             que.pop();
                
-// //                 disNearestThief[curr_i][curr_j] = level;
+//                 disNearestThief[curr_i][curr_j] = level;
                 
-// //                 //push neipghbour in queue
-// //                 for(auto dir: direction){
-// //                     int new_i = curr_i + dir[0];
-// //                     int new_j = curr_j + dir[1];
+//                 //push neipghbour in queue
+//                 for(auto dir: direction){
+//                     int new_i = curr_i + dir[0];
+//                     int new_j = curr_j + dir[1];
                     
-// //                     if(new_i<0 || new_i >=n || new_j<0 || new_j >=n || vis[new_i][new_j]){
-// //                         continue;
-// //                     }
+//                     if(new_i<0 || new_i >=n || new_j<0 || new_j >=n || vis[new_i][new_j]){
+//                         continue;
+//                     }
                     
-// //                     que.push({new_i, new_j});
-// //                     vis[new_i][new_j] =true;
-// //                 }
-// //             }
-// //             level++;
-// //         }
-// //         return;
-//     // }
+//                     que.push({new_i, new_j});
+//                     vis[new_i][new_j] =true;
+//                 }
+//             }
+//             level++;
+//         }
+//         return;
+    // }
     
     
 //     bool check(vector<vector<int>>&disNearestThief, int safeFactor){
@@ -73,79 +73,8 @@
 //         return false;
 //     }
     
-//     int maximumSafenessFactor(vector<vector<int>>& grid) {
-//         n= grid.size();
-        
-//         // step-1: Precalculation of disNearestThief for each cell
-//         vector<vector<int>>disNearestThief(n, vector<int>(n, -1));
-//         vector<vector<bool>>vis(n, vector<bool>(n, false));
-//         queue<pair<int, int>>que;
-        
-//         // Push all theif position in queue
-//         for(int i=0; i<n; i++){
-//             for(int j=0; j<n; j++){
-//                 if(grid[i][j]==1){
-//                     que.push({i, j});
-//                     vis[i][j]=true;
-//                 }        
-//             }
-//         }
-        
-//         // multisource BFS
-//         // multisourceBFS(disNearestThief, grid, vis, que);
-//          int level=0;
-//         while(!que.empty()){
-//             int size= que.size();
-//             while(size--){
-//                 int curr_i = que.front().first;
-//                 int curr_j = que.front().second;
-//             que.pop();
-               
-//                 disNearestThief[curr_i][curr_j] = level;
-                
-//                 //push neipghbour in queue
-//                 for(auto dir: direction){
-//                     int new_i = curr_i + dir[0];
-//                     int new_j = curr_j + dir[1];
-                    
-//                     if(new_i<0 || new_i >=n || new_j<0 || new_j >=n || vis[new_i][new_j]){
-//                         continue;
-//                     }
-                    
-//                     que.push({new_i, new_j});
-//                     vis[new_i][new_j] =true;
-//                 }
-//             }
-//             level++;
-//         }
-        
-//         // Step-2: Apply Binary Search on Safeness factor 
-//         int l=0, r= 400;    // As 1 <= grid.length == n <= 400
-//         int ans=0;
-//        while(l<=r){
-//             int mid = l + ((r-l)/2);
-//            //Now check if ther is any path existing in which every cell value >= mid
-//            if(check(disNearestThief, mid)){
-//                ans= mid;
-//                l= mid+1;
-//            }
-//            else{
-//                r= mid-1;
-//            }
-//        }
-//         return ans; 
-//     }
-// };
     
-    
-   
-class Solution {
-public:
-    int n;
-    
-    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-
-    bool check(vector<vector<int>>& distNearestThief, int sf) {
+     bool check(vector<vector<int>>& disNearestThief, int sf) {
         queue<pair<int, int>> que;
 
         vector<vector<bool>> visited(n, vector<bool>(n, false));
@@ -153,7 +82,7 @@ public:
         que.push({0, 0});
         visited[0][0] = true;
 
-        if(distNearestThief[0][0] < sf)
+        if(disNearestThief[0][0] < sf)
             return false;
 
         while(!que.empty()) {
@@ -166,12 +95,12 @@ public:
                 return true;
             }
 
-            for(vector<int>& dir : directions) {
+            for(vector<int>& dir : direction) {
                 int new_i = curr_i + dir[0];
                 int new_j = curr_j + dir[1];
 
                 if(new_i >= 0 && new_i < n && new_j >= 0 && new_j < n && visited[new_i][new_j] != true) {
-                    if(distNearestThief[new_i][new_j] < sf) {
+                    if(disNearestThief[new_i][new_j] < sf) {
                         continue; //reject this cell
                     }
                     que.push({new_i, new_j});
@@ -185,67 +114,135 @@ public:
         
     }
 
+    
     int maximumSafenessFactor(vector<vector<int>>& grid) {
-        n = grid.size();
-
-        //Step-1 Precalculation of distNearestThief - for each cell
-        vector<vector<int>> distNearestThief(n, vector<int>(n, -1));
-        queue<pair<int, int>> que;
-        vector<vector<bool>> visited(n, vector<bool>(n, false));
-
-        //push all cells in queue where theives are present
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1) {
+        n= grid.size();
+        
+        // step-1: Precalculation of disNearestThief for each cell
+        vector<vector<int>>disNearestThief(n, vector<int>(n, -1));
+        vector<vector<bool>>vis(n, vector<bool>(n, false));
+        queue<pair<int, int>>que;
+        
+        // Push all theif position in queue
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1){
                     que.push({i, j});
-                    visited[i][j] = true;
-                }
+                    vis[i][j]=true;
+                }        
             }
         }
-
-        int level = 0;
-        while(!que.empty()) {
-            int size = que.size();
-
-            while(size--) {
+        
+        // multisource BFS
+        // multisourceBFS(disNearestThief, grid, vis, que);
+         int level=0;
+        while(!que.empty()){
+            int size= que.size();
+            while(size--){
                 int curr_i = que.front().first;
                 int curr_j = que.front().second;
-                que.pop();
-                distNearestThief[curr_i][curr_j] = level;
-                for(vector<int>& dir : directions) {
+            que.pop();
+               
+                disNearestThief[curr_i][curr_j] = level;
+                
+                //push neipghbour in queue
+                for(auto dir: direction){
                     int new_i = curr_i + dir[0];
                     int new_j = curr_j + dir[1];
-
-                    if(new_i < 0 || new_i >= n || new_j < 0 || new_j >= n || visited[new_i][new_j]) {
+                    
+                    if(new_i<0 || new_i >=n || new_j<0 || new_j >=n || vis[new_i][new_j]){
                         continue;
                     }
-
+                    
                     que.push({new_i, new_j});
-                    visited[new_i][new_j] = true;
-
+                    vis[new_i][new_j] =true;
                 }
-
             }
             level++;
         }
-
-        //Step-2 Apply binary search on SF
-        int l = 0;
-        int r = 400;
-        int result = 0;
-
-        while(l <= r) {
-            int mid_sf = l + (r-l)/2;
-
-            if(check(distNearestThief, mid_sf)) {
-                result = mid_sf;
-                l = mid_sf+1;
-            } else {
-                r = mid_sf-1;
-            }
-        }
-
-        return result;
         
+        // Step-2: Apply Binary Search on Safeness factor 
+        int l=0, r= 400;    // As 1 <= grid.length == n <= 400
+        int ans=0;
+       while(l<=r){
+            int mid = l + ((r-l)/2);
+           //Now check if ther is any path existing in which every cell value >= mid
+           if(check(disNearestThief, mid)){
+               ans= mid;
+               l= mid+1;
+           }
+           else{
+               r= mid-1;
+           }
+       }
+        return ans; 
     }
 };
+    
+    
+   
+
+   
+//     int maximumSafenessFactor(vector<vector<int>>& grid) {
+//         n = grid.size();
+
+//         //Step-1 Precalculation of distNearestThief - for each cell
+//         vector<vector<int>> distNearestThief(n, vector<int>(n, -1));
+//         queue<pair<int, int>> que;
+//         vector<vector<bool>> visited(n, vector<bool>(n, false));
+
+//         //push all cells in queue where theives are present
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(grid[i][j] == 1) {
+//                     que.push({i, j});
+//                     visited[i][j] = true;
+//                 }
+//             }
+//         }
+
+//         int level = 0;
+//         while(!que.empty()) {
+//             int size = que.size();
+
+//             while(size--) {
+//                 int curr_i = que.front().first;
+//                 int curr_j = que.front().second;
+//                 que.pop();
+//                 distNearestThief[curr_i][curr_j] = level;
+//                 for(vector<int>& dir : directions) {
+//                     int new_i = curr_i + dir[0];
+//                     int new_j = curr_j + dir[1];
+
+//                     if(new_i < 0 || new_i >= n || new_j < 0 || new_j >= n || visited[new_i][new_j]) {
+//                         continue;
+//                     }
+
+//                     que.push({new_i, new_j});
+//                     visited[new_i][new_j] = true;
+
+//                 }
+
+//             }
+//             level++;
+//         }
+
+//         //Step-2 Apply binary search on SF
+//         int l = 0;
+//         int r = 400;
+//         int result = 0;
+
+//         while(l <= r) {
+//             int mid_sf = l + (r-l)/2;
+
+//             if(check(distNearestThief, mid_sf)) {
+//                 result = mid_sf;
+//                 l = mid_sf+1;
+//             } else {
+//                 r = mid_sf-1;
+//             }
+//         }
+
+//         return result;
+        
+//     }
