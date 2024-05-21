e67c0bc13c70f18fd674628532aaf7f5cb9b03f4
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-05-21 16:24:54 +0530

    Time: 0 ms (100.00%), Space: 14.9 MB (16.02%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void solve(vector<int>nums, vector<int>output, int index, vector<vector<int>>&ans){
-        if(index>nums.size()-1){
-            ans.push_back(output);
+    void solve(int i, vector<int>nums, vector<int>temp, vector<vector<int>>&ans){
+        if(i==nums.size()){
+            ans.push_back(temp);
             return;
         }
         
-        // case of exclusion 
-      solve(nums, output, index+1, ans);
-        
-        // case of inclusion 
-        output.push_back(nums[index]);
-        solve(nums, output, index+1, ans);
-        
+        temp.push_back(nums[i]);
+        solve(i+1, nums, temp, ans);
+        temp.pop_back();
+        solve(i+1, nums, temp, ans);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>output;
-        int index=0;
-        solve(nums, output, index, ans);
+        vector<int>temp;
+        solve(0, nums, temp, ans);
         return ans;
     }
 };
