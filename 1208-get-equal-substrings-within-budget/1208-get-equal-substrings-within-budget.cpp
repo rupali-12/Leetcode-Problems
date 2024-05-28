8d40983b70be3071fdebc514088f1146e1915514
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-05-28 10:40:30 +0530

    Time: 4 ms (85.51%), Space: 9.5 MB (12.01%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int n= s.length();
+        vector<int>diff(n);
+        int mxLength=0, length=0, sum=0, i=0, j=0;
+        for(int i=0; i<n; i++){
+            diff[i] = abs(t[i]-s[i]);
+        }
+        while(j<n){
+            sum+= diff[j];
+            while(sum>maxCost){
+                length--;
+                sum-= diff[i];
+                i++;
+            }
+            
+            length++;
+            j++;
+            mxLength= max(length, mxLength);
+        }
+        return mxLength;
+    }
+};
