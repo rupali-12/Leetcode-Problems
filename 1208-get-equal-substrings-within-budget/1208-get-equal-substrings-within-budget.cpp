ef80785cec58ae01999af7e04a137972b6e0a1f2
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-05-28 10:41:06 +0530

    Time: 6 ms (62.23%), Space: 9.5 MB (7.76%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -4,14 +4,16 @@ public:
         int n= s.length();
         vector<int>diff(n);
         int mxLength=0, length=0, sum=0, i=0, j=0;
+        
         for(int i=0; i<n; i++){
             diff[i] = abs(t[i]-s[i]);
         }
+        
         while(j<n){
             sum+= diff[j];
             while(sum>maxCost){
                 length--;
-                sum-= diff[i];
+                sum-= diff[i];    // diff[i] = t[i]-s[i]
                 i++;
             }
             
