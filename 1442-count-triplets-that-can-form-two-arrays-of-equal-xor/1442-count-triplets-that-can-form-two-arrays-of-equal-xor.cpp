b813c503f913d8d530988263070462734b27f87c
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-05-30 10:29:36 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (43.21%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -4,13 +4,29 @@ public:
         int n = arr.size();
         int count=0;
         
-         // Approach-1: 
-        for(int i=0; i<n; i++){
-            int val= arr[i];
-            for(int k=i+1; k<n; k++){
-                val= val^arr[k];
-                if(val==0){
-                    count+= (k-i);
+        //  // Approach-1: 
+        // for(int i=0; i<n; i++){
+        //     int val= arr[i];
+        //     for(int k=i+1; k<n; k++){
+        //         val= val^arr[k];
+        //         if(val==0){
+        //             count+= (k-i);
+        //         }
+        //     }
+        // }
+        // return count;
+        
+        // Approach-2: Using Prefix sum 
+        vector<int>prefix(n+1, 0);
+        
+        for(int i=1; i<=n; i++){
+            prefix[i] = prefix[i-1]^arr[i-1];
+        }
+        
+        for(int i=0; i<=n; i++){
+            for(int k=i+1; k<=n; k++){
+                if(prefix[i]==prefix[k]){
+                    count+= (k-i-1);
                 }
             }
         }
