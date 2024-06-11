5a95347b1e582beddb9cc5c7f3681472d5d5de98
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-06-11 13:23:57 +0530

    Time: 3 ms (53.89%), Space: 10.7 MB (6.31%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,33 +1,60 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        vector<int>ans;
+        
+//         // Approach-1: using map 
+//         vector<int>ans;
+//         unordered_map<int, int>mp;
+//         for(int i=0; i<arr1.size(); i++){
+//             mp[arr1[i]]++;    
+//         }
+        
+//         for(int i=0; i<arr2.size(); i++){
+//            int count = mp[arr2[i]];
+//             while(count--){
+//                 ans.push_back(arr2[i]);
+//             }
+//             mp.erase(arr2[i]);
+//         }
+        
+//         int sizeToBeSorted= ans.size();
+        
+//         // now push remaining element 
+//         for(auto m: mp){
+//             int count=m.second;
+//             while(count--){
+//                 ans.push_back(m.first);
+//             }
+//         }
+        
+//         // sort element from sizeTBeSorted to end of ans array 
+//         sort(ans.begin()+sizeToBeSorted, ans.end());
+        
+//         return ans;
+        // *******************************************************************
+         // Approach-2: using lambda function 
         unordered_map<int, int>mp;
-        for(int i=0; i<arr1.size(); i++){
-            mp[arr1[i]]++;    
-        }
-        
         for(int i=0; i<arr2.size(); i++){
-           int count = mp[arr2[i]];
-            while(count--){
-                ans.push_back(arr2[i]);
-            }
-            mp.erase(arr2[i]);
+            mp[arr2[i]]=i;    
         }
         
-        int sizeToBeSorted= ans.size();
-        
-        // now push remaining element 
-        for(auto m: mp){
-            int count=m.second;
-            while(count--){
-                ans.push_back(m.first);
-            }
+        for(int i=0; i<arr1.size(); i++){
+           if(!mp.count(arr1[i])){
+               mp[arr1[i]]= INT_MAX;
+           }
         }
         
-        // sort element from sizeTBeSorted to end of ans array 
-        sort(ans.begin()+sizeToBeSorted, ans.end());
+        // sort element using lambda function 
         
-        return ans;
+        auto lambda = [&](int num1, int num2){
+            if(mp[num1]==mp[num2]){
+                return num1<num2;
+            }
+            return mp[num1]<mp[num2];    // if true then return num1 else num2 
+        };
+        
+        sort(arr1.begin(), arr1.end(), lambda);
+        
+        return arr1;
     }
 };
