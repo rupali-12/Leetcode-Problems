5816fb7c20e1b5edac023eed4c810e97c5ab2823
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-06-18 17:13:31 +0530

    Time: 51 ms (57.86%), Space: 41 MB (66.37%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -3,27 +3,60 @@ public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         int n= profit.size(), m= worker.size();
         
-        // Approach-1: Using Heap sort 
-        priority_queue<pair<int, int>>pq;
-        for(int i=0; i<n; i++){
-            int prof = profit[i];
-            int diff = difficulty[i];
+//         // Approach-1: Using Heap sort 
+//         priority_queue<pair<int, int>>pq;
+//         for(int i=0; i<n; i++){
+//             int prof = profit[i];
+//             int diff = difficulty[i];
             
-            pq.push({prof, diff});
+//             pq.push({prof, diff});
+//         }
+        
+//         int i=0, totalProfit=0;
+//         // sort workers in desc 
+//         sort(worker.begin(), worker.end(), greater<int>());
+        
+//         while(i<m && !pq.empty()){
+//             if(pq.top().second > worker[i]){   // worker is incapable
+//                 pq.pop();
+//             }
+//             else{
+//                 totalProfit+= pq.top().first;
+//                 i++;
+//             }
+//         }
+//         return totalProfit;
+        
+        // Approach-2: Using Preprocessing
+        vector<pair<int, int>>vec;
+        for(int i=0; i<n; i++){
+            vec.push_back({difficulty[i], profit[i]});
         }
         
-        int i=0, totalProfit=0;
-        // sort workers in desc 
-        sort(worker.begin(), worker.end(), greater<int>());
+        sort(vec.begin(), vec.end());
         
-        while(i<m && !pq.empty()){
-            if(pq.top().second > worker[i]){   // worker is incapable
-                pq.pop();
+        // pre processing to find the maximum profit of worker till index i at constant time 
+        for(int i=1; i<n; i++){
+            vec[i].second= max(vec[i].second, vec[i-1].second);
+        }
+        
+        int totalProfit=0;
+        for(int i=0; i<m; i++){
+            int workerLevel = worker[i];
+            
+            int l=0, r= vec.size()-1;
+            int maxProfit=0;
+            while(l<=r){
+                int mid= l+(r-l)/2;
+                if(worker[i]>=vec[mid].first){
+                    maxProfit= max(maxProfit, vec[mid].second);
+                    l= mid+1;
+                }
+                else{
+                    r= mid-1;
+                }
             }
-            else{
-                totalProfit+= pq.top().first;
-                i++;
-            }
+            totalProfit+= maxProfit;
         }
         return totalProfit;
     }
