41e913957eb77ee5ec2bac33718f1ac70a4598b6
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-07-10 18:48:47 +0530

    Time: 9 ms (22.38%), Space: 14.3 MB (17.13%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -2,18 +2,31 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         
-        // Approach-1: Brute force 
-        int depth=0;
+//         // Approach-1: Brute force 
+//         int depth=0;
+//         for(string str: logs){
+//             if(str=="../"){
+//                 // depth--;
+//                 // if(depth<0) depth=0;
+                
+//                 depth= max(0, depth-1);
+//             }
+//             else if(str=="./") continue;
+//             else depth++;
+//         }
+//         return depth;
+        
+        // Approach-2: Stack
+        stack<string>st;
         for(string str: logs){
             if(str=="../"){
-                // depth--;
-                // if(depth<0) depth=0;
-                
-                depth= max(0, depth-1);
+                if(!st.empty()){
+                    st.pop();
+                }
             }
             else if(str=="./") continue;
-            else depth++;
+            else st.push(str);
         }
-        return depth;
+        return st.size();
     }
 };
