07b970df7027fba2b0f3fbe4811d40a7660bd99d
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-08-07 22:56:54 +0530

    Time: 7 ms (19.94%), Space: 12.5 MB (16.40%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    // Make map 
+    unordered_map<int, string>belowTen = {{0, ""}, {1, "One"}, {2,"Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+     unordered_map<int, string>belowTwenty = {{10, "Ten"}, {11, "Eleven"}, {12,"Twelve"}, {13, "Thirteen"}, {14, "Fourteen"}, {15, "Fifteen"}, {16, "Sixteen"}, {17, "Seventeen"}, {18, "Eighteen"}, {19, "Nineteen"}};
+     unordered_map<int, string>belowHundred = {{1, "Ten"}, {2, "Twenty"}, {3, "Thirty"}, {4, "Forty"}, {5, "Fifty"}, {6, "Sixty"}, {7, "Seventy"}, {8, "Eighty"}, {9, "Ninety"}};
+    
+    string solve(int num){
+        if(num < 10){
+            return belowTen[num];
+        }
+        if(num<20){
+            return belowTwenty[num];
+        }
+        
+        // below hundred   85
+        if(num<100){
+            return belowHundred[num/10] + ((num%10 !=0)? (" "+solve(num%10)): "");
+        }
+        
+        // below 1000 
+        if(num<1000){
+            return solve(num/100) + " Hundred" + ((num%100 !=0)? (" "+solve(num%100)):"");
+        }
+        
+        // below Million (6-0's) 99999
+        if(num<1000000){
+            return solve(num/1000) + " Thousand"+ ((num%1000 !=0)?(" "+solve(num%1000)):"");
+        }
+        
+        // below Billion(9-0's)
+        if(num<1000000000){
+            return solve(num/1000000) + " Million"+ ((num%1000000 !=0)?(" "+solve(num%1000000)):"");
+        }
+        
+        // else case of  more than billion  
+        return solve(num/1000000000) + " Billion"+ ((num%1000000000 !=0)?(" "+solve(num%1000000000)):"");
+    }
+    
+    string numberToWords(int num) {
+        if(num ==0) return "Zero";
+        
+        return solve(num);
+    }
+};
