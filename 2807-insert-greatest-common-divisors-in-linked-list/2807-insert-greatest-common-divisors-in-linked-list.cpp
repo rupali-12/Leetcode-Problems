af35bbb54fbe27b1ec259694210aa826856b3d07
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-09-10 10:45:19 +0530

    Time: 39 ms (60.32%), Space: 38.5 MB (5.71%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -10,53 +10,50 @@
  */
 class Solution {
 public:
-    int findGCD(int a, int b){
+    int findGcd(int a, int b){
         if(a==b) return a;
-        else if(a>b){
-            return findGCD(a-b, b);
+        if(a>b){
+            return findGcd(a-b, b);
         }
-        else{
-            return findGCD(a, b-a);
-        }
+        return findGcd(a, b-a);
     }
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
+        
         vector<int>v;
         ListNode* temp= head;
         while(temp!=NULL){
             v.push_back(temp->val);
             temp= temp->next;
         }
+        
         int n= v.size();
         vector<int>gcd(v.size()-1);
-        
-        // fill gcd vector
         for(int i=0; i<n-1; i++){
             int first= v[i];
             int second= v[i+1];
-            int curr_gcd = findGCD(first, second);
-            gcd[i]= curr_gcd;
+            int curr_gcd = findGcd(first, second);
+            gcd[i] = curr_gcd;
         }
-        ListNode* ans= new ListNode(0);
-       temp= ans;
         
+        ListNode* ans = new ListNode(0);
+        temp=ans;
         for(int i=0; i<n-1; i++){
-        
-            // insert ith from v  
+            // insert ith from v 
             ListNode* newNode1= new ListNode(v[i]);
             temp->next= newNode1;
-            temp= newNode1;
-           
-             // insert ith from gcd  
+            temp= temp->next;
+            
+             // insert ith from gcd
             ListNode* newNode2= new ListNode(gcd[i]);
             temp->next= newNode2;
-            temp= newNode2;
+            temp= temp->next;
         }
         
-        // insert last value from vth 
-        ListNode* newNode= new ListNode(v[n-1]);
-        temp->next= newNode;
-        temp= newNode;
+        // insert last node from v 
+            ListNode* newNode= new ListNode(v[n-1]);
+            temp->next= newNode;
+            temp= temp->next;
         return ans->next;
     }
 };
