07b9be28435d28da5eb7e5a5f177ce4e8cb571eb
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-09-11 12:35:55 +0530

    Time: 3 ms (41.47%), Space: 7.6 MB (44.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -5,13 +5,10 @@ public:
         
         int ans=0;
         while(total!=0){
-           int bit = total&1;
-           if(bit==1){
-               ans++;
-           }
-          total>>=1;
+            int bit= total&1;
+            if(bit==1) ans++;
+            total>>=1;
         }
-        
         return ans;
     }
 };
