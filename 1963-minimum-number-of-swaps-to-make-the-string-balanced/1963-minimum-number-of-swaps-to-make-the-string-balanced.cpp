23b0c2830935b803df14c8fa8797978fab68648d
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-10-08 18:17:22 +0530

    Time: 106 ms (53.60%), Space: 32.6 MB (79.33%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,39 +1,16 @@
+
 class Solution {
 public:
-    int minSwaps(string str) {
-        
-  // step-1: check odd condition
-  if(str.length()%2!=0){
-    return -1;
-  }
-  //  step-2: Remove Valid part 
-       int openCount=0, closeCount=0;
-     stack<int>s;
-     for(int i=0; i<str.length(); i++){
-       char ch= str[i];
-       if(ch=='['){
-         s.push(ch);
-       }
-       else{
-         //  ch is closed bracket 
-        if(!s.empty() && s.top()=='['){
-          s.pop();
+    int minSwaps(string s) {
+        int size = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch == '[')
+                size++;
+            else if (size > 0) 
+                size--;
         }
-        else{
-          s.push(ch);
-        }
-       }
-     }
-
-    //  step 2 count number of opening and closing brackets in stack present after removing valid part 
-    while(!s.empty()){
-      if(s.top()=='[')
-        openCount++;
-      else
-        closeCount++;
-    s.pop();
-    }
-  int res = (openCount+1)/2 + (closeCount+1)/2;
-   return res/2;
+        return (size + 1) / 2;
     }
 };
