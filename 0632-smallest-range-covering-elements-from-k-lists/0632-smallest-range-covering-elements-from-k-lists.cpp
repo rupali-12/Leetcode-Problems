1a81090cc536e814001dd98e93a0da60565e12cd
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-10-13 10:32:40 +0530

    Time: 38 ms (84.45%), Space: 22.4 MB (55.25%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,75 +1,37 @@
-
-class node{
-public:
-   int data;
-   int listNumber;
-   int valueInCurrList;
-
-   node(int data, int row, int col){
-       this->data = data;
-       listNumber =row;
-       valueInCurrList = col;
-   }
-};
-class compare{
-    public:
-    bool operator()(node* a, node*  b){
-        return a->data > b->data;
-    }
-};
 class Solution {
 public:
-    
-    vector<int> smallestRange(vector<vector<int>>& a) {
-        int k= a.size();
-         int mn =INT_MAX;
-    int mx =INT_MIN;
-    int range;
-    priority_queue<node*, vector<node*>, compare>minHeap;
-    // step-1 :: insert first element of all lists and track min/ max element
-    for(int i=0;i <k; i++){
-        node* temp = new node(a[i][0],i,0);
-        mn = min(a[i][0], mn);
-        mx = max(a[i][0], mx);
-        minHeap.push(temp);
-    }
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        // Vector to store all numbers with their list index
+        vector<pair<int, int>> ordered;
 
-    // step-2:: process for min and max;
-    int rangeStart =mn, rangeEnd =mx;
+        // Populate the ordered vector with all numbers and their corresponding list index
+        for (size_t k = 0; k < nums.size(); ++k)
+            for (auto n: nums[k]) ordered.push_back({n, k});
 
-    // process ranges  
-    while(!minHeap.empty()){
+        // Sort the ordered vector based on the numbers
+        sort(ordered.begin(), ordered.end());
 
-    // mn fetch 
-       node* temp = minHeap.top();
-       minHeap.pop();
-       mn= temp->data;    // min update
+        int i = 0, k = 0;  // i: start of current range, k: count of unique lists covered
+        vector<int> ans;   // Vector to store the final answer (smallest range)
+        unordered_map<int, int> count;  // Map to keep track of number of elements from each list
 
-    //    range and ans updation 
-       if((mx-mn)< (rangeEnd-rangeStart)){
-        //    range = rangeEnd - rangeStart;
-        rangeEnd = mx;
-        rangeStart =mn;
-       }
+        // Iterate through the sorted ordered vector
+        for (size_t j = 0; j < ordered.size(); ++j) {
+            // If this is a new list or increment the count for this list
+            if (! count[ordered[j].second]++) ++k;
 
-    //    now update mx 
-    int r =temp->listNumber;
-    int c =temp->valueInCurrList;
+            // If we have covered all lists
+            if (k == nums.size()) { 
+                // Shrink the range from the start while maintaining coverage of all lists
+                while (count[ordered[i].second] > 1) --count[ordered[i++].second]; 
 
-    // next element exits 
-    if((c+1) < a[r].size()){
-      node* newNode =new node(a[r][c +1], r, c+1);
-      minHeap.push(newNode);
-       mx =max(mx, a[r][c+1]);
-        
-    }
-    else{         //  next element not exits 
-        break;
-    }
-    }
-    vector<int>ans;
-        ans.push_back(rangeStart);
-        ans.push_back(rangeEnd);
-    return ans;
+                // Update the answer if this is the first valid range or if it's smaller than the previous range
+                if (ans.empty() || ans[1] - ans[0] > ordered[j].first - ordered[i].first) {
+                    ans = vector<int>{ordered[i].first, ordered[j].first};
+                }
+            }
+        }
+
+        return ans;  // Return the smallest range that includes at least one number from each list
     }
 };
