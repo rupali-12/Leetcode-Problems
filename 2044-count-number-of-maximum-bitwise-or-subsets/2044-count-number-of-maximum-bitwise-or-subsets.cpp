f9da0f7e12ad6fa0f65346024dfaa5610a5b70d1
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2024-10-18 11:57:26 +0530

    Time: 170 ms (11.36%), Space: 324.5 MB (6.37%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -15,6 +15,24 @@ public:
         
         return takeCount + notTakeCount;
     }
+     int solveRecMemo(int idx, int currOr, vector<int>nums, int &maxOr, vector<vector<int>>&dp){
+        if(idx==nums.size()){
+            
+            if(currOr==maxOr) return dp[idx][currOr] = 1;
+            else return dp[idx][currOr] = 0;
+        }
+         if(dp[idx][currOr]!=-1){
+             return dp[idx][currOr];
+         }
+        
+        // Take 
+        int takeCount= solveRecMemo(idx+1, currOr | nums[idx], nums, maxOr, dp);
+        
+         // Not Take 
+        int notTakeCount= solveRecMemo(idx+1, currOr, nums, maxOr, dp);
+        
+        return dp[idx][currOr] = takeCount + notTakeCount;
+    }
     int countMaxOrSubsets(vector<int>& nums) {
          int maxOr =0;
         for(auto n: nums){
@@ -22,7 +40,11 @@ public:
         }
         int currOr=0;
         
-        // Approach-1: Recursion 
-        return solveRec(0, currOr, nums, maxOr);
+        // // Approach-1: Recursion 
+        // return solveRec(0, currOr, nums, maxOr);
+        
+        // Approach-2: Recursion + Memoization
+        vector<vector<int>>dp(nums.size()+1, vector<int>(maxOr+1, -1));
+        return solveRecMemo(0, currOr, nums, maxOr, dp);
     }
 };
