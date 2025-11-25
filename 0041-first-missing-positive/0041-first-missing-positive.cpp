18de14ac621f9c28e7d73d022721f24a0916431f
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-11-25 13:14:34 +0530

    Time: 0 ms (100%), Space: 55.1 MB (60.81%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
+//   short: if ith idx  value is positive means number i+1 is missing as we are marking i-1th idx as negative if nums[i] is posite and i-1 is valid
+// example 
+// index0 → negative → 1 present
+// index1 → negative → 2 present
+// index4 → positive → 5 is missing
+
     int firstMissingPositive(vector<int>& nums) {
-        int n= nums.size();
-        
-        // make all neg and 0 as n+1
+        int n=nums.size();
+
+        // mark all neg and 0 as n+1
         for(int i=0; i<n; i++){
             if(nums[i]<=0){
-                nums[i]= n+1;
+                nums[i] = n+1;
             }
         }
-        
-        // mark nums[i] as neg if index possible 
+
+        // mark nums[i] as neg if index possible
         for(int i=0; i<n; i++){
-            int index= abs(nums[i])-1;
-            if(index<n && nums[index]>0){  /// nums[index]>0 as it should not be already neg bcoz then it will become positive
-                 nums[index]*= -1;
+            int idx = abs(nums[i])-1;
+            // if idx is possible then make it negative 
+            if(idx<n && nums[idx]>0){  // make sure nums[idx] should be positive to make it negative if it is neg  already then it will become positive
+            nums[idx]*= -1;
             }
         }
-        
+
+        // check now, if value at any idx is positive it means it value+1 is not present 
         for(int i=0; i<n; i++){
             if(nums[i]>0){
                 return i+1;
