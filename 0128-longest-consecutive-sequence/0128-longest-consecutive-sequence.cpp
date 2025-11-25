45c3dac720aa849b8ef6fce07b2d0389f1af631e
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-11-25 12:00:53 +0530

    Time: 195 ms (5.59%), Space: 91.3 MB (12.07%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0) return 0;
-        sort(nums.begin(), nums.end());
-        int currcount=0, lastSmaller= INT_MIN, longestLen=1;
-        for(int i=0; i<nums.size(); i++){
-            // case of consecutive 
-            if(nums[i]-1 == lastSmaller){
-                currcount++;
-                lastSmaller= nums[i];
-            }
-            // case of non consecutive and not equal 
-            else if(lastSmaller!=nums[i]){
-                currcount=1;
-                lastSmaller= nums[i];
-            }
-            longestLen = max(longestLen, currcount);
+        int n= nums.size();
+
+    //     // Approach 1: Sorting 
+    //     sort(nums.begin(), nums.end());
+    //     int longestSub=0, lastSmallest=INT_MIN, currCount=0;
+    //     for(int i=0; i<n; i++){
+    //         // case of consecutive
+    //         if((nums[i]-1)==lastSmallest){
+    //            currCount++;
+    //            lastSmallest = nums[i];
+    //         }
+    //         // case of non consecutive 
+    //         else if(lastSmallest != nums[i]){
+    //            currCount=1;
+    //            lastSmallest = nums[i];
+    //         }
+    //         longestSub = max(longestSub, currCount);
+    //     }
+    //    return longestSub;
+
+    // Approach 2: using Set 
+    if(n==0) return 0;
+    set<int>st;
+    for(int num: nums){
+        st.insert(num);
+    }
+
+    int longestSub=1;
+    for(int num: st){
+        if(st.find(num-1)!=st.end()){
+            continue;
         }
-       return longestLen;
+        else{
+            int currNum=num;
+            int currCount=1;
+            while(st.find(currNum+1)!=st.end()){
+                currNum++;
+                currCount++;
+            }
+
+            // calculate longest length
+            longestSub = max(longestSub, currCount);
+        }
+    }
+        return longestSub;
     }
 };
