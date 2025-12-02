938958a63990b3c644b4c3210265c14b720021ce
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-02 13:07:15 +0530

    Time: 29 ms (31.22%), Space: 32.8 MB (10.29%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
-    bool comp(int a[], int b[]){
-        for(int i=0; i<26; i++){
-            if(a[i]!=b[i]){
-                return false;
-            }
+    bool comp(vector<int>count1, vector<int>count2){
+         for(int i=0; i<26; i++){
+            if(count2[i]!=count1[i]) return false;
         }
         return true;
     }
     bool checkInclusion(string s1, string s2) {
-        int temp1[26]={0};
-        
-        // Store count of every characters in s1
+    vector<int>count1(26, 0);
+    vector<int>count2(26, 0);
+
+    //    store count character of s1
         for(int i=0; i<s1.length(); i++){
-            int ind = s1[i]-'a';
-            temp1[ind]++;
+            count1[s1[i]-'a']++;
         }
-        int i=0, winSize=s1.length();
-        int temp2[26]={0};
-      while(i<winSize && i<s2.length()){
-          int ind= s2[i]-'a';
-          temp2[ind]++;
-          i++;
-      }
-         if(comp(temp1, temp2)){
-           return true;   
-          }
-        
-        while(i<s2.length()){
-            int n= s2[i]-'a';
-            temp2[n]++;
-            n= s2[i-winSize]-'a';
-            temp2[n]--;
-            i++;
-            if(comp(temp1, temp2)){
-                return true;
-            }
-        }
-        return false;
+
+    //    process first window 
+    int winSize=s1.length();
+    int i=0;
+    while(i<winSize && i<s2.length()){
+       int idx = s2[i]-'a';
+       count2[idx]++;
+       i++;
+    }
+
+    if(comp(count1, count2)){
+        return true;
+    }
+
+    // process remaining window 
+    while(i<s2.length()){
+       int indNew = s2[i]-'a';
+       count2[indNew]++;
+
+       int indOld = s2[i-winSize]-'a';
+       count2[indOld]--;
+       i++;
+       if(comp(count1, count2)) return true;
+    }
+
+    return false;
+
     }
 };
