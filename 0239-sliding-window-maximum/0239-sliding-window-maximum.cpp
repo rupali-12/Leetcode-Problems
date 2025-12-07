06149b31a8a7720325fb961973311e67561883fe
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-07 20:11:12 +0530

    Time: 36 ms (26.38%), Space: 139.3 MB (25.92%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
         vector<int>ans;
         deque<int>dq;
-        
+
+        // process first window 
         for(int i=0; i<k; i++){
-            while(!dq.empty() && dq.back()<nums[i]){
+            while(!dq.empty() && nums[i]>dq.back()){
                 dq.pop_back();
             }
             dq.push_back(nums[i]);
         }
-           ans.push_back(dq.front());
-        
-        for(int i=k; i<nums.size(); i++){
-            if(dq.front() == nums[i-k]){    // bcoz if this is true then that element can never be the answer for nxt window 
-                dq.pop_front();           
+        ans.push_back(dq.front());
+
+        // process remaining window 
+        for(int i=k; i<n; i++){
+            // if left most element of previous window always be the outgoing element if it is the max element then remove from deque also
+            if(dq.front() == nums[i-k]){
+                dq.pop_front();
             }
-            
-             while(!dq.empty() && dq.back()<nums[i]){
+
+            while(!dq.empty() && nums[i]>dq.back()){
                 dq.pop_back();
             }
+
             dq.push_back(nums[i]);
             ans.push_back(dq.front());
         }
