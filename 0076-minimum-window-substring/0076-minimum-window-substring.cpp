aef6be6c3771f304dd29361e2dd336d3caa7d159
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-07 20:40:48 +0530

    Time: 4 ms (73.61%), Space: 11.4 MB (81.49%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       if(t.length()>s.length()) return "";
-        int minWinSize=INT_MAX, i=0, j=0, start_i=0;
-        unordered_map<char, int>mp;
-        for(auto a: t){
-            mp[a]++;
+        int n= s.length();
+        if(t.length() > n){
+            return "";
         }
-        int reqCount= t.length();
-        while(j<s.length()){
+
+        unordered_map<char, int>mp;
+        int minWindowSize=INT_MAX;
+        int start_i=0, i=0, j=0;
+        int requiredCount=t.length();
+
+        // store t in mp
+        for(auto c: t){
+            mp[c]++;
+        }
+
+        while(j<n){
             char ch = s[j];
-            if(mp[ch]>0){    //  it is required so take from map and decrease its count
-                reqCount--;
+            if(mp[ch]>0){
+                requiredCount--;
             }
             mp[ch]--;
-            
-            // shrink window 
-            while(reqCount==0){
-                int currWinSize=j-i+1;
-                if(minWinSize>currWinSize){
-                    minWinSize= currWinSize;
-                    start_i = i;
+
+            // case when shrink
+            while(requiredCount==0){
+                int currentWindowSize= j-i+1;
+                if(minWindowSize > currentWindowSize){
+                    minWindowSize = currentWindowSize;
+                    start_i =i;  // this store start of min window
                 }
-                mp[s[i]]++;
+                mp[s[i]]++;  // increase as it required we shrink
                 if(mp[s[i]]>0){
-                    reqCount++;
+                    requiredCount++;  // if any char count>0 it means we required as non required will always be negative count
                 }
                 i++;
             }
             j++;
         }
-        return minWinSize==INT_MAX ? "": s.substr(start_i, minWinSize);
+       return minWindowSize==INT_MAX? "": s.substr(start_i, minWindowSize);
     }
 };
-
