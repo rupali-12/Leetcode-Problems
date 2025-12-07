5c6d22710ba9cb93b2725740b53e38a685041442
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-07 15:54:17 +0530

    Time: 70 ms (59.35%), Space: 160.5 MB (47.2%) - LeetHub

--- 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.cpp
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.cpp
@@ -1,30 +1,98 @@
 class Solution {
 public:
-    const int mod= 1e9 + 7;
+   const int mod= 1e9+7;
+   typedef long long ll;
+   int n;
+   ll solveDP(int i, vector<int>&nums, int k, vector<int>&dp){
+    if(i>=n){
+        return 1;
+    }
+
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+
+    ll count= 0;
+    int mx=INT_MIN;
+    int mn=INT_MAX;
+    for(int j=i; j<n; j++){
+         mx= max(mx, nums[j]);
+         mn= min(mn, nums[j]);
+         if(mx-mn >k){
+            break;
+         }
+         count= (count + solveDP(j+1, nums, k, dp))%mod;
+    }
+    return dp[i] = count;
+   }
     int countPartitions(vector<int>& nums, int k) {
-        int n= nums.size();
-        vector<long long>dp(n+1, 0);
-        vector<long long>prefix(n+1, 0);
-        dp[0]=1;
-        prefix[0] =1;
-        multiset<int>s;
-        int j=0;
-        for(int i=0; i<n; i++){
-            s.insert(nums[i]);
+        n= nums.size();
+        // // Approach- 1: DP + Memoization
+        // vector<int>dp(n, -1);
+        // return solveDP(0, nums, k, dp);
 
-            while(j<=i && (*s.rbegin() - *s.begin())>k){
-                s.erase(s.find(nums[j]));
-                j++;
+// ********************************************************************************************************
+        
+    //     // Approach-2: Bottom up 
+    //     vector<int>dp(n+1,0);
+    //    dp[n]=1;
+    //     for(int i=n-1; i>=0; i--){
+    //         int mx= INT_MIN;
+    //         int mn= INT_MAX;
+    //         ll count = 0;
+    //         for(int j=i; j<n; j++){
+    //            mx= max(mx, nums[j]);
+    //            mn= min(mn, nums[j]);
+    //            if(mx-mn >k){
+    //             break;
+    //            }
+    //            count = (count + dp[j+1])%mod;
+    //         }
+    //         dp[i] = count;
+    //     }
+    //     return dp[0];
+
+// ********************************************************************************************************
+        
+        // Approach 3: optimized approach 
+        vector<ll>dp(n+1, 0);
+        vector<ll>prefixSum(n+1, 0);
+        dp[0] = 1;
+        prefixSum[0]=1;
+        deque<int>maxdq;
+        deque<int>mindq;
+        int i=0, j=0;
+        while(j<n){
+
+            // maintain max deque
+            while(!maxdq.empty() && nums[j] > nums[maxdq.back()]){
+                maxdq.pop_back();
+            }
+            maxdq.push_back(j);
+            // maintain min deque
+             while(!mindq.empty() && nums[j] < nums[mindq.back()]){
+                mindq.pop_back();
+            }
+            mindq.push_back(j);
+
+            // check invalid window condition 
+            while(i<=j && (nums[maxdq.front()] - nums[mindq.front()])>k){
+                i++;
+
+                // check deques contain valid i index value 
+                if(maxdq.front() < i){
+                    maxdq.pop_front();
+                }
+                if(mindq.front() < i){
+                    mindq.pop_front();
+                }
             }
 
-            if(j>0){
-                dp[i+1] = (prefix[i] - prefix[j-1]+mod)%mod;
-            }
-            else{
-                dp[i+1] = prefix[i]%mod;
-            }
-            prefix[i+1] = (prefix[i]+dp[i+1])%mod;
+            // now have valid window update dp, prefixSum
+            dp[j+1] = (prefixSum[j] - (i>0? prefixSum[i-1]:0)+mod)%mod;
+            prefixSum[j+1] = (prefixSum[j] + dp[j+1])%mod;
+            j++;
         }
-        return dp[n];
+            return dp[n];
     }
 };
