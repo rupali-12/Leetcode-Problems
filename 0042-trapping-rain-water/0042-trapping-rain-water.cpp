6769c883f0164d2b0df4c179dbd8dc5d1f514593
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-10 12:19:39 +0530

    Time: 3 ms (18.76%), Space: 27.4 MB (18.31%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size();
-        vector<int>prevLargest(n), nextLargest(n);
-       
-        prevLargest[0]= height[0];
-         int i=1;
+        int ans=0;
+        int n= height.size();
+        vector<int>nextLarger(n, 0);
+        vector<int>prevLarger(n, 0);
+
+        // first calculate prevLarger 
+        prevLarger[0] = height[0];
+        int i=1;
         while(i<n){
-            prevLargest[i] = max(prevLargest[i-1], height[i]);
-            i++;
+          prevLarger[i] = max(prevLarger[i-1], height[i]);
+          i++;
         }
-        
-        nextLargest[n-1]= height[n-1];
-         i= n-2;
+
+        // first calculate nextLarger 
+        nextLarger[n-1]=height[n-1];
+        i=n-2;
         while(i>=0){
-            nextLargest[i] = max(nextLargest[i+1], height[i]);
+            nextLarger[i] = max(nextLarger[i+1], height[i]);
             i--;
         }
-        
-        int ans=0;
-        for(i=0; i<n; i++){
-            int mn= min(nextLargest[i], prevLargest[i]);
-            ans+= (mn-height[i]);
+
+        for(int i=0; i<n; i++){
+            int mnheight= min(prevLarger[i], nextLarger[i]);
+            ans+= mnheight - height[i];  // minus height of bar, as to calculate the actual portion which is trapped at a particular place
         }
         return ans;
     }
