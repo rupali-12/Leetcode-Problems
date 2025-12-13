72a0e65e36baccda70e5d675e5702ec425c7dfb5
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-14 00:09:33 +0530

    Time: 0 ms (100%), Space: 15.7 MB (66.98%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int idx1, idx2;
-        // find idx1 --> which is index where nums[i]<ums[i+1]
-        for(int i=nums.size()-2; i>=0; i--){
-            if(nums[i]<nums[i+1]){
-                idx1 = i;
-                break;
-            }
-        }
-        if(idx1<0){
-            sort(nums.begin(), nums.end());
-            return;
-        }
-         // find idx2 --> which is index wher value is just greater than value at index idx1
-       else{
-            for(int i=nums.size()-1; i>=0; i--){
-            if(nums[i]>nums[idx1]){
-                idx2 = i;
-                break;
-            }
-        }
+        int n=nums.size();
+        // idx1 = blue_gola_index, idx2 = index which is just greater than blue_gola_index 
+        int idx1=-1, idx2=-1;
         
-        // swap value at idx1 and value at idx2 
-        swap(nums[idx1], nums[idx2]);
-        
-        // sort right part of idx1 
-        sort(nums.begin()+idx1+1, nums.end());
-       }
+        // find idx1 --> index where nums[i]>nums[i-1]
+        for(int i=n-1; i>0; i--){
+           if(nums[i]>nums[i-1]){
+            idx1=i-1;
+            break;
+           }
+        }
+
+    //    find idx2 only when there is one idx1 exist 
+    // idx2-> index where value is just greater than value at index idx1 
+        if(idx1!=-1){
+            for(int i=n-1; i>idx1; i--){   // idx1+1 se check krna h just greater element
+                if(nums[i]>nums[idx1]){
+                    idx2= i;
+                    break;
+                }
+            }
+            swap(nums[idx1], nums[idx2]);
+        }
+
+        reverse(nums.begin() + idx1+1, nums.end());
     }
 };
