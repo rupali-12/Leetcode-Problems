b9baaff5e66f884a826c3f067a39783e145e3742
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-15 12:14:18 +0530

    Time: 0 ms (100%), Space: 11 MB (92.99%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -3,10 +3,12 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string>ans(n+1);
         for(int i=1; i<=n; i++){
-            if(i%3==0 && i%5==0) ans[i] = "FizzBuzz";
-            else if(i%3==0) ans[i] = "Fizz";
-            else if(i%5==0) ans[i] = "Buzz";
-            else ans[i] = to_string(i);
+            if(i%3==0) ans[i] = "Fizz";
+            if(i%5==0){
+                // ans[i]=""? "Buzz": (ans[i]+"Buzz");
+                ans[i]= ans[i]+"Buzz";
+            }
+            if(ans[i]=="") ans[i] = to_string(i);
         }
         ans.erase(ans.begin());
         return ans;
