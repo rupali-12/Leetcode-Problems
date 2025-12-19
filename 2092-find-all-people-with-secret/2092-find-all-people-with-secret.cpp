8c2e79ef4a35bf7cb6512e6b9da06c458be43dab
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-19 19:22:51 +0530

    Time: 798 ms (10.03%), Space: 453.1 MB (5.15%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,50 +1,65 @@
 class Solution {
 public:
-    typedef pair<int, int>P;
+   typedef pair<int, int>P;
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-     
-        // make graph
-        map<int, vector<pair<int,  int>>>graph;
-        // initial insertion 
-        graph[0].push_back({firstPerson, 0});
-        graph[firstPerson].push_back({0, 0});
-        
-        for(auto& x: meetings){
-            int p1= x[0];
-            int p2= x[1];
-            int time= x[2];
-            graph[p1].push_back({p2, time});
-            graph[p2].push_back({p1, time});
+        map<int, vector<P>>timeMeetings;
+
+        for(auto meeting: meetings){
+            int person1 = meeting[0];
+            int person2 = meeting[1];
+            int time = meeting[2];
+            timeMeetings[time].push_back({person1, person2});
         }
-        
-        vector<int>knowing(n, 0);
-        priority_queue<P, vector<P>, greater<P>>pq;
-        // bfs 
-        // {time, secondPeron}
-        pq.push({0, 0});
-        pq.push({0, firstPerson});
-        while(!pq.empty()){
-            auto T = pq.top();
-            pq.pop();
-            int currTime= T.first, currPerson= T.second;
-            if(knowing[currPerson]==1) continue;
-            knowing[currPerson]= 1;
-            for(auto& neigh: graph[currPerson]){
-               int time=neigh.second;
-                int person = neigh.first;
-                if(!knowing[person] && time>=currTime){
-                    pq.push({time, person});
+
+        vector<bool>knowSecret(n, false);
+        knowSecret[0]=true;
+        knowSecret[firstPerson] = true;
+
+        for(auto &it: timeMeetings){
+            int t = it.first;
+            vector<P>meets  = it.second;
+
+            queue<int>que;
+            unordered_map<int, vector<int>>adj;  // a-> {b, c, d}
+            unordered_set<int>alreadyAdded;
+
+            for(auto& [person1, person2]: meets){
+               adj[person1].push_back(person2);
+               adj[person2].push_back(person1);
+
+            //    if person1 already know 
+            if(knowSecret[person1]==true && alreadyAdded.find(person1)==alreadyAdded.end()){
+                que.push(person1);
+                alreadyAdded.insert(person1);
+            }
+
+            //    if person2 already know 
+            if(knowSecret[person2]==true && alreadyAdded.find(person2)==alreadyAdded.end()){
+                que.push(person2);
+                alreadyAdded.insert(person2);
+            }
+            }
+
+            // Now I know who know who according to time 
+            // process BFS
+            while(!que.empty()){
+                int person = que.front();
+                que.pop();
+
+                for(auto & nextPerson: adj[person]){
+                    if(knowSecret[nextPerson]==false){
+                        knowSecret[nextPerson]=true;
+                        que.push(nextPerson);
+                    }
                 }
             }
         }
-        
-        
-        vector<int>ans;
-        for(int i=0; i<knowing.size(); i++){
-            if(knowing[i]==1){
-                ans.push_back(i);
+        vector<int>result;
+        for(int i=0; i<n; i++){
+            if(knowSecret[i]==true){
+                result.push_back(i);
             }
         }
-        return ans;
+        return result;
     }
 };
