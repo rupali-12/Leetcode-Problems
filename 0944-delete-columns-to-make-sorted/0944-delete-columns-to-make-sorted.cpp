0e3a3031ba917dd135eb015001032a4ac01d0ab4
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-20 13:27:44 +0530

    Time: 4 ms (45.13%), Space: 15.7 MB (14.42%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int count=0;
-        int n=strs.size();
-        int len = strs[0].length();
-        for(int i=0; i<len; i++){
-            string temp="";
-           for(int curr=0; curr<n; curr++){
-              temp+= strs[curr][i];   // current string ka ith char
-           }
-        //    now we have ith char of every string
-        string withoutSort= temp;
-        sort(temp.begin(), temp.end());
-        if(temp!=withoutSort){   // those which are not sorted already
-            count++;
+    
+    // //  Approach-1 Brute Force 
+    //     int count=0;
+    //     int n=strs.size();
+    //     int len = strs[0].length();
+    //     for(int i=0; i<len; i++){
+    //         string temp="";
+    //        for(int curr=0; curr<n; curr++){
+    //           temp+= strs[curr][i];   // current string ka ith char
+    //        }
+    //     //    now we have ith char of every string
+    //     string withoutSort= temp;
+    //     sort(temp.begin(), temp.end());
+    //     if(temp!=withoutSort){   // those which are not sorted already
+    //         count++;
+    //     }
+    //     }
+    //     return count;
+
+    // Approach 2: Optimize approach 
+    int row = strs.size();
+    int col = strs[0].length();
+    int count=0;
+    for(int c=0; c<col; c++){
+        for(int r=1; r<row; r++){
+            if(strs[r][c] < strs[r-1][c]){
+                count++;
+                break;
+            }
         }
-        }
-        return count;
+    }
+    return count;
     }
 };
