fdc64dfe2b9932b7d7acd24e7b63a7e900cb09ae
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2025-12-23 18:23:09 +0530

    Time: 114 ms (46.33%), Space: 166.9 MB (52.12%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    int n;
-    int dp[100001][3];
-    int binSearch(vector<vector<int>>&events, int endTime){
-        int l=0, r= n-1;
-        int ans=n;
-        while(l<=r){
-            int mid= l+(r-l)/2;
-            if(events[mid][0]> endTime){
-                ans= mid;
-                r= mid-1;
-            }
-            else{
-                l= mid+1;
-            }
+int n;
+int dp[100001][3];
+   int binarySearch(vector<vector<int>>& events, int currEndTime){
+      int low= 0;
+      int high= n-1;
+      int ans=n;
+      while(low<=high){
+        int mid = low + (high-low)/2;
+        if(events[mid][0] > currEndTime){
+            ans = mid;
+            high= mid-1;
         }
-        return ans;
-    }
-    int solve(vector<vector<int>>&events, int i, int count){
-        if(count==2 || i>=n) return 0;
-        if(dp[i][count]!=-1){
-            return dp[i][count];
+        else{
+            low =mid+1;
         }
-        int nextValidIdx = binSearch(events, events[i][1]);
-        int take= events[i][2] + solve(events, nextValidIdx, count+1);
-        
-        int notTake= solve(events, i+1, count);
-        
-        return dp[i][count]= max(take, notTake);
+      }
+      return ans;
+   }
+   int solve(vector<vector<int>>& events, int i, int count){
+    if(count==2 || i==n){
+        return 0;
     }
+
+    if(dp[i][count]!=-1){
+        return dp[i][count];
+    }
+    
+    int nextValidIdx = binarySearch(events, events[i][1]);   // end time of current index i
+    int take = events[i][2] + solve(events, nextValidIdx, count+1);
+    int notTake = solve(events, i+1, count);
+    return dp[i][count] = max(take, notTake);
+   }
     int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
-        n= events.size();
-        int count=0;
+        n = events.size();
+        sort(events.begin(), events.end());\
         memset(dp, -1, sizeof(dp));
-        
+        int count=0;
         return solve(events, 0, count);
     }
 };
