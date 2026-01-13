3c55ca4f76497b3e47f1b50af17a5a913e1ce005
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-01-13 12:05:56 +0530

    Time: 93 ms (69.36%) | Memory: 198.7 MB (52.02%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.cpp
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
+    double areaBelowMid(double mid, vector<vector<int>>& squares){
+        double area = 0;
+        for(auto &square: squares){
+            double y = square[1], len = square[2];
+        if(mid >= (y+len)){
+           area+= len*len;
+        }
+        else if((mid>y) && (mid<(y+len))){
+            area+= len*(mid-y);
+        }
+        }
+        return area;
+    }
     double separateSquares(vector<vector<int>>& squares) {
-        double max_y = 0, total_area = 0;
-        for (auto& sq : squares) {
-            int y = sq[1], l = sq[2];
-            total_area += (double)l * l;
-            max_y = max(max_y, (double)(y + l));
+        double totalArea=0, ansY=0;
+        double mn = INT_MAX, mx=INT_MIN;
+        for(auto &square: squares){
+            double len = square[2];
+            totalArea += len* len;
+            mn = min(mn, (double)square[1]);
+            mx = max(mx, (double)square[1]+len);
         }
 
-        auto check = [&](double limit_y) -> bool {
-            double area = 0;
-            for (auto& sq : squares) {
-                int y = sq[1], l = sq[2];
-                if (y < limit_y) {
-                    area += l * min(limit_y - y, (double)l);
-                }
+        double target = totalArea/2;
+        double low = mn, high = mx;
+        while(high - low > 1e-5){
+            double mid = low + (high-low)/2;
+            if(areaBelowMid(mid, squares) < target){
+                low = mid;
             }
-            return area >= total_area / 2;
-        };
-
-        double lo = 0, hi = max_y;
-        double eps = 1e-5;
-        while (abs(hi - lo) > eps) {
-            double mid = (hi + lo) / 2;
-            if (check(mid)) {
-                hi = mid;
-            } else {
-                lo = mid;
+            else{
+                ansY = mid;
+                high= mid;
             }
         }
-        return hi;
+        return ansY;
     }
 };
