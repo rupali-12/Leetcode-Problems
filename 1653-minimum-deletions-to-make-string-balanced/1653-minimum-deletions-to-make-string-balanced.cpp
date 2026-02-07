d8e7c86a38914d79b7b21fa2730dbd8afd388794
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-02-07 15:22:26 +0530

    Time: 41 ms (30.03%), Space: 26.4 MB (40.38%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length();
-        stack<char>st ; 
-        int count = 0 ;
+        int n= s.length();
+        stack<char>st;
         st.push(s[0]);
-        for(int i=1;i<n;i++){
-            if(!st.empty()  && (st.top() == 'b' && s[i] == 'a')){
+        int ans=0;
+        for(int i=1; i<n; i++){
+            if(!st.empty() && (st.top()=='b' && s[i]=='a')){
                 st.pop();
-                count+=1;
+                ans+=1;
             }
             else{
                 st.push(s[i]);
             }
         }
-        return count ;
+        return ans;
     }
 };
