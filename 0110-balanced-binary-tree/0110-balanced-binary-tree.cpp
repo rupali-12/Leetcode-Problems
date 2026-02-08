c537f2bef185c8eb120e682e99884c2771d19e86
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-02-08 20:00:56 +0530

    Time: 0 ms (100%), Space: 23 MB (82.16%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -9,30 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//  A binary tree is height-balanced if for every node,
+// the height difference between its left subtree and right subtree is at most 1.
 class Solution {
 public:
-     int height(TreeNode* root){
-        if(root==NULL){
-            return 0;
-        }
+    int height(TreeNode* root){
+        if(root ==NULL) return 0;
+
         int left = height(root->left);
         int right = height(root->right);
         return max(left, right) +1;
     }
-    bool isBalanced(TreeNode *root)
-    {
-        //  Your Code here
-        if(root==NULL){
-            return true;
-        }
-        int left= height(root->left);
-        int right= height(root->right);
-        if(abs(left-right)>1){
-            return false;
-        }
-        int ansleft = isBalanced(root->left);
-        int ansRight =isBalanced(root->right);
-        return ansleft && ansRight;
-        
+    bool isBalanced(TreeNode* root){
+        if(root==NULL) return true;
+
+        // check for root 
+        int left = height(root->left);
+        int right = height(root->right);
+        if(abs(left-right)>1) return false;
+
+        bool leftAns = isBalanced(root->left);
+        bool rightAns = isBalanced(root->right);
+        return leftAns && rightAns; 
     }
 };
