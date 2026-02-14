756d5f01c45d22853ef8ac020affefb2a19cc07a
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-02-14 23:37:46 +0530

    Time: 5 ms (59.08%) | Memory: 8.6 MB (96.85%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double dp[101][101];
+    double solve(int poured, int i, int j){
+        if(i<0 || j<0 || i<j)
+           return 0.0;
+
+        if(i==0 && j==0){   // reached top glass
+           return dp[i][j] = poured;
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        double upper_left = (solve(poured, i-1, j-1)-1)/2.0;
+        double upper_right = (solve(poured, i-1, j)-1)/2.0;
+        if(upper_left < 0)
+           upper_left =0.0;
+        if(upper_right < 0)
+           upper_right =0.0;
+
+        return dp[i][j] = upper_left + upper_right;
+    }
+    double champagneTower(int poured, int query_row, int query_glass) {
+        for(int i=0; i<101; i++){
+            for(int j=0; j<101; j++){
+                dp[i][j] =-1;
+            }
+        }
+        return min(1.0, solve(poured, query_row, query_glass));
+    }
+};
