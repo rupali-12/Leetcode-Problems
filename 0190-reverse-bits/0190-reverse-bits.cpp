fb270f9c4d888730bb437cc78a4a91a969b76a7e
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-02-16 17:41:38 +0530

    Time: 0 ms (100%), Space: 8.2 MB (73.18%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     int reverseBits(int n) {
+        // uint32_t result =0;
+        // for(int i=0; i<32; i++){
+        //     int lsb = n & 1;
+        //     int revLsb = lsb << (31-i);
+        //     result = revLsb | result;
+        //     n= n>>1;
+        // }
+        // return result;
+
         uint32_t result =0;
         for(int i=0; i<32; i++){
             int lsb = n & 1;
-            int revLsb = lsb << (31-i);
-            result = revLsb | result;
+            result = result<<1;
+            result = result | lsb;
             n= n>>1;
         }
         return result;
