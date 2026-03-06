0cdc3a615c88bbc2eec20da84d0822d626ef3dd8
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-03-06 11:39:01 +0530

    Time: 0 ms (100%), Space: 8.3 MB (46.93%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int count=0;
-        int i=0;
-        while(i<s.length()){
-            if(s[i]=='1'){
-                count++;
-                while(i<s.length() && s[i]=='1'){
-                    i++;
-                }
-            }
-            else{
-                i++;
-            }
+        // // Approach -1 
+        // int count=0;   // count of contigous count of 1's
+        // int i=0;
+        // while(i<s.length()){
+        //     if(s[i]=='1'){
+        //         count++;
+        //         while(i<s.length() && s[i]=='1'){
+        //             i++;
+        //         }
+        //     }
+        //     else{
+        //         i++;
+        //     }
 
-            if(count>1) return false;
-        }
-        return true;
+        //     if(count>1) return false;
+        // }
+        // return true;
+
+        // Approach 2: find pattern "01" , if not found then true
+        return s.find("01")==string::npos;   // means not found, and when we not found then only one set is present
+        
     }
 };
