e5a658b499666ff2fe17246a161b3a3fd48ae509
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-03-09 11:32:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(vector<int>& piles, int speed, int h){
+        int hours =0;
+        for(auto pile:piles){
+           hours+= ceil((pile+speed-1)/speed);
+        }
+        return hours<=h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left =1, right=1;
+
+        // find max value for right 
+        for(auto pile: piles){
+            right =max(right, pile);
+        }
+
+        // check if we can finish 
+        while(left<=right){
+            int mid =left + (right-left)/2;
+            if(canFinish(piles, mid, h)){
+                right = mid-1;
+            }
+            else{
+                left =mid+1;
+            }
+        }
+        return left;
+    }
+};
