a8dccdd89a49097f5c1d964126b3eb77f92279dd
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-03-17 10:46:13 +0530

    Time: 35 ms (59.57%), Space: 89.4 MB (52.13%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
-        // cumulative row 
-        int R= matrix.size();
-        int C= matrix[0].size();
-        for(int row=1; row<R; row++){
-            for(int col=0; col<C; col++){
-                if(matrix[row][col]!=0){
-                    matrix[row][col] += matrix[row-1][col];
-                }
-            }
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int mxArea = INT_MIN;
+        for(int row=0; row<rows; row++){
+           for(int col = 0; col<cols; col++){
+              if(matrix[row][col] ==1 && row>0){
+                matrix[row][col]+= matrix[row-1][col];
+              }
+           }
+          
+          vector<int>heights = matrix[row];
+          sort(heights.begin(), heights.end(), greater<int>());
+          for(int col=0; col<cols; col++){
+            int base = col + 1;
+            int height = heights[col];
+            mxArea = max(mxArea, (base*height));
+          }
         }
-        
-        
-        int maxArea=INT_MIN;
-        for(int row=0; row<R; row++){
-            sort(matrix[row].begin(), matrix[row].end(), greater<int>());
-            for(int col=0; col<C; col++){
-                int len = col+1;
-                int currArea = len*matrix[row][col];
-                maxArea= max(maxArea, currArea);
-            }
-        }
-        return maxArea;
+       return mxArea;
     }
 };
