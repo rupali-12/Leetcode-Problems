90a95622827c0a79a840479f758f5eaf64bd7101
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-03-24 11:06:17 +0530

    Time: 101 ms (11.33%), Space: 18.5 MB (65.78%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,92 +10,37 @@
  */
 class Solution {
 public:
-    class compare{
-    public:
-    bool operator()(ListNode* a,ListNode* b){
-        return a->val > b->val;
-    }
-};
     ListNode* merge(ListNode* first, ListNode* second){
-        if(first==NULL){
-            return second;
-        }
-        if(second==NULL){
-            return first;
-        }
+        if(first==NULL) return second;
+        if(second==NULL) return first;
         
-        ListNode* ans = new ListNode(-1);
-        ListNode* temp =ans;
-        
-        while(first!=NULL && second!=NULL){
+        ListNode* ans=new ListNode(0);
+        ListNode* temp=ans;
+        while(first!=NULL && second !=NULL){
             if(first->val < second->val){
-                temp->next= first;
-                temp= first;
+                temp->next = first;
                 first= first->next;
             }
             else{
-                  temp->next= second;
-                temp= second;
+                temp->next = second;
                 second= second->next;
             }
+                temp=temp->next;
         }
         
-        while(first!=NULL){
-             temp->next= first;
-                temp= first;
-                first= first->next;
+        temp->next = first==NULL?second:first;
+        return ans->next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty()) return NULL;
+       ListNode* ans= lists[0];
+        int i=1;
+
+        while(i<lists.size()){
+            ans = merge(ans, lists[i]);
+            i++;
         }
-        while(second!=NULL){
-              temp->next= second;
-                temp= second;
-                second= second->next;
-        }
-        ans= ans->next;
         return ans;
-    }
-    ListNode* mergeKLists(vector<ListNode*>& listArray) {
-        // if(lists.empty()){
-        //     return NULL;
-        // }
-        // ListNode* ans = lists[0];
-        // int i=1;
-        // while(i<lists.size()){
-        //   ans = merge(ans, lists[i]);
-        //     i++;
-        // }
-        // return ans;
-        
-        
-        // **********************************************
-        // Approach-2: using min heap 
-        priority_queue<ListNode*, vector<ListNode*>, compare>minheap;
-
-    int k= listArray.size();
-    // insert head of all lists 
-    for(int i=0; i<k; i++){
-         if(listArray[i]!=NULL){
-             minheap.push(listArray[i]);
-         }
-    }
-    
-   ListNode* head=NULL, *tail=NULL;
-
-    while(!minheap.empty()){
-        ListNode* top = minheap.top();
-        minheap.pop();
-
-        if(head==NULL){
-            head=tail= top;
-        }
-        else{
-            tail->next= top;
-            tail= top;
-        }
-
-        if(top->next!=NULL){
-            minheap.push(top->next);
-        }
-    }
-    return head;
     }
 };
