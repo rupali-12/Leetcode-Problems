c6743fa9fc9e29bf4db5e08d1ae432934fc1982b
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-03-24 11:33:41 +0530

    Time: 3 ms (64.33%), Space: 18.3 MB (81.96%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,6 +10,12 @@
  */
 class Solution {
 public:
+class compare{
+    public:
+    bool operator()(ListNode* a, ListNode* b){
+        return a->val > b->val;
+    }
+};
     ListNode* merge(ListNode* first, ListNode* second){
         if(first==NULL) return second;
         if(second==NULL) return first;
@@ -33,14 +39,50 @@ public:
     }
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.empty()) return NULL;
-       ListNode* ans= lists[0];
-        int i=1;
+        // Approach-1: MAnual processing merging one by one
+    //     if(lists.empty()) return NULL;
+    //    ListNode* ans= lists[0];
+    //     int i=1;
 
-        while(i<lists.size()){
-            ans = merge(ans, lists[i]);
-            i++;
+    //     while(i<lists.size()){
+    //         ans = merge(ans, lists[i]);
+    //         i++;
+    //     }
+    //     return ans;
+
+    // Approach 2: using priority queue
+     int k=lists.size();
+     priority_queue<ListNode*, vector<ListNode*>, compare>minHeap;
+
+     if(k==0) return NULL;
+
+     for(int i=0; i<k; i++){
+        if(lists[i]!=NULL){
+            minHeap.push(lists[i]);
         }
-        return ans;
+     }
+
+     ListNode* head= NULL;
+     ListNode* tail= NULL;
+
+     while(minHeap.size()>0){
+       ListNode* top =minHeap.top();
+       minHeap.pop();
+
+       if(top->next != NULL){  // insert in heap
+            minHeap.push(top->next);
+          }
+
+       if(head==NULL){   // first insertion
+          head= top;
+          tail=top;
+       }
+       else{
+          tail->next= top;
+          tail =top;
+       }
+     }
+
+     return head;
     }
 };
