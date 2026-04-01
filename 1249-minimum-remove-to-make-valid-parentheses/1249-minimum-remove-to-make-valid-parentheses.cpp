1206b2df3012d1959734b1dc2e52de6ca72f00a9
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-04-01 18:59:18 +0530

    Time: 12 ms (30.87%), Space: 14.5 MB (49.3%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-      int n= s.length();
+        // approach -1: using stack
+        int n=s.length();
+        unordered_set<int>removedIdx;
         stack<int>st;
-        unordered_set<int>removed_idx;
-        
+
         for(int i=0; i<n; i++){
-           if(s[i]=='('){
-               st.push(i);
-           }    
+            if(s[i]=='('){
+                st.push(i);
+            }
             else if(s[i]==')'){
                 if(st.empty()){
-                    removed_idx.insert(i);
+                    removedIdx.insert(i);
                 }
                 else{
                     st.pop();
                 }
             }
         }
-        
+
+        // place in removedIdx if any extra open brackets present in st
         while(!st.empty()){
-            removed_idx.insert(st.top());
+            removedIdx.insert(st.top());
             st.pop();
         }
-       string ans="";
+
+        string ans="";
         for(int i=0; i<n; i++){
-            if(removed_idx.find(i)==removed_idx.end()){
-                ans+= s[i];
-            }
+           if(removedIdx.find(i)==removedIdx.end()){
+            ans+=s[i];
+           }
         }
-        return ans;
+       return ans;
     }
 };
