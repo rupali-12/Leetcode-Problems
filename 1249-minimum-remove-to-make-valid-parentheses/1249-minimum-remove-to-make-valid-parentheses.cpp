7eef67c271da8c57b64e80ff58863ad62b2f6ea7
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-04-01 18:58:39 +0530

    Time: 15 ms (18.56%), Space: 14.7 MB (37.23%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,110 +1,34 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        
-//         // Approach-1: Using Stack 
-//         int n= s.length();
-//         stack<int>st;
-//         unordered_set<int>removed_idx;
-        
-//         for(int i=0; i<n; i++){
-//            if(s[i]=='('){
-//                st.push(i);
-//            }    
-//             else if(s[i]==')'){
-//                 if(st.empty()){
-//                     removed_idx.insert(i);
-//                 }
-//                 else{
-//                     st.pop();
-//                 }
-//             }
-//         }
-        
-//         while(!st.empty()){
-//             removed_idx.insert(st.top());
-//             st.pop();
-//         }
-//        string ans="";
-//         for(int i=0; i<n; i++){
-//             if(removed_idx.find(i)==removed_idx.end()){
-//                 ans+= s[i];
-//             }
-//         }
-//         return ans;
-        
-      // *************************************************************************    
-//          // Approach-2: Without Using Stack 
-//         int open=0, close=0;
-//         string ans="";
-//         // forward traversing to remove unbalanced closed parentheses 
-//         for(int i=0; i<s.length(); i++){
-//            if(s[i]>='a' && s[i]<='z'){
-//                ans+= s[i];
-//            }    
-//             else if(s[i]=='('){
-//                 open++;
-//                 ans+= s[i];
-//             }
-//             else if(open>0){
-//                 open--;
-//                 ans+= s[i];
-//             }
-//         }
-        
-//         // backward traversing to remove unbalanced open parentheses 
-//         string final_ans="";
-//           for(int i=ans.length()-1; i>=0; i--){
-//            if(ans[i]>='a' && ans[i]<='z'){
-//                final_ans+= ans[i];
-//            }    
-//             else if(ans[i]==')'){
-//                 close++;
-//                 final_ans+= ans[i];
-//             }
-//             else if(close>0){
-//                 close--;
-//                 final_ans+= ans[i];
-//             }
-//         }
-//         reverse(final_ans.begin(), final_ans.end());
-//         return final_ans;
-        
- // *************************************************************************
-        // Approach-3: Optimized Approach 
-           
-        int n= s.length();
-        string temp="";
-         int ignore_open=0;
+      int n= s.length();
+        stack<int>st;
+        unordered_set<int>removed_idx;
         
         for(int i=0; i<n; i++){
-             if(s[i]>='a' && s[i]<='z'){
-                temp+= s[i];
-            }
-            else if(s[i]=='('){
-                ignore_open++;
-                temp+= s[i];
-            }
+           if(s[i]=='('){
+               st.push(i);
+           }    
             else if(s[i]==')'){
-                if(ignore_open>0){
-                    ignore_open--;
-                    temp+= s[i];
+                if(st.empty()){
+                    removed_idx.insert(i);
+                }
+                else{
+                    st.pop();
                 }
             }
         }
-
-        cout<<temp<<endl;
-        string ans="";
-        for(int i=temp.length()-1; i>=0; i--){
-          if(temp[i]=='(' && ignore_open>0){
-                ignore_open--;
-            }
-            else{
-                ans+= temp[i];
+        
+        while(!st.empty()){
+            removed_idx.insert(st.top());
+            st.pop();
+        }
+       string ans="";
+        for(int i=0; i<n; i++){
+            if(removed_idx.find(i)==removed_idx.end()){
+                ans+= s[i];
             }
         }
-        
-        reverse(ans.begin(), ans.end());
-       return ans;
+        return ans;
     }
 };
