440054629df87a2c0bfd77b6f38a3c1cb219fbba
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-04-04 23:00:54 +0530

    Time: 64 ms (5.01%), Space: 98 MB (5.04%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,68 +1,68 @@
 class Solution {
 public:
-     // int largestRectangleArea(vector<int>& heights) {
-    // // Approach-1>> Brute Force>>>>O(n^2)-->TLE
-    //     int n=heights.size();
-    //     int area=0;
-    //     for(int i=0;i <n; i++){
-    //         int currArea=0;
-    //         int minHeight=INT_MAX;
-    //         for(int j=i; j<n; j++){
-    //             minHeight =min(minHeight, heights[j]);
-    //             currArea = max(currArea, minHeight*(j-i+1));
-    //         }
-    //         area =max(area, currArea);
-    //     }
-    //    return area;
-    //  }
-         
-         
-         // Approach-2>> Using Stack>>>>>
-    // here we have to push indexes not values 
-    // and compare it with value at that index which is stored at top of stack
+    // int largestRectangleArea(vector<int>& heights) {
+        // // Approach-1: Brute Force: T(O^2)
+        // int n=heights.size();
+        // int area=0;
+        // for(int i=0; i<n; i++){
+        //     int currArea = 0;
+        //     int minHeight = INT_MAX;
+        //     for(int j=i; j<n; j++){
+        //         minHeight = min(minHeight, heights[j]);
+        //         currArea = max(currArea, minHeight*(j-i+1));
+        //     }
+        //     area = max(area, currArea);
+        // }
+        // return area;
+    // }
 
-    vector<int>prevSmaller(vector<int> arr, int n){
-         stack<int>s;
-   vector<int>ans(n);
-   s.push(-1);
-   for(int i=0; i<n; i++){
-     while (s.top()!=-1 && arr[s.top()] >= arr[i]) {
-       s.pop();
-     }
-         ans[i]= s.top();
-         s.push(i);
-   }
-   return ans;
-    }
-     vector<int>nextSmaller(vector<int> arr, int n){
-         stack<int>s;
-   vector<int>ans(n);
-   s.push(-1);
-   for(int i=n-1; i>=0; i--){
-     while (s.top()!=-1 && arr[s.top()] >= arr[i]) {
-       s.pop();
-     }
-         ans[i]= s.top();
-         s.push(i);
-   }
-   return ans;
-    }
-      int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int>prev(n);
-          prev= prevSmaller(heights, n);
-          
-        vector<int>next(n);
-          next = nextSmaller(heights, n);
-          int ans= INT_MIN;
+    vector<int>prevSmaller(vector<int>arr, int n){
+        stack<int>st;
+        vector<int>ans(n);
+        st.push(-1);
         for(int i=0; i<n; i++){
-            int len = heights[i];
-            if(next[i]==-1){
-                next[i]=n;
+            while(st.top()!=-1 && arr[st.top()] >= arr[i]){
+                st.pop();
             }
-            int breadth = next[i]-prev[i]-1;
-            ans= max(ans, len*breadth);
+
+            ans[i] =st.top();
+            st.push(i);
         }
-          return ans;
+        return ans;
+    }
+
+    vector<int>nextSmaller(vector<int>arr, int n){
+        stack<int>st;
+        vector<int>ans(n);
+        st.push(-1);
+        for(int i=n-1; i>=0; i--){
+            while(st.top()!=-1 && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+
+            ans[i] =st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        // Approach: 2 Optimize using stack and storing nextSmaller and prev smaller 
+        int n= heights.size();
+        vector<int>prev(n);
+        prev = prevSmaller(heights, n);
+
+        vector<int>next(n);
+        next = nextSmaller(heights, n);
+       
+        int ans= INT_MIN;
+        for(int i=0; i<n; i++){
+            int height = heights[i];
+            if(next[i]==-1){
+                next[i]= n;
+            }
+            int breadth = next[i] - prev[i] -1;
+            ans=max(ans, height*breadth);
+        }
+        return ans;
     }
 };
