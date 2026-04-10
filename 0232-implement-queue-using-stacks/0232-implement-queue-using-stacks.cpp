077cbce8649021785095cd431659d90bd8f2572a
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-04-10 12:45:25 +0530

    Time: 0 ms (100%), Space: 9.7 MB (31.04%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,95 +1,66 @@
-// class MyQueue {
-//     private:
-//      int *arr;
-//       int size;
-//       int frnt, rear;
-// public:
-//     MyQueue() {
-//          size= 100001;
-//        arr= new int[size];
-//        frnt=rear=0;
-//     }
-    
-//     void push(int x) {
-//           if(rear==size)
-//            return;
-        
-//         arr[rear]=x;
-//         rear++;
-//         return;
-//     }
-    
-//     int pop() {
-//         if(frnt==rear){
-//             return -1;
-//         }
-//         int temp=arr[frnt];
-//         arr[frnt]=-1;
-//         frnt++;
-//         return temp;
-//     }
-    
-//     int peek() {
-//             if(frnt==-1){
-//             return -1;
-//         }
-//         return arr[frnt];
-//     }
-    
-//     bool empty() {
-//           if(frnt==rear){
-//             return true;
-//         }
-//         return false;
-//     }
-// };
-
-// /**
-//  * Your MyQueue object will be instantiated and called as such:
-//  * MyQueue* obj = new MyQueue();
-//  * obj->push(x);
-//  * int param_2 = obj->pop();
-//  * int param_3 = obj->peek();
-//  * bool param_4 = obj->empty();
-//  */
-
-
-// ***************************************************************************
 class MyQueue {
-    private:
-    stack<int>s;
 public:
+
+    // // Using Arrays 
+    // int front;
+    // vector<int>arr;
+    // MyQueue() {
+    //     front=0;
+    // }
+    
+    // void push(int x) {
+    //     arr.push_back(x);
+    // }
+    
+    // int pop() {
+    //     if(empty()) return -1;
+    //     int val= arr[front++];
+    //     return val;
+    // }
+    
+    // int peek() {
+    //     if(empty()) return -1;
+    //     return arr[front];
+    // }
+    
+    // bool empty() {
+    //     return front == arr.size();
+    // }
+
+    // Using Stacks 
+    stack<int>s1, s2;
     MyQueue() {
     }
     
     void push(int x) {
-        stack<int>temp;
-     while(!s.empty()){
-         temp.push(s.top());
-         s.pop();
-     }
-        s.push(x);
-        while(!temp.empty()){
-            s.push(temp.top());
-            temp.pop();
-        }
-        return;
+       s1.push(x);
     }
     
     int pop() {
-        int temp= s.top();
-        s.pop();
-        return temp;
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int val = s2.top();
+        s2.pop();
+        return val;
     }
     
     int peek() {
-        return s.top();
+       if(s2.empty()){
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+       }
+       return s2.top();
     }
     
     bool empty() {
-        return s.empty();
+        return s1.empty() && s2.empty();
     }
-    
 };
 
 /**
