5841b35e93875ed3d918219227055ac0dec0027c
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-04-12 23:28:13 +0530

    Time: 107 ms (83.99%), Space: 38.5 MB (74.99%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -1,55 +1,47 @@
 class Solution {
 public:
-    void fillNeighbours(queue<string>&q, unordered_set<string>&st, string curr){
-       for(int i=0; i<4; i++){
-           char ch = curr[i];
-           char inc= ch=='9'?'0': ch+1;
-           char dec= ch=='0'?'9': ch-1;
-           
-           // increment 
-           curr[i]= inc;
-           if(st.find(curr)==st.end()){
+    void fillNeighbour(queue<string>&que, string curr, unordered_set<string>&st){
+        for(int i=0; i<4; i++){
+            char ch= curr[i];
+            char dec = ch=='0' ? '9': ch-1;
+            char inc = ch=='9' ? '0': ch+1;
+
+            curr[i]=dec;
+            if(st.find(curr) == st.end()){
+               que.push(curr);
                st.insert(curr);
-               q.push(curr);
-           }
-           
-           // decrement 
-           curr[i]= dec;
-           if(st.find(curr)==st.end()){
+            }
+
+            curr[i]=inc;
+            if(st.find(curr) == st.end()){
+               que.push(curr);
                st.insert(curr);
-               q.push(curr);
-           }
-           
-           // make curr as previous 
-           curr[i]=ch;
-       }    
-        return;
+            }
+            curr[i]=ch;
+        }
     }
-    
     int openLock(vector<string>& deadends, string target) {
-        unordered_set<string>st(begin(deadends), end(deadends));
-        
-        string start="0000";
-        if(st.find(start)!=st.end()){  //  is invalid case 
+        unordered_set<string>st(deadends.begin(), deadends.end());
+
+        queue<string>que;
+        string start= "0000";
+          if(st.find(start)!=st.end()){  //  is invalid case 
             return -1;
         }
         
+        que.push(start);
         int level=0;
-        queue<string>q;
-        q.push(start);
-        
-        while(!q.empty()){
-           int size= q.size();
-            
+        while(!que.empty()){
+            int size= que.size();
             while(size--){
-                string curr= q.front();
-                q.pop();
-                
+                string curr = que.front();
+                que.pop();
+
                 if(curr==target){
                     return level;
                 }
-                
-                fillNeighbours(q, st, curr);
+
+                fillNeighbour(que, curr, st);
             }
             level++;
         }
