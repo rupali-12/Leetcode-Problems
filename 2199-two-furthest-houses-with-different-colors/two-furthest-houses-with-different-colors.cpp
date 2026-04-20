0417c6a94cbd6e20bcbd3ce89b77174f00535c09
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-04-20 10:05:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (40.24%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -2,13 +2,35 @@ class Solution {
 public:
     int maxDistance(vector<int>& colors) {
         int mxAns=0, n=colors.size();
-        for(int i=0; i<n-1; i++){
-           for(int j=i+1; j<n; j++){
-              if(colors[i]!=colors[j]){
-                mxAns = max(mxAns, abs(j-i));
-              }
-           }
-        }
-        return mxAns;
+
+        // // Approach-1: Brute Force
+        // for(int i=0; i<n-1; i++){
+        //    for(int j=i+1; j<n; j++){
+        //       if(colors[i]!=colors[j]){
+        //         mxAns = max(mxAns, abs(j-i));
+        //       }
+        //    }
+        // }
+        // return mxAns;
+       
+    //    Approach-2: Optimize O(n)
+       int left=0, right=0;
+    //    check left idx if any left make max and with n-1
+       for(int i=0; i<n; i++){
+          if(colors[i]^colors[n-1]){
+            left=i;
+            break;
+          }
+       }
+
+    //    check right idx if any right make max and with 0
+       for(int i=n-1; i>=0; i--){
+          if(colors[i]^colors[0]){
+            right=i;
+            break;
+          }
+       }
+       mxAns = max(n-1-left, right);   // right-0
+       return mxAns;
     }
 };
