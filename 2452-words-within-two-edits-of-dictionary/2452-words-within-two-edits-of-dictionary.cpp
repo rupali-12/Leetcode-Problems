be620a61875e5cef72b2d87e881160b1a164fe44
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-04-22 10:21:50 +0530

    Time: 11 ms (49.2%), Space: 18.8 MB (43.32%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    bool isPossible(string str, vector<string>& dictionary){
-        for(auto s: dictionary){
-            int cnt=0;
-            for(int i=0;i<str.length(); i++){
-                if(str[i]!=s[i]) cnt++;
-            }
-            if(cnt<=2) return true;
-        }
-        return false;
-    }
-
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
         vector<string>ans;
         int n=queries.size();
 
-        for(int i=0; i<n; i++){
-            if(isPossible(queries[i], dictionary)){
-                ans.push_back(queries[i]);
+        for (auto &q : queries) {
+            for(auto s: dictionary){
+            int cnt=0;
+            for(int i=0;i<q.length(); i++){
+                if(q[i]!=s[i]) {
+                    cnt++;
+                    if(cnt>2) break;
+                }
+            }
+            if(cnt<=2){
+                ans.push_back(q);
+                break;
+            }
             }
         }
         return ans;
