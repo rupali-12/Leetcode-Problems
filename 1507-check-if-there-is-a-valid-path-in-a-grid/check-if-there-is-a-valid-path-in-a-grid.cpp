0dd4bf8cd78fbf10fe94d39b1a5d57699e9591c7
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-04-27 10:56:08 +0530

    Time: 815 ms (5.24%) | Memory: 154.7 MB (5.24%) - LeetSync

---
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    unordered_map<int, vector<vector<int>>>directions = {
+       {1, {{0, -1}, {0, 1}}},
+       {2, {{-1, 0}, {1, 0}}},
+       {3, {{0, -1}, {1, 0}}},
+       {4, {{0, 1}, {1, 0}}},
+       {5, {{0, -1}, {-1, 0}}},
+       {6, {{-1, 0}, {0, 1}}},
+    };
+    int m, n;
+
+    bool dfs(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& visited){
+      if(i==m-1 && j==n-1) return true;
+
+      visited[i][j]=1;
+
+      for(auto dir: directions[grid[i][j]]){
+        int new_i= i+dir[0];
+        int new_j= j+dir[1];
+
+        // check for outbound
+        if(new_i<0 || new_i>=m || new_j<0 || new_j>=n || visited[new_i][new_j]==1) continue;
+
+        for(auto backDir: directions[grid[new_i][new_j]]){
+            if((new_i + backDir[0] == i) && (new_j + backDir[1] == j)){
+              if(dfs(grid, new_i, new_j, visited)){
+                return true;
+              }
+            }
+        }
+
+      }
+       return false;
+    }
+    bool hasValidPath(vector<vector<int>>& grid) {
+         m =grid.size();
+         n=grid[0].size();
+
+        vector<vector<int>>visited(m, vector<int>(n, 0));
+        return dfs(grid, 0, 0, visited);
+    }
+};
