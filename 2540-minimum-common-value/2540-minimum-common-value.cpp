97cb7a96909783f0a6a9e1337e89d6a195577f5f
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-05-19 09:59:12 +0530

    Time: 44 ms (15.72%), Space: 69.7 MB (14.31%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-//         // Approach-1: With space 
-//         int i=0;
-//         int ans=-1;
-//         map<int, int>mp;
-//         for(auto a: nums1){
-//             mp[a]++;
-//         }
-        
-//         while(i<nums2.size()){
-//             if(mp[nums2[i]]>0){
-//                 ans= nums2[i];
-//                 break;
-//             }
-//             i++;
-//         }
-//         return ans;
-        
-        // Approach-2: without extra space
-        int n1= nums1.size(), n2= nums2.size();
-        if(n1>n2){
-            swap(nums1, nums2);
-            swap(n1, n2);
+        int n1=nums1.size(), n2=nums2.size();
+        // Approach -1 with space
+        unordered_set<int>st;
+        for(int num: nums1){
+            st.insert(num);
         }
-        
-        int prev=0;
-        for(int i=0; i<n1; i++){
-            int x=nums1[i];
-            
-            // find lower bound of x 
-            int j= lower_bound(nums2.begin()+prev, nums2.end(), x)-nums2.begin();
-            if(j==n2) return -1;
-            else if(x==nums2[j]) return x;
-            else prev= j;
+
+        for(int i=0; i<n2; i++){
+            if(st.find(nums2[i])!=st.end()){
+                return nums2[i];
+            }
         }
         return -1;
     }
 };
-
-
-
-
