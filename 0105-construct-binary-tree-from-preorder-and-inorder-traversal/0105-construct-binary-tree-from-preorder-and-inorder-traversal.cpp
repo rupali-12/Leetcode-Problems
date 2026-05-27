7cb4a6aead98f6706f01115ab3aaf94957e67353
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-05-27 12:20:26 +0530

    Time: 4 ms (54.77%), Space: 27.1 MB (91.62%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,36 +11,22 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& in, vector<int>& pre, int &index, int inOrderStart, int inOrderEnd, int n, map<int, queue<int>>&NodeToIndex){
-        if(index>=n || inOrderStart>inOrderEnd){
-            return NULL;
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end, int &idx){
+        if(start>end) return NULL;
+        int rootVal = preorder[idx];
+        int i=start;
+        for(; i<=end; i++){
+            if(rootVal==inorder[i]) break;
         }
-        
-        int element = pre[index++];   // index is first element of pre array in each call 
-        
-        TreeNode* root =new TreeNode(element);
-        
-        // find position of root in inorder  
-        int position = NodeToIndex[element].front();
-        NodeToIndex[element].pop();
-        
-        // call for left
-      root->left = solve(in, pre, index, inOrderStart, position-1, n, NodeToIndex);
-        
-        // call for right
-        root->right = solve(in, pre, index, position+1, inOrderEnd, n, NodeToIndex);
-        
+        idx++;
+        TreeNode* root = new TreeNode(rootVal);
+        root->left = solve(preorder, inorder, start, i-1, idx);
+        root->right = solve(preorder, inorder, i+1, end, idx);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-         int preOrderIndex=0;
-        int n= preorder.size();
-        map<int, queue<int>>NodeToIndex;
-       for(int i=0; i<n; i++){
-           NodeToIndex[inorder[i]].push(i);
-       }
-       
-        TreeNode* ans =solve(inorder, preorder, preOrderIndex, 0, n-1, n, NodeToIndex);
-        return ans;
+        int n= inorder.size();
+        int idx = 0;
+        return solve(preorder, inorder, 0, n-1, idx);
     }
 };
