60d9f8832763ad0d202dc59ebacc970334aa268e
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-07-06 11:11:00 +0530

    Time: 0 ms (100%), Space: 15.2 MB (30.6%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,8 +1,30 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-         int n= intervals.size();
-         vector<vector<int>>result;
+        // // Approach 1:
+        //  int n= intervals.size();
+        //  vector<vector<int>>result;
+        //  auto lambda = [](vector<int>&vec1, vector<int>&vec2){
+        //     if(vec1[0]==vec2[0]){
+        //         return vec1[1]>vec2[1];
+        //     }
+        //     return vec1[0]<vec2[0];
+        //  };
+        //  sort(begin(intervals), end(intervals), lambda);
+
+        //  result.push_back(intervals[0]);
+
+        //  for(int i=1; i<n; i++){
+        //     // itheinterval covered by element present at result back
+        //     if(result.back()[1] >= intervals[i][1]){
+        //         continue;
+        //     }
+        //     result.push_back(intervals[i]);
+        //  }
+        //  return result.size();
+
+        // Approach -2:O(1)
+          int n= intervals.size(), lastEnd=0, size=0;
          auto lambda = [](vector<int>&vec1, vector<int>&vec2){
             if(vec1[0]==vec2[0]){
                 return vec1[1]>vec2[1];
@@ -11,15 +33,17 @@ public:
          };
          sort(begin(intervals), end(intervals), lambda);
 
-         result.push_back(intervals[0]);
+         lastEnd = intervals[0][1];
+         size++;
 
          for(int i=1; i<n; i++){
             // itheinterval covered by element present at result back
-            if(result.back()[1] >= intervals[i][1]){
+            if(lastEnd >= intervals[i][1]){
                 continue;
             }
-            result.push_back(intervals[i]);
+            lastEnd = intervals[i][1];
+            size++;
          }
-         return result.size();
+         return size;
     }
 };
