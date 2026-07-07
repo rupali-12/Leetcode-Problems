e45500da2094601e72619de5d89672d1932673c7
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-07-07 10:45:21 +0530

    Time: 0 ms (100%), Space: 9 MB (74.01%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        if(n==0) return 0;
-        string str = to_string(n);
-        string s ="";
-        for(int i=0; i<str.length(); i++){
-            if(str[i]!='0'){
-                s+= str[i];
-            }
-        }
-        long long sum=0;
-        for(auto ch :s){
-            sum+= ch-'0';
-        }
+        // if(n==0) return 0;
+        // string str = to_string(n);
+        // string s ="";
+        // for(int i=0; i<str.length(); i++){
+        //     if(str[i]!='0'){
+        //         s+= str[i];
+        //     }
+        // }
+        // long long sum=0;
+        // for(auto ch :s){
+        //     sum+= ch-'0';
+        // }
 
-        long long ans = 0;
-        int num = stoi(s);
-        ans = num*sum;
-        return ans;
+        // long long ans = 0;
+        // int num = stoi(s);
+        // ans = num*sum;
+        // return ans;
+
+        // Approach-1:optimize
+        if(n==0) return 0;
+        long long num =0, sum=0, place=1;
+        while(n>0){
+            int digit = n%10;
+            if(digit!=0){
+                num+= digit*place;
+                place*=10;
+                sum+= digit;
+            }
+            n/=10;
+        }
+        return num*sum;
     }
 };
