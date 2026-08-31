6b9ce1ab48c1ccaf65a15ff757951886d88313ba
Rupalisha_12 <78897399+rupali-12@users.noreply.github.com>
2026-08-31 11:53:19 +0530

    Time: 16 ms (5.16%), Space: 142.7 MB (5.02%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -10,6 +10,49 @@
  */
 class Solution {
 public:
+   bool isCritical(vector<int>& arr, int i){
+    if((arr[i-1]<arr[i] && arr[i+1]<arr[i]) || (arr[i-1]>arr[i] && arr[i+1]>arr[i])){
+        return true;
+    }
+    return false;
+   }
+
+   int findMaxDistance(vector<int>arr, int n){
+     int firstCriticalPos =-1, lastCriticalPos=-1;
+     int mxDis;
+     for(int i=1; i<n-1; i++){
+       if(isCritical(arr, i)){
+        if(firstCriticalPos ==-1) firstCriticalPos = i+1;  //  index to pos
+        else lastCriticalPos =i+1;
+       }
+     }
+     if(lastCriticalPos ==-1) mxDis = -1;
+     else mxDis = lastCriticalPos - firstCriticalPos;
+     return mxDis;
+   }
+
+       int findMinDistance(vector<int>arr, int n){
+     int firstCriticalPos =-1, secondCriticalPos=-1;
+     int mnDis=-1;
+    for(int i=1; i<n-1; i++){
+        if(isCritical(arr, i)){
+            if(firstCriticalPos == -1)  firstCriticalPos =i+1;
+            else {
+                if(secondCriticalPos ==-1){
+                secondCriticalPos =i+1;
+                mnDis = secondCriticalPos - firstCriticalPos;
+            }
+            else{
+                firstCriticalPos = secondCriticalPos;
+                secondCriticalPos = i+1;
+                mnDis = min(mnDis, (secondCriticalPos - firstCriticalPos));
+            }
+            }
+        }
+    }
+    return mnDis;
+   }
+
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         // Approach -1: Brute force
         vector<int>arr;
@@ -22,6 +65,9 @@ public:
         int n = arr.size();
         if(n<3) return {-1, -1};
 
-        int mxDistance = maxDistance
+        int mxDistance = findMaxDistance(arr, n);
+        int mnDistance = findMinDistance(arr, n);
+
+        return {mnDistance, mxDistance};
     }
 };
